bfd2d2becbdfd46cfe09d1ffa151603a24e1bbd6
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-07-14 17:40:55 +0900

    update exposed port

--- gitlab/docker-run-gitlab.sh
+++ gitlab/docker-run-gitlab.sh
@@ -11,9 +11,9 @@ docker run \
     --restart unless-stopped \
     --hostname gitlab.example.com \
     --publish 9443:443 \
-    --publish 9480:80 \
+    --publish 9480:9480 \
     --publish 9422:22 \
-    --env GITLAB_OMNIBUS_CONFIG="external_url 'http://gitlab.example.com/'; gitlab_rails['lfs_enabled'] = true; gitlab_rails['gitlab_shell_ssh_port'] = 9422;" \
+    --env GITLAB_OMNIBUS_CONFIG="external_url 'http://gitlab.example.com:9480'; gitlab_rails['lfs_enabled'] = true; gitlab_rails['gitlab_shell_ssh_port'] = 9422;" \
     --mount type=bind,src=${GITLAB_HOME}/config,dst=/etc/gitlab \
     --mount type=bind,src=${GITLAB_HOME}/logs,dst=/var/log/gitlab \
     --mount type=bind,src=${GITLAB_HOME}/data,dst=/var/opt/gitlab \
