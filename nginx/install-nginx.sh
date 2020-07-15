1f5fd6d98e5365238c90bbedfbadb0937a3feb93
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-07-15 17:50:50 +0900

    add nginx installation

---
+++ nginx/install-nginx.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+# https://www.nginx.com/resources/wiki/start/topics/tutorials/install
+
+cat > /etc/yum.repos.d/nginx.repo <<EOF 
+[nginx]
+name=nginx repo
+baseurl=https://nginx.org/packages/centos/$releasever/$basearch/
+gpgcheck=0
+enabled=1
+EOF
+
+yum install -y nginx
