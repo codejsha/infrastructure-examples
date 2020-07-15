a222abd9a93edac5e87e1791c93e8caf4f7749ea
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-07-15 14:55:07 +0900

    add descheduler

---
+++ descheduler/install-descheduler.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+# https://github.com/kubernetes-sigs/descheduler
+
+git clone --single-branch --branch release-1.18 https://github.com/kubernetes-sigs/descheduler.git
+
+kubectl create -f descheduler/kubernetes/rbac.yaml
+kubectl create -f descheduler/kubernetes/configmap.yaml
+
+### run as a job
+# kubectl create -f descheduler/kubernetes/job.yaml
+
+### run as a cronjob
+kubectl create -f descheduler/kubernetes/cronjob.yaml
