17f6b8c2112db90292e74f65a1f8a99398ba466b
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-07-16 11:03:04 +0900

    add mysql

---
+++ mysql/docker-run-mysql.ps1
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+docker run `
+    --detach `
+    --name mysql8 `
+    --publish 3306:3306 `
+    --env MYSQL_ROOT_PASSWORD="changeit" `
+    --env PGDATA=/var/lib/postgresql/data/pgdata `
+    --mount type=bind,src=$env:USERPROFILE\volume\mysql,dst=/var/lib/mysql `
+    mysql:8.0.21
+
+######################################################################
+
+docker run `
+    --detach `
+    --name mysql5 `
+    --publish 3306:3306 `
+    --env MYSQL_ROOT_PASSWORD="changeit" `
+    --env PGDATA=/var/lib/postgresql/data/pgdata `
+    --mount type=bind,src=$env:USERPROFILE\volume\mysql,dst=/var/lib/mysql `
+    mysql:5.7.31
---
+++ mysql/helm-install-mysql.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+NAMESPACE="mysql-system"
+kubectl create namespace ${NAMESPACE}
+
+helm install my-mysql \
+    --namespace ${NAMESPACE} \
+    --set imageTag="8.0.21" \
+    --set mysqlRootPassword="changeit" \
+    --set persistence.storageClass="rook-ceph-block" \
+    --set service.type="LoadBalancer" \
+    --set service.loadBalancerIP="10.10.10.97" \
+    --set timezone="Asia/Seoul" \
+    --version 1.6.6 \
+    stable/mysql
+
+    ### mysql 5
+    # --set imageTag: "5.7.31"
+
+### get the root password
+# MYSQL_ROOT_PASSWORD=$(kubectl get secret --namespace ${NAMESPACE} my-mysql -o jsonpath="{.data.mysql-root-password}" | base64 --decode; echo)
+### connect to the database directly from outside the cluster
+### https://dev.mysql.com/doc/refman/8.0/en/connection-options.html
+# mysql --host=${MYSQL_HOST} --port=${MYSQL_PORT} --user=root --password=${MYSQL_ROOT_PASSWORD}
