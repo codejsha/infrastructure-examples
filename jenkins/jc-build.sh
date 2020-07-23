4a91c004c0b213d2998117b03eef8297838cb4a4
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-07-23 17:31:35 +0900

    add log tail

--- jenkins/jc-build.sh
+++ jenkins/jc-build.sh
@@ -6,4 +6,6 @@ source jc-env-job.sh
 JENKINS_JOB_NAME="${1:-${JENKINS_JOB_NAME}}"
 
 ${JAVA_HOME}/bin/java -jar ${JENKINS_FILE_DIR}/jenkins-cli.jar -s ${JENKINS_URL} -webSocket -auth ${JENKINS_USER}:${JENKINS_API_TOKEN} \
-    build ${JENKINS_JOB_NAME}
+    build ${JENKINS_JOB_NAME} -f -v
+# ${JAVA_HOME}/bin/java -jar ${JENKINS_FILE_DIR}/jenkins-cli.jar -s ${JENKINS_URL} -webSocket -auth ${JENKINS_USER}:${JENKINS_API_TOKEN} \
+#     build ${JENKINS_JOB_NAME} -s -v
