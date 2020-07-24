bb5d844973ba872531ef6619f23146c7ab06367f
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-07-24 15:29:06 +0900

    update jdbc debug

--- weblogic/debug.sh
+++ weblogic/debug.sh
@@ -42,29 +42,28 @@ JAVA_OPTIONS="${JAVA_OPTIONS} -Djava.util.logging.config.file=${DOMAIN_HOME}/Ora
 # JAVA_OPTIONS="${JAVA_OPTIONS} -Doracle.jdbc.LogFile=${LOG_HOME}/jdbc.log"
 
 cat > ${DOMAIN_HOME}/OracleLog.properties <<EOF
-.level=FINEST
-handlers=java.util.logging.ConsoleHandler
-java.util.logging.ConsoleHandler.level=ALL
-java.util.logging.ConsoleHandler.formatter=java.util.logging.SimpleFormatter
+# handlers=java.util.logging.ConsoleHandler
+# java.util.logging.ConsoleHandler.level=ALL
+# java.util.logging.ConsoleHandler.formatter=java.util.logging.SimpleFormatter
 
-# handlers=java.util.logging.FileHandler
-# java.util.logging.FileHandler.level=ALL
-# java.util.logging.FileHandler.pattern=jdbc%g.log
-# java.util.logging.FileHandler.count=10000
-# java.util.logging.FileHandler.formatter=java.util.logging.SimpleFormatter
+handlers=java.util.logging.FileHandler
+java.util.logging.FileHandler.level=ALL
+java.util.logging.FileHandler.pattern=jdbc%g.log
+java.util.logging.FileHandler.count=10000
+java.util.logging.FileHandler.formatter=java.util.logging.SimpleFormatter
 
-# oracle.jdbc.connector.level=FINEST
-# oracle.jdbc.driver.level=FINEST
-# oracle.jdbc.internal.level=FINEST
-# oracle.jdbc.oci.level=FINEST
-# oracle.jdbc.oracore.level=FINEST
-# oracle.jdbc.pool.level=FINEST
-# oracle.jdbc.rowset.level=FINEST
-# oracle.jdbc.util.level=FINEST
-# oracle.jdbc.xa.level=FINEST
-# oracle.jdbc.xa.client.level=FINEST
-# oracle.jpub.level=FINEST
-# oracle.net.level=FINEST
-# oracle.sql.level=FINEST
-# oracle.sql.converter.level=FINEST
+oracle.jdbc.connector.level=FINEST
+oracle.jdbc.driver.level=FINEST
+oracle.jdbc.internal.level=FINEST
+oracle.jdbc.oci.level=FINEST
+oracle.jdbc.oracore.level=FINEST
+oracle.jdbc.pool.level=FINEST
+oracle.jdbc.rowset.level=FINEST
+oracle.jdbc.util.level=FINEST
+oracle.jdbc.xa.level=FINEST
+oracle.jdbc.xa.client.level=FINEST
+oracle.jpub.level=FINEST
+oracle.net.level=FINEST
+oracle.sql.level=FINEST
+oracle.sql.converter.level=FINEST
 EOF
