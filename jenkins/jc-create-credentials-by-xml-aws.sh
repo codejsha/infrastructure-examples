bee2109c6e8b8029cf4919bf603fe014dde2c117
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-07-26 21:44:58 +0900

    add/update credential commands

--- jenkins/jc-create-credentials-by-xml-aws.sh
+++ jenkins/jc-create-credentials-by-xml-aws.sh
@@ -2,28 +2,33 @@
 
 source jc-env.sh
 
-CREDENTIAL_DOMAIN="cicd"
-CREDENTIAL_USERNAME="username"
-CREDENTIAL_PASSWORD="changeit"
-CREDENTIAL_DESCRIPTION=""
+CREDENTIAL_DOMAIN="${${CREDENTIAL_DOMAIN}:-cicd}"
+CREDENTIAL_ACCESSKEY="${1:-accesskey}"
+CREDENTIAL_SECRETKEY="${2:-secretkey}"
+CREDENTIAL_DESCRIPTION="${CREDENTIAL_DESCRIPTION}"
 
 CREDENTIAL_UUID=$(uuidgen)
 echo "UUID: ${CREDENTIAL_UUID}"
 
-cat > credentials.xml <<EOF
-<com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl plugin="credentials@2.3.12">
+cat > credentials-aws.xml <<EOF
+<com.cloudbees.jenkins.plugins.awscredentials.AWSCredentialsImpl plugin="aws-credentials@1.28">
     <scope>GLOBAL</scope>
     <id>${CREDENTIAL_UUID}</id>
     <description>${CREDENTIAL_DESCRIPTION}</description>
-    <username>${CREDENTIAL_USERNAME}</username>
-    <password>${CREDENTIAL_PASSWORD}</password>
-</com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl>
+    <accessKey>${CREDENTIAL_ACCESSKEY}</accessKey>
+    <secretKey>${CREDENTIAL_SECRETKEY}</secretKey>
+    <iamRoleArn></iamRoleArn>
+    <iamMfaSerialNumber></iamMfaSerialNumber>
+</com.cloudbees.jenkins.plugins.awscredentials.AWSCredentialsImpl>
 EOF
+}
 
 ${JAVA_HOME}/bin/java -jar ${JENKINS_FILE_DIR}/jenkins-cli.jar -s ${JENKINS_URL} -webSocket -auth ${JENKINS_USER}:${JENKINS_API_TOKEN} \
-    create-credentials-by-xml "SystemCredentialsProvider::SystemContextResolver::jenkins" "${CREDENTIAL_DOMAIN}" < credentials.xml
+    create-credentials-by-xml "SystemCredentialsProvider::SystemContextResolver::jenkins" "${CREDENTIAL_DOMAIN}" < credentials-aws.xml
 # ${JAVA_HOME}/bin/java -jar ${JENKINS_FILE_DIR}/jenkins-cli.jar -s ${JENKINS_URL} -webSocket -auth ${JENKINS_USER}:${JENKINS_API_TOKEN} \
-#     create-credentials-by-xml "system::system::jenkins" "${CREDENTIAL_DOMAIN}" < credentials.xml
+#     create-credentials-by-xml "system::system::jenkins" "${CREDENTIAL_DOMAIN}" < credentials-aws.xml
+
+rm credentials-aws.xml
 
 ### get-credentials-as-xml
 # ${JAVA_HOME}/bin/java -jar ${JENKINS_FILE_DIR}/jenkins-cli.jar -s ${JENKINS_URL} -webSocket -auth ${JENKINS_USER}:${JENKINS_API_TOKEN} \
---
+++ jenkins/jc-delete-credentials.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+source jc-env.sh
+
+CREDENTIAL_DOMAIN="${${CREDENTIAL_DOMAIN}:-cicd}"
+CREDENTIAL_UUID="${1:-${CREDENTIAL_UUID}}"
+
+${JAVA_HOME}/bin/java -jar ${JENKINS_FILE_DIR}/jenkins-cli.jar -s ${JENKINS_URL} -webSocket -auth ${JENKINS_USER}:${JENKINS_API_TOKEN} \
+    delete-credentials "SystemCredentialsProvider::SystemContextResolver::jenkins" "${CREDENTIAL_DOMAIN}" ${CREDENTIAL_UUID}
+# ${JAVA_HOME}/bin/java -jar ${JENKINS_FILE_DIR}/jenkins-cli.jar -s ${JENKINS_URL} -webSocket -auth ${JENKINS_USER}:${JENKINS_API_TOKEN} \
+#     delete-credentials "system::system::jenkins" "${CREDENTIAL_DOMAIN}" ${CREDENTIAL_UUID}
