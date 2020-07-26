30b36bfcc752f612f20c393688d9957a445ac9cf
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-07-26 21:43:25 +0900

    update syntax

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -3,19 +3,19 @@
 USERNAME="prouser"
 
 sudo yum install -y yum-utils
-yum-config-manager \
+sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install -y docker-ce docker-ce-cli containerd.io
 
-usermod -a -G docker ${USERNAME}
-systemctl enable docker
-systemctl start docker
+sudo usermod -a -G docker ${USERNAME}
+sudo systemctl enable docker
+sudo systemctl start docker
 
 # dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 # dnf install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.10-3.2.el7.x86_64.rpm
 # dnf install docker-ce -y
 #
-# usermod -a -G docker ${USERNAME}
-# systemctl enable --now docker
-# systemctl status docker
+# sudo usermod -a -G docker ${USERNAME}
+# sudo systemctl enable --now docker
+# sudo systemctl status docker
