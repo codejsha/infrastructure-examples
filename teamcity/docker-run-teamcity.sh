5b4ad1f427c044b12f0d2c1467e734813a464591
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-08-13 12:41:38 +0900

    add teamcity

---
+++ teamcity/docker-run-teamcity.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+docker run \
+    --interactive \
+    --tty \
+    --name teamcity-server-instance \
+    --publish 8111:8111 \
+    --mount type=bind,src=/mnt/volume/teamcity/datadir,dst=/data/teamcity_server/datadir \
+    --mount type=bind,src=/mnt/volume/teamcity/logs,dst=/opt/teamcity/logs \
+    jetbrains/teamcity-server
