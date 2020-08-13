5d7fad17d39d91e4add346b5ec2d37f12b98ad78
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-08-13 12:44:36 +0900

    add tekton pipeline helloworld

---
+++ tekton/pipeline-helloworld/task-goodbye.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+# https://tekton.dev/docs/getting-started/
+
+cat > task-goodbye.yaml <<EOF
+apiVersion: tekton.dev/v1beta1
+kind: Task
+metadata:
+  name: goodbye
+spec:
+  steps:
+    - name: goodbye
+      image: ubuntu
+      script: |
+        #!/bin/bash
+        echo "Goodbye World!"
+EOF
+
+kubectl apply -f task-goodbye.yaml
+
+tkn task start goodbye --dry-run
+tkn task start goodbye
+# tkn task start goodbye --dry-run > taskRun-goodbye.yaml
+# kubectl create -f taskRun-goodbye.yaml
+
+tkn taskrun logs --last -f 
---
+++ tekton/pipeline-helloworld/task-hello.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+# https://tekton.dev/docs/getting-started/
+
+cat > task-hello.yaml <<EOF
+apiVersion: tekton.dev/v1beta1
+kind: Task
+metadata:
+  name: hello
+spec:
+  steps:
+    - name: hello
+      image: ubuntu
+      command:
+        - echo
+      args:
+        - "Hello World!"
+EOF
+
+kubectl apply -f task-hello.yaml
+
+tkn task start hello --dry-run
+tkn task start hello
+# tkn task start hello --dry-run > taskRun-hello.yaml
+# kubectl create -f taskRun-hello.yaml
+
+tkn taskrun logs --last -f 
