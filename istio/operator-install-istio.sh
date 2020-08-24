f5bcf2ae1ede070d920792b0031e5010454a9d49
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-08-24 13:27:57 +0900

    update install istio

--- istio/operator-install-istio.sh
+++ istio/operator-install-istio.sh
@@ -19,23 +19,8 @@ istioctl profile list
 ###     demo
 ###     empty
 
-# istioctl install --set profile=default
+istioctl install --set profile=default
 # istioctl install --set profile=demo
-
-istioctl install \
-    --set profile=default \
-    --set values.gateways.istio-ingressgateway.enabled=true \
-    --set values.gateways.istio-egressgateway.enabled=true \
-    --set components.pilot.enabled=true \
-    --set components.citadel.enabled=true \
-    --set components.policy.enabled=true \
-    --set components.telemetry.enabled=true \
-    --set components.cni.enabled=false \
-    --set addonComponents.grafana.enabled=true \
-    --set addonComponents.kiali.enabled=true \
-    --set addonComponents.prometheus.enabled=true \
-    --set addonComponents.tracing.enabled=true \
-    --set addonComponents.istiocoredns.enabled=false
 
 # kubectl get services -n istio-system
 # kubectl get pods -n istio-system
