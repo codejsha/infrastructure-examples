d932613f1bfdc095461a705c4c8a002bcb49e824
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-08-25 16:54:11 +0900

    add tomcat install

---
+++ tomcat/install-tomcat.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+# TOMCAT_MAJOR_VERSION="8"
+# TOMCAT_VERSION="8.5.57"
+TOMCAT_MAJOR_VERSION="9"
+TOMCAT_VERSION="9.0.37"
+# TOMCAT_MAJOR_VERSION="10"
+# TOMCAT_VERSION="10.0.0-M7"
+
+APPSVR_DIR="/svc/appsvr"
+
+curl -o ${APPSVR_DIR}/apache-tomcat-${TOMCAT_VERSION}.tar.gz -JLO http://mirror.apache-kr.org/tomcat/tomcat-${TOMCAT_MAJOR_VERSION}/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz
+tar -C ${APPSVR_DIR} -xzf apache-tomcat-${TOMCAT_VERSION}.tar.gz
+mv apache-tomcat-${TOMCAT_VERSION} tomcat${TOMCAT_MAJOR_VERSION}
+rm -f ${APPSVR_DIR}/apache-tomcat-${TOMCAT_VERSION}.tar.gz
