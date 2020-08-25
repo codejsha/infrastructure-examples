be07be42e32aa2709964ea98f6727e943960c595
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-08-25 21:14:48 +0900

    update tomcat instance

--- tomcat/instance/create-scripts.sh
+++ tomcat/instance/create-scripts.sh
@@ -56,10 +56,8 @@ cat <<EOF >> ${CATALINA_BASE}/start-${INSTANCE_NAME}.sh
 export CATALINA_OPTS
 
 touch \${CATALINA_OUT}
-\${CATALINA_HOME}/bin/catalina.sh start
-if [ "\${1}" != "notail" ]; then
-    tail -f ${CATALINA_OUT}
-fi
+\${CATALINA_HOME}/bin/startup.sh
+tail -f ${CATALINA_OUT}
 EOF
 
 ######################################################################
@@ -73,7 +71,7 @@ export JAVA_HOME="${JAVA_HOME}"
 export CATALINA_HOME="${CATALINA_HOME}"
 export CATALINA_BASE="${CATALINA_BASE}"
 
-\${CATALINA_HOME}/bin/catalina.sh stop
+\${CATALINA_HOME}/bin/shutdown.sh
 EOF
 
 ######################################################################
