7f82337323ea027cbbbb791d3417bfc39714a19b
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-08-27 19:43:15 +0900

    remove comments

--- tomcat/instance/env-base.sh
+++ tomcat/instance/env-base.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
-### define variables
 export INSTANCE_NAME="${1:-inst1}"
 export JAVA_HOME="/usr/lib/jvm/java-11"
 export CATALINA_HOME="/usr/local/tomcat"
@@ -23,6 +22,5 @@ export GC_LOG_OUT="\${LOG_DIR}/gc_\${INSTANCE_NAME}.log"
 
 ######################################################################
 
-### set variables
 export JAVA_VERSION="$(${JAVA_HOME}/bin/java -version 2>&1 /dev/null \
     | grep version | awk '{print $3}' | tr -d '"')"
