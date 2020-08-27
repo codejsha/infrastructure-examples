03cd27c61daf24cf2dfd08d77edc9ac48e281cc3
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-08-27 19:46:32 +0900

    add tomcat-redis

---
+++ tomcat/tomcat-redis/create-instance-cluster-redis.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/bash
+#!/usr/bin/bash
+
+source ./env-base.sh
+source ./env-function.sh
+source ./env-redisson.sh
+
+### create catalina base
+mkdir -p ${CATALINA_BASE}
+/usr/bin/cp -rpf ${CATALINA_HOME}/conf ${CATALINA_BASE}
+/usr/bin/cp -rpf ${CATALINA_HOME}/lib ${CATALINA_BASE}
+/usr/bin/cp -rpf ${CATALINA_HOME}/temp ${CATALINA_BASE}
+/usr/bin/cp -rpf ${CATALINA_HOME}/work ${CATALINA_BASE}
+mkdir -p ${CATALINA_BASE}/webapps/ROOT
+mkdir -p "$(eval echo ${LOG_DIR})"
+mkdir -p "$(eval echo $(eval echo ${DUMP_LOG_DIR}))"
+
+######################################################################
+
+### copy config files
+mv ${CATALINA_BASE}/conf/server.xml ${CATALINA_BASE}/conf/server.xml_default
+mv ${CATALINA_BASE}/conf/context.xml ${CATALINA_BASE}/conf/context.xml_default
+mv ${CATALINA_BASE}/conf/tomcat-users.xml ${CATALINA_BASE}/conf/tomcat-users.xml_default
+
+envsubst < ./server-cluster-redis.xml > ${CATALINA_BASE}/conf/server.xml
+envsubst < ./context.xml > ${CATALINA_BASE}/conf/context.xml
+# envsubst < ./tomcat-users.xml > ${CATALINA_BASE}/conf/tomcat-users.xml
+
+# /usr/bin/cp -pf ./server-cluster-redis.xml ${CATALINA_BASE}/conf/server.xml
+# /usr/bin/cp -pf ./context.xml ${CATALINA_BASE}/conf/context.xml
+# /usr/bin/cp -pf ./tomcat-users.xml ${CATALINA_BASE}/conf/tomcat-users.xml
+
+######################################################################
+
+### copy redisson config file
+if [ "${REDISSON_MODE}" == "CLUSTER_MODE" ]; then
+    /usr/bin/cp -pf ./redisson-cluster.yaml ${CATALINA_BASE}/conf/redisson.yaml
+elif [ "${REDISSON_MODE}" == "MASTER_SLAVE_MODE" ]; then
+    /usr/bin/cp -pf ./redisson-sentinel.yaml ${CATALINA_BASE}/conf/redisson.yaml
+elif [ "${REDISSON_MODE}" == "SINGLE_MODE" ]; then
+    /usr/bin/cp -pf ./redisson-proxy.yaml ${CATALINA_BASE}/conf/redisson.yaml
+fi
+
+######################################################################
+
+### format xml
+# tidy_indent ${CATALINA_BASE}/conf/server.xml
+# tidy_indent ${CATALINA_BASE}/conf/context.xml
+# tidy_indent ${CATALINA_BASE}/conf/tomcat-users.xml
+# tidy_indent ${CATALINA_BASE}/conf/redisson.yaml
+# tidy_nowrap ${CATALINA_BASE}/conf/server.xml
+# tidy_nowrap ${CATALINA_BASE}/conf/context.xml
+# tidy_nowrap ${CATALINA_BASE}/conf/tomcat-users.xml
+# tidy_nowrap ${CATALINA_BASE}/conf/redisson.yaml
+
+### change permission
+chmod 600 ${CATALINA_BASE}/conf/*
+
+######################################################################
+
+echo "An instance (${INSTANCE_NAME}) is created."
