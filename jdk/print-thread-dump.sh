e2609beca44e8bc9da2a0780b6fe419217cb6416
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-01 07:23:36 +0900

    update thread dump

--- jdk/print-thread-dump.sh
+++ jdk/print-thread-dump.sh
@@ -24,6 +24,7 @@ for IDX in $(seq 1 ${LOOP_COUNT})
 do
     ${JAVA_HOME}/bin/jstack -l ${PID} > ${PID}-threaddump-${IDX}.tdump
     # ${JAVA_HOME}/bin/jcmd ${PID} Thread.print > ${PID}-threaddump-${IDX}.tdump
-    
+    # kill -3 ${PID}
+
     sleep ${INTERVAL_SECONDS}
 done
