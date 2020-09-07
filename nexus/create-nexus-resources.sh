116e87a118aa8ea8342deff7203308cd6c7cc42f
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-07 13:35:25 +0900

    fix syntax error

--- nexus/create-nexus-resources.sh
+++ nexus/create-nexus-resources.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-create-user.sh "developer" "developer"
+bash create-user.sh "developer" "developer"
 
 bash create-blobstore.sh "nexus-maven-central"
 bash create-blobstore.sh "nexus-docker-hub"
@@ -9,7 +9,7 @@ bash create-blobstore.sh "nexus-helm-chartmuseum"
 
 bash delete-repository.sh "maven-central"
 bash create-maven-proxy-repo.sh "maven-central" "nexus-maven-central" "https://repo1.maven.org/maven2/"
-basj update-maven-group-repo.sh "docker-group" "default" "\"maven-central\",\"maven-releases\",\"maven-snapshots\""
+bash update-maven-group-repo.sh "docker-group" "default" "\"maven-central\",\"maven-releases\",\"maven-snapshots\""
 
 bash create-docker-proxy-repo.sh "docker-hub" "nexus-docker-hub" "https://registry-1.docker.io" "HUB"
 bash create-docker-proxy-repo.sh "docker-registry" "nexus-docker-registry" "http://my-docker-registry.registry-system:5000" "REGISTRY"
