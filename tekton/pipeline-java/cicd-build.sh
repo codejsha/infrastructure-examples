2123ff959ad688412c6480d37cb2af2036916bc1
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-07 21:16:32 +0900

    update image digest

--- tekton/pipeline-java/cicd-build.sh
+++ tekton/pipeline-java/cicd-build.sh
@@ -6,6 +6,7 @@ kubectl apply --filename https://raw.githubusercontent.com/tektoncd/catalog/mast
 kubectl apply --filename https://raw.githubusercontent.com/tektoncd/catalog/master/task/maven/0.2/maven.yaml
 # kubectl apply --filename https://raw.githubusercontent.com/tektoncd/catalog/master/task/kaniko/0.1/kaniko.yaml
 kubectl apply --filename kaniko-0.1-custom.yaml
+kubectl apply --filename verify-image-digest.yaml
 
 kubectl apply --filename docker-resource.yaml
 kubectl apply --filename git-resource.yaml
