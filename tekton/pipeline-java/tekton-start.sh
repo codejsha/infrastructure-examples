9a7c9097dfca713e1ee8441e206985714acf5cd7
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-10 15:53:15 +0900

    update tekton java pipeline

--- tekton/pipeline-java/tekton-start.sh
+++ tekton/pipeline-java/tekton-start.sh
@@ -7,10 +7,21 @@ kubectl apply --filename https://raw.githubusercontent.com/tektoncd/catalog/mast
 # kubectl apply --filename https://raw.githubusercontent.com/tektoncd/catalog/master/task/kaniko/0.1/kaniko.yaml
 kubectl apply --filename kaniko-0.1-custom.yaml
 kubectl apply --filename verify-image-digest.yaml
+kubectl apply --filename https://raw.githubusercontent.com/tektoncd/catalog/master/task/git-cli/0.1/git-cli.yaml
 
-kubectl apply --filename docker-resource.yaml
-kubectl apply --filename git-resource.yaml
-kubectl apply --filename maven-resource.yaml
+kubectl apply --filename github-secret.yaml
+kubectl apply --filename github-serviceaccount.yaml
+
+kubectl apply --filename gitlab-secret.yaml
+kubectl apply --filename gitlab-secretaccount.yaml
+kubectl apply --filename gitlab-role.yaml
+kubectl apply --filename gitlab-rolebinding.yaml
+kubectl apply --filename gitlab-trigger.yaml
+
+kubectl apply --filename docker-configmap.yaml
+kubectl apply --filename maven-configmap.yaml
+kubectl apply --filename input-asset-configmap.yaml
+
 kubectl apply --filename pvc.yaml
 kubectl apply --filename pipeline.yaml
 kubectl create --filename run.yaml
