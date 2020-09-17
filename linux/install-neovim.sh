95e0974718c2b44a386245f7c8cb8b0379cb037e
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-18 02:52:13 +0900

    Update install-neovim.sh

--- linux/install-neovim.sh
+++ linux/install-neovim.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/bash
 
-USERNAME="prouser"
-
 sudo yum install -y fuse fuse-sshfs
 
 if [ ! -e "/usr/local/bin/nvim" ]; then
-    curl -o /usr/local/bin/nvim -JLO https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage
-    chmod 755 /usr/local/bin/nvim
+    sudo curl -o /usr/local/bin/nvim -JLO https://github.com/neovim/neovim/releases/download/v0.4.4/nvim.appimage
+    sudo chmod 755 /usr/local/bin/nvim
 fi
 
-su - ${USERNAME} -c "mkdir -p ${HOME}/.config/nvim"
-su - ${USERNAME} -c "cat <<EOF > ${HOME}/.config/nvim/init.vim
+mkdir -p ${HOME}/.config/nvim
+cat <<EOF > ${HOME}/.config/nvim/init.vim
 syntax on
 set termguicolors
 set background=dark
@@ -20,15 +18,15 @@ set softtabstop=4
 set shiftwidth=4
 set expandtab
 set pastetoggle=<F11>
-EOF"
+EOF
 
 GREP_VAR="$(grep -q nvim ${HOME}/.bashrc)"
 if [ -n "${GREP_VAR}" ]; then
-su - ${USERNAME} -c "cat <<EOF > ${HOME}/.bashrc
+cat <<EOF > ${HOME}/.bashrc
 
 # neovim
 alias vi=\"nvim\"
 alias vim=\"nvim\"
 alias vimdiff=\"nvim -d\"
-EOF"
+EOF
 fi
