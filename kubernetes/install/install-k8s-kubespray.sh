b93ee1499798708aaf8a747fd12c1e68a0e663e7
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-19 21:41:35 +0900

    update kubespray install

--- kubernetes/install/install-k8s-kubespray.sh
+++ kubernetes/install/install-k8s-kubespray.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/bash
 
-PASSWORD="changeit"
-
-git clone -b release-2.13 https://github.com/kubernetes-sigs/kubespray.git
+PASSWORD="${PASSWORD}"
 
 echo "${PASSWORD}" | sudo -S yum install -y python3-pip
-pip3 install -r requirements.txt
+git clone -b release-2.14 https://github.com/kubernetes-sigs/kubespray.git
 
 cd kubespray
-/bin/cp -rpf inventory/sample inventory/mycluster
+sudo pip3 install -r requirements.txt
+/usr/bin/cp -rpf inventory/sample inventory/mycluster
 
 declare -a IPS=(10.10.10.11 10.10.10.12 10.10.10.13 10.10.10.21 10.10.10.22 10.10.10.23)
 CONFIG_FILE=inventory/mycluster/hosts.yaml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
 
-cat inventory/mycluster/group_vars/all/all.yml
-cat inventory/mycluster/group_vars/k8s-cluster/k8s-cluster.yml
+/usr/bin/cp -f ../hosts.yaml inventory/mycluster/hosts.yaml
+/usr/bin/cp -f ../all.yaml inventory/mycluster/group_vars/all/all.yml
+/usr/bin/cp -f ../docker.yaml inventory/mycluster/group_vars/all/docker.yml
+/usr/bin/cp -f ../addons.yaml inventory/mycluster/group_vars/k8s-cluster/addons.yml
+/usr/bin/cp -f ../k8s-cluster.yaml inventory/mycluster/group_vars/k8s-cluster/k8s-cluster.yml
 
-cp ../hosts.yaml inventory/mycluster/hosts.yaml
 ansible-playbook -i inventory/mycluster/hosts.yaml --become --become-user=root --user root cluster.yml
