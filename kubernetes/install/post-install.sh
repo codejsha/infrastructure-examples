04b681d1baace52a350b5a9a3cdd9d403ef264c2
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-20 15:07:23 +0900

    update file permission

--- kubernetes/install/post-install.sh
+++ kubernetes/install/post-install.sh
@@ -5,3 +5,4 @@ sudo scp root@controlplane1.example.com:/root/.kube/config ${HOME}/.kube/config
 
 sudo mkdir -p /etc/ssl/etcd/ssl/
 sudo scp root@controlplane1.example.com:/etc/ssl/etcd/ssl/* /etc/ssl/etcd/ssl/
+sudo chown -R root:$(id -gn) /etc/ssl/etcd/ssl/
