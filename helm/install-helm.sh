12a83411f2c1409233f3495a063e9885c4888f64
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-21 13:35:35 +0900

    update file ownership

--- helm/install-helm.sh
+++ helm/install-helm.sh
@@ -2,13 +2,15 @@
 
 HELM_VERSION="3.3.1"
 curl -JLO https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz
-sudo tar -xvzf helm-v${HELM_VERSION}-linux-amd64.tar.gz
+tar -xvzf helm-v${HELM_VERSION}-linux-amd64.tar.gz
 sudo mv linux-amd64/helm /usr/local/bin/helm
+sudo chown root:root /usr/local/bin/helm
 
 # HELM_VERSION="2.16.10"
 # curl -JLO https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz
 # tar -xvzf helm-v${HELM_VERSION}-linux-amd64.tar.gz
 # sudo mv linux-amd64/helm /usr/local/bin/helm
+# sudo chown root:root /usr/local/bin/helm
 
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com
