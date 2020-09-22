5dac69b3f8391a6a6a4c536276880a8967f5c140
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-22 23:34:54 +0900

    add comments

--- ohs/check-version.sh
+++ ohs/check-version.sh
@@ -24,4 +24,19 @@ function check_version_by_httpd {
 }
 
 # check_version_by_directory
+### 11g Result:
+### 06/26/2019  11:23 AM    <DIR>          11.1.1.9.0
+###
+### 12c Result:
+### 06/26/2019  01:42 PM    <DIR>          12.1.3.0.0
+### 06/26/2019  01:13 PM    <DIR>          12.2.1.3.0
+
 check_version_by_httpd
+### 11g Result:
+### Server version: Oracle-HTTP-Server/2.2.22 (Unix)
+### Server built:   Mar 20 2015 20:48:14
+### Server label:   APACHE_11.1.1.9.0_LINUX.X64_150320.1901
+###
+### 12c Result:
+### Server version: Oracle-HTTP-Server-12.2.1.3.0/2.4.25 (Unix)
+### Server built:   Aug 17 2017 18:56:05"
