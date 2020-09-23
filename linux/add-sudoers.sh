5b601e24112afcc7af3bcb95137c2243ac1eae8e
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-23 21:20:11 +0900

    add sudoers

---
+++ linux/add-sudoers.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+sudo sed -i -E '/^root\tALL=\(ALL\) \tALL$/ a prouser\tALL=(ALL) \tALL' /etc/sudoers
+sudo sed -i -E '/^root\tALL=\(ALL\) \tALL$/ a prouser\tALL=(ALL) \tNOPASSWD:ALL' /etc/sudoers
