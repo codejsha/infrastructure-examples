6cb5f897c62e00abbd8e4c8486bdf14c0bcab1bd
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-23 22:25:52 +0900

    install kustomize

---
+++ kustomize/install-kustomize.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+# https://kubernetes-sigs.github.io/kustomize/installation/
+# https://kubernetes-sigs.github.io/kustomize/installation/binaries/
+
+curl -s "https://raw.githubusercontent.com/\
+kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+
+sudo mv ./kustomize /usr/local/bin
+sudo chown root:root /usr/local/bin/kustomize
