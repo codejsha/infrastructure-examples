91c42028d6e115e5276e79f6874fa9f0c279ae51
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-23 22:22:53 +0900

    update helm gitlab

--- gitlab/helm-install-gitlab.sh
+++ gitlab/helm-install-gitlab.sh
@@ -5,17 +5,12 @@
 helm repo add gitlab https://charts.gitlab.io
 helm repo update
 
-NAMESPACE="git-system"
+NAMESPACE="gitlab-system"
 kubectl create namespace ${NAMESPACE}
 
 # helm install my-gitlab \
 helm upgrade --install my-gitlab \
     --namespace ${NAMESPACE} \
     --set global.edition="ce" \
-    --set global.hosts.domain="gitlab.example.com" \
-    --set global.ingress.configureCertmanager="false" \
-    --set global.ingress.annotations."kubernetes\.io/ingress\.class"="nginx" \
-    --set certmanager.install="false" \
-    --set nginx-ingress.install="false" \
     --version 4.2.1 \
     gitlab/gitlab
