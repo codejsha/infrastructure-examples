c81874b2e6bf157e071810761dc66b260b94eae4
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-23 22:06:26 +0900

    add github enterprise vm

---
+++ github/create-github-vm.ps1
@@ -0,0 +1,22 @@
+$VMName = "GitHubServerMachine"
+$VMPath = "D:\hyper"
+$Switch = "HyperInternal"
+$GitHubVHDName = "github-enterprise-2.21.7.vhd"
+$StorageVHDName = "github-storage.vhd"
+
+New-VM -Name $VMName `
+    -Path $VMPath `
+    -Generation 1 `
+    -MemoryStartupBytes 2GB `
+    -Switch $Switch `
+    -BootDevice VHD `
+    -VHDPath "$VMPath\$VMName\Virtual Hard Disks\$GitHubVHDName"
+Set-VMProcessor -VMName $VMName -Count 2
+Set-VMMemory $VMName -DynamicMemoryEnabled $False
+Add-VMNetworkAdapter -VMName $VMName -SwitchName $Switch
+New-VHD -Path "$VMPath\$VMName\Virtual Hard Disks\$StorageVHDName" -SizeBytes 64GB
+ADD-VMHardDiskDrive -VMName $VMName `
+    -ControllerType IDE `
+    -ControllerNumber 0 `
+    -ControllerLocation 1 `
+    -Path "$VMPath\$VMName\Virtual Hard Disks\$StorageVHDName"
