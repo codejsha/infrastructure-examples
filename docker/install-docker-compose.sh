55afe50d6fc5d42ed4ec8837ceabdfda3decd008
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-25 21:50:07 +0900

    update docker compose version

--- docker/install-docker-compose.sh
+++ docker/install-docker-compose.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
+# https://github.com/docker/compose
 
-DOCKER_COMPOSE_VERSION="1.26.2"
+DOCKER_COMPOSE_VERSION="1.27.3"
 
 sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 # sudo ln -snf /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+######################################################################
 
 # sudo yum install -y docker-compose
