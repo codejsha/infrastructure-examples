05a6773a0708702ecf4dca6a4257237c3b1b782e
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-25 21:49:38 +0900

    update syntax

--- gitlab/docker-run-gitlab.sh
+++ gitlab/docker-run-gitlab.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 
 GITLAB_HOME="/mnt/volume/gitlab"
-mkdir -p ${GITLAB_HOME}/config
-mkdir -p ${GITLAB_HOME}/logs
-mkdir -p ${GITLAB_HOME}/data
+sudo rm -rf ${GITLAB_HOME}
+sudo mkdir -p ${GITLAB_HOME}/config
+sudo mkdir -p ${GITLAB_HOME}/logs
+sudo mkdir -p ${GITLAB_HOME}/data
 
 docker run \
     --detach \
@@ -14,7 +15,7 @@ docker run \
     --publish 9480:9480 \
     --publish 9422:22 \
     --env GITLAB_OMNIBUS_CONFIG="external_url 'http://gitlab.example.com:9480'; gitlab_rails['lfs_enabled'] = true; gitlab_rails['gitlab_shell_ssh_port'] = 9422;" \
-    --mount type=bind,src=${GITLAB_HOME}/config,dst=/etc/gitlab \
-    --mount type=bind,src=${GITLAB_HOME}/logs,dst=/var/log/gitlab \
-    --mount type=bind,src=${GITLAB_HOME}/data,dst=/var/opt/gitlab \
+    --mount type=bind,source=${GITLAB_HOME}/config,target=/etc/gitlab \
+    --mount type=bind,source=${GITLAB_HOME}/logs,target=/var/log/gitlab \
+    --mount type=bind,source=${GITLAB_HOME}/data,target=/var/opt/gitlab \
     gitlab/gitlab-ce:latest
