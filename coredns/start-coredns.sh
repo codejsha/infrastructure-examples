4ba0a6d0a6c748c506b2d39575ef8d3bb477d8b3
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-30 00:02:27 +0900

    update syntax

--- coredns/start-coredns.sh
+++ coredns/start-coredns.sh
@@ -12,7 +12,7 @@ function start_coredns_nohup {
     sudo nohup /usr/local/bin/coredns \
         -conf ${COREDNS_HOME}/corefile-server.conf \
         -pidfile ${COREDNS_HOME}/coredns.pid \
-        > ./nohup.coredns.out 2>&1 &
+        > ${COREDNS_HOME}/nohup.coredns.out 2>&1 &
     sleep 1
     sudo chown $(id -un):$(id -gn) ${COREDNS_HOME}/coredns.pid
 }
--- iplanet/install-iplanet.sh
+++ iplanet/install-iplanet.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 INSTALL_FILE_DIR="/mnt/share/oracle-iplanet-web-server/Oracle-iPlanet-Web-Server-7.0.27-linux-x64"
 INSTALL_SCRIPT_DIR="/svc/infrastructure/iplanet"
