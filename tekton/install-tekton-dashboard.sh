0e8cd4c526dd425617865908c4462f078742cc47
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-30 00:51:19 +0900

    remove tekton additional yaml apply

--- tekton/install-tekton-dashboard.sh
+++ tekton/install-tekton-dashboard.sh
@@ -7,24 +7,3 @@ kubectl config set-context --current --namespace="${NAMESPACE}"
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/dashboard/latest/tekton-dashboard-release.yaml
 # TEKTON_DASHBOARD_VERSION="0.9.0"
 # kubectl apply --filename https://github.com/tektoncd/dashboard/releases/download/v${TEKTON_DASHBOARD_VERSION}/tekton-dashboard-release.yaml
-
-cat <<EOF > ./tekton-dashboard-ingress.yaml
-apiVersion: networking.k8s.io/v1beta1
-kind: Ingress
-metadata:
-  name: tekton-dashboard
-  annotations:
-    kubernetes.io/ingress.class: "nginx"
-  namespace: tekton-pipelines
-spec:
-  rules:
-  - host: tekton.example.com
-    http:
-      paths:
-      - path: /
-        backend:
-          serviceName: tekton-dashboard
-          servicePort: 9097
-EOF
-
-kubectl apply --filename tekton-dashboard-ingress.yaml
--- tekton/install-tekton-pipeline.sh
+++ tekton/install-tekton-pipeline.sh
@@ -8,34 +8,3 @@ kubectl config set-context --current --namespace="${NAMESPACE}"
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 # TEKTON_PIPELINE_VERSION="0.15.2"
 # kubectl apply --filename https://github.com/tektoncd/pipeline/releases/download/v${TEKTON_PIPELINE_VERSION}/release.yaml
-
-# cat <<EOF > ./s3-bucket.yaml
-# apiVersion: v1
-# kind: Secret
-# metadata:
-#   name: tekton-storage
-#   namespace: tekton-pipelines
-# type: kubernetes.io/opaque
-# stringData:
-#   boto-config: |
-#     [Credentials]
-#     aws_access_key_id = "AWS_ACCESS_KEY_ID"
-#     aws_secret_access_key = "AWS_SECRET_ACCESS_KEY"
-#     [s3]
-#     host = s3.us-east-1.amazonaws.com
-#     [Boto]
-#     https_validate_certificates = True
-# ---
-# apiVersion: v1
-# kind: ConfigMap
-# metadata:
-#   name: config-artifact-bucket
-#   namespace: tekton-pipelines
-# data:
-#   location: s3://mybucket
-#   bucket.service.account.secret.name: tekton-storage
-#   bucket.service.account.secret.key: boto-config
-#   bucket.service.account.field.name: BOTO_CONFIG
-# EOF
-#
-# kubectl apply -f s3-bucket.yaml
