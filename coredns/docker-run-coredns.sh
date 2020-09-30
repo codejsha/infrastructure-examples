73df83e622c0d67beb7168331880b2637998d6d1
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-30 14:54:39 +0900

    add docker coredns

---
+++ coredns/docker-run-coredns.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+COREDNS_VOLUME_DIR="/mnt/volume/coredns"
+sudo mkdir -p ${COREDNS_VOLUME_DIR}
+sudo /usr/bin/cp -f corefile-server.conf ${COREDNS_VOLUME_DIR}/Corefile
+
+docker run \
+    --detach \
+    --name coredns \
+    --restart=unless-stopped \
+    --publish 53:53/udp \
+    --mount type=bind,src=${COREDNS_VOLUME_DIR},dst=/root/coredns \
+    coredns/coredns:latest -conf /root/coredns/Corefile
