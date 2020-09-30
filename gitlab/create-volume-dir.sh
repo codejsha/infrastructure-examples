fe8f4e8bf40c3740a8eb989166074ca3025a0b34
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-30 15:10:19 +0900

    update syntax

--- gitlab/create-volume-dir.sh
+++ gitlab/create-volume-dir.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-GITLAB_MOUNT_DIR="/mnt/volume/gitlab"
-sudo rm -rf ${GITLAB_MOUNT_DIR}
-sudo mkdir -p ${GITLAB_MOUNT_DIR}/config
-sudo mkdir -p ${GITLAB_MOUNT_DIR}/logs
-sudo mkdir -p ${GITLAB_MOUNT_DIR}/data
+GITLAB_VOLUME_DIR="/mnt/volume/gitlab"
+sudo rm -rf ${GITLAB_VOLUME_DIR}
+sudo mkdir -p ${GITLAB_VOLUME_DIR}/config
+sudo mkdir -p ${GITLAB_VOLUME_DIR}/logs
+sudo mkdir -p ${GITLAB_VOLUME_DIR}/data
