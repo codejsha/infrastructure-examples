abfdb9bb819903278cf5fd19247da3df309567b6
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-30 11:05:37 +0900

    update minio service type to clusterip

--- minio/helm-install-minio.sh
+++ minio/helm-install-minio.sh
@@ -13,7 +13,7 @@ helm upgrade --install my-minio \
     --namespace ${NAMESPACE} \
     --set accessKey="admin" \
     --set secretKey="${PASSWORD}" \
-    --set service.type="LoadBalancer" \
+    --set service.type="ClusterIP" \
     --set ingress.enabled="true" \
     --set ingress.labels."node-role\.kubernetes\.io/ingress"="platform" \
     --set ingress.annotations."kubernetes\.io/ingress\.class"="nginx" \
@@ -23,3 +23,6 @@ helm upgrade --install my-minio \
     --set persistence.accessMode="ReadWriteOnce" \
     --version 7.0.1 \
     minio/minio
+
+    ### for loadbalancer
+    # --set service.type="LoadBalancer" \
