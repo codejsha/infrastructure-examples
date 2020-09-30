b504fadc1460ad18b99f4888febe88f93f356ead
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-09-30 11:24:16 +0900

    fix error

--- coredns/stop-coredns.sh
+++ coredns/stop-coredns.sh
@@ -3,7 +3,7 @@
 COREDNS_HOME="/svc/infrastructure/coredns"
 
 function stop_coredns {
-    sudo pkill -pidfile ${COREDNS_HOME}/coredns.pid
+    sudo pkill --pidfile ${COREDNS_HOME}/coredns.pid
     # sudo kill -9 $(cat ${COREDNS_HOME}/coredns.pid)
 }
 
