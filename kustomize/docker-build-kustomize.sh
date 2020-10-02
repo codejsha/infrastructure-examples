14e3985b521a8bb5bcb4ae9daaa54cb3a4abe5ea
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-10-02 19:22:45 +0900

    update kustomize

---
+++ kustomize/docker-build-kustomize.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+docker build --tag registry.example.com/kustomize-cli:latest .
+docker push registry.example.com/kustomize-cli:latest
--- kustomize/install-kustomize.sh
+++ kustomize/install-kustomize.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 # https://kubernetes-sigs.github.io/kustomize/installation/
 # https://kubernetes-sigs.github.io/kustomize/installation/binaries/
+# https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh
 
 curl -s "https://raw.githubusercontent.com/\
 kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
