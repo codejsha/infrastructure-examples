487077c9bcb8bbe9b62c20eb6d98de27eb7e8416
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-10-02 18:35:33 +0900

    update docker

--- docker/docker-mount-option.ps1
+++ docker/docker-mount-option.ps1
@@ -4,17 +4,17 @@
 
 ### bind
 
-# --mount type=bind,src=F:\share,dst=/mnt/share `
-# --mount type=bind,src=F:\storage,dst=/mnt/storage `
+--mount type=bind,src=F:\share,dst=/mnt/share `
+--mount type=bind,src=F:\storage,dst=/mnt/storage `
 
-# --mount type=bind,src=/mnt/share,dst=/mnt/share \
-# --mount type=bind,src=/mnt/storage,dst=/mnt/storage \
-# --mount type=bind,src=/mnt/volume/log,dst=/var/log \
+--mount type=bind,src=/mnt/share,dst=/mnt/share \
+--mount type=bind,src=/mnt/storage,dst=/mnt/storage \
+--mount type=bind,src=/mnt/volume/log,dst=/var/log \
 
 ######################################################################
 
 ### volume
 
-# docker volume create vol1
-# --mount type=volume,src=vol1,dst=/var/log `
-# --mount type=volume,src=vol1,dst=/var/log \
+docker volume create vol1
+--mount type=volume,src=vol1,dst=/var/log `
+--mount type=volume,src=vol1,dst=/var/log \
