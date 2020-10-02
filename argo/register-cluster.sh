be13af957adbf52553ed6141e6fe653e77dec3ac
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-10-02 12:08:23 +0900

    update address

--- argo/register-cluster.sh
+++ argo/register-cluster.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 # argocd cluster list
-# argocd cluster get argocd https://192.168.140.11:6443
+# argocd cluster get argocd https://10.10.10.11:6443
 # argocd cluster add
 argocd cluster add kubernetes-admin@cluster.local
