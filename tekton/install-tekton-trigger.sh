c8547f414f96bf828390d4241cc285ac231cfcd1
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-10-02 11:53:44 +0900

    update tekton

--- tekton/install-tekton-trigger.sh
+++ tekton/install-tekton-trigger.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
+# https://github.com/tektoncd/triggers
 
 NAMESPACE="tekton-pipelines"
 kubectl create namespace ${NAMESPACE}
 kubectl config set-context --current --namespace="${NAMESPACE}"
 
-kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
-# TEKTON_TRIGGER_VERSION="0.7.0"
-# kubectl apply --filename https://github.com/tektoncd/triggers/releases/download/v${TEKTON_TRIGGER_VERSION}/release.yaml
+# kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
+TEKTON_TRIGGER_VERSION="0.8.1"
+kubectl apply --filename https://github.com/tektoncd/triggers/releases/download/v${TEKTON_TRIGGER_VERSION}/release.yaml
