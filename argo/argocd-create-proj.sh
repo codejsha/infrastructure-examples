9b02205ba5542abd31c8e95d5067e0f1065dd055
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-10-04 04:32:57 +0900

    add argocd scripts

---
+++ argo/argocd-create-proj.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+PROJECT="myproject"
+SERVER="*"
+NAMESPACE="*"
+URL="*"
+GROUP="*"
+KIND="*"
+
+argocd proj create ${PROJECT}
+argocd proj add-destination PROJECT ${SERVER} ${NAMESPACE}
+argocd proj add-source ${PROJECT} ${URL}
+argocd proj allow-cluster-resource ${PROJECT} ${GROUP} ${KIND}
---
+++ argo/argocd-create-role.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+# https://argoproj.github.io/argo-cd/user-guide/projects/
+
+PROJECT="myproject"
+APP="myproject"
+ROLE_NAME="get-role"
+
+argocd proj role create ${PROJECT} ${ROLE_NAME}
+argocd proj role list ${PROJECT}
+argocd proj role get ${PROJECT} ${ROLE_NAME}
+
+# argocd proj role create-token ${PROJECT} ${ROLE_NAME}
+# argocd proj role create-token ${PROJECT} ${ROLE_NAME} --expires-in 3h
+JWT_TOKEN=$(argocd proj role create-token ${PROJECT} ${ROLE_NAME})
+echo ${JWT_TOKEN}
+
+argocd proj role add-policy ${PROJECT} ${ROLE_NAME} --action get --permission allow --object ${APP}
+argocd proj role add-policy ${PROJECT} ${ROLE_NAME} --action sync --permission allow --object ${APP}
+argocd app get ${PROJECT} ${ROLE_NAME} --auth-token ${JWT_TOKEN}
+argocd app get $PROJ-$TOKEN --auth-token $JWT
