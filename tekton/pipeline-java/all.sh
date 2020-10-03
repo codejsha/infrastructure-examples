bfb942a81231aa26883a610547575f365fc69d0c
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-10-03 17:48:24 +0900

    update java pipeline

--- tekton/pipeline-java/all.sh
+++ tekton/pipeline-java/all.sh
@@ -5,8 +5,8 @@ kubectl config set-context --current --namespace=tekton-pipelines
 kubectl apply --filename https://raw.githubusercontent.com/tektoncd/catalog/master/task/git-clone/0.2/git-clone.yaml
 kubectl apply --filename https://raw.githubusercontent.com/tektoncd/catalog/master/task/maven/0.2/maven.yaml
 # kubectl apply --filename https://raw.githubusercontent.com/tektoncd/catalog/master/task/kaniko/0.1/kaniko.yaml
-kubectl apply --filename kaniko-0.1-custom.yaml
-kubectl apply --filename verify-image-digest.yaml
+kubectl apply --filename kaniko-0.1-custom-task.yaml
+kubectl apply --filename verify-image-digest-task.yaml
 # kubectl apply --filename https://raw.githubusercontent.com/tektoncd/catalog/master/task/kubernetes-actions/0.1/kubernetes-actions.yaml
 kubectl apply --filename kustomize-cli-task.yaml
 kubectl apply --filename https://raw.githubusercontent.com/tektoncd/catalog/master/task/git-cli/0.1/git-cli.yaml
@@ -14,10 +14,12 @@ kubectl apply --filename https://raw.githubusercontent.com/tektoncd/catalog/mast
 # kubectl apply --filename github-secret.yaml
 # kubectl apply --filename github-serviceaccount.yaml
 
-kubectl apply --filename gitlab-secret.yaml
-kubectl apply --filename gitlab-serviceaccount.yaml
-kubectl apply --filename gitlab-role.yaml
-kubectl apply --filename gitlab-rolebinding.yaml
+kubectl apply --filename gitlab-cd-secret.yaml
+kubectl apply --filename gitlab-cd-serviceaccount.yaml
+kubectl apply --filename gitlab-ci-secret.yaml
+kubectl apply --filename gitlab-ci-serviceaccount.yaml
+kubectl apply --filename gitlab-ci-role.yaml
+kubectl apply --filename gitlab-ci-rolebinding.yaml
 kubectl apply --filename gitlab-trigger.yaml
 kubectl apply --filename gitlab-eventlistener-ingress.yaml
 
@@ -26,7 +28,6 @@ kubectl apply --filename maven-configmap.yaml
 kubectl apply --filename input-asset-configmap.yaml
 kubectl apply --filename kube-configmap.yaml
 kubectl apply --filename pvc.yaml
-kubectl apply --filename java-pipeline.yaml
+kubectl apply --filename pipeline.yaml
 
-# kubectl create --filename run.yaml
 # tkn pipelineruns logs --follow --last
