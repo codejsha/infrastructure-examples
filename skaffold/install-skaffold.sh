7b03b0bb888f7ddb56d58ee530a0811004dedb1c
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-10-05 03:24:05 +0900

    add skaffold install

---
+++ skaffold/install-skaffold.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+# https://skaffold.dev/docs/install/
+
+curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && \
+sudo install skaffold /usr/local/bin/
