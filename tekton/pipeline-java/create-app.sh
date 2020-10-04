51a0b55ca21f74c922ba00310cd28171c3173f38
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-10-04 16:33:14 +0900

    add create app, proj scripts

---
+++ tekton/pipeline-java/create-app.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+kubectl apply --filename app.yaml
---
+++ tekton/pipeline-java/create-appproj.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+UUID="$(uuidgen)"
+SECONDS_SINCE="$(date +%s)"
+
+sed -i "s/<UUID>/${UUID}/g" appproj.yaml
+sed -i "s/<ISSUED_AT>/${SECONDS_SINCE}/g" appproj.yaml
+
+kubectl apply --filename appproj.yaml
