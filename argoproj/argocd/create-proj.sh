adbb6a29308f480fa1ffb917657141c851741e4b
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-10-04 11:29:35 +0900

    fix error

--- argoproj/argocd/create-proj.sh
+++ argoproj/argocd/create-proj.sh
@@ -8,6 +8,6 @@ GROUP="*"
 KIND="*"
 
 argocd proj create ${PROJECT}
-argocd proj add-destination PROJECT ${SERVER} ${NAMESPACE}
+argocd proj add-destination ${PROJECT} ${SERVER} ${NAMESPACE}
 argocd proj add-source ${PROJECT} ${URL}
 argocd proj allow-cluster-resource ${PROJECT} ${GROUP} ${KIND}
