a821b2a738d914ce1fa6dbb89fdebe9e931ed466
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-10-09 12:01:34 +0900

    update docker mount options

--- docker/docker-mount-option.sh
+++ docker/docker-mount-option.sh
@@ -1,20 +1,24 @@
-### additional mount options
+### Docker mount options
 
 ######################################################################
 
-### bind
-
---mount type=bind,src=F:\share,dst=/mnt/share `
---mount type=bind,src=F:\storage,dst=/mnt/storage `
+### Bind
 
 --mount type=bind,src=/mnt/share,dst=/mnt/share \
 --mount type=bind,src=/mnt/storage,dst=/mnt/storage \
 --mount type=bind,src=/mnt/volume/log,dst=/var/log \
 
+### for windows:
+# --mount type=bind,src=F:\share,dst=/mnt/share `
+# --mount type=bind,src=F:\storage,dst=/mnt/storage `
+
+
 ######################################################################
 
-### volume
+### Volume
 
 docker volume create vol1
---mount type=volume,src=vol1,dst=/var/log `
 --mount type=volume,src=vol1,dst=/var/log \
+
+### for windows:
+# --mount type=volume,src=vol1,dst=/var/log `
