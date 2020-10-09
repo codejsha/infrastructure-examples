b44e64ee5b155ba2af40c68f53ec2f5a066cc81a
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-10-09 12:01:52 +0900

    update docker service

--- docker/docker-service.sh
+++ docker/docker-service.sh
@@ -3,7 +3,7 @@
 cat <<EOF | sudo tee /etc/systemd/system/docker.service.d/override.conf
 [Service]
 ExecStart=
-ExecStart=/usr/bin/dockerd --host tcp://0.0.0.0:2375 --host unix:///var/run/docker.sock
+ExecStart=/usr/bin/dockerd --host unix:///var/run/docker.sock --host tcp://0.0.0.0:2375
 EOF
 
 systemctl daemon-reload
