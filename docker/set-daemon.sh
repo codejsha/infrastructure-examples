74b274f7408b54655e656f4b7e511731f503f259
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-10-10 08:55:56 +0900

    add docker daemon setting

---
+++ docker/set-daemon.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "insecure-registries": ["registry.example.com", "core.harbor.example.com", "kraken.example.com", "nexus.example.com"],
+  "registry-mirrors": ["https://nexus.example.com", "http://nexus.example.com"]
+}
+EOF
+
+systemctl daemon-reload
+systemctl restart docker.service
