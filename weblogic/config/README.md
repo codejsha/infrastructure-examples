211bee6260f994ffb28b50b717ebc7ae2810e54b
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-10-13 04:31:10 +0900

    update weblogic config

---
+++ weblogic/config/README.md
@@ -0,0 +1,46 @@
+# WebLogic Configuration
+
+## All
+
+```bash
+bash ./all.sh
+```
+
+## Domain
+
+- [conf-domain.sh](/weblogic/config/conf-domain.sh)
+- [set-connection-filter.sh](/weblogic/config/set-connection-filter.sh)
+- [set-domain-config-archive.sh](/weblogic/config/set-domain-config-archive.sh)
+- [set-domain-cookie-name.sh](/weblogic/config/set-domain-cookie-name.sh)
+- [set-domain-log.sh](/weblogic/config/set-domain-log.sh)
+
+## Identity
+
+- [create-boot-properties.sh](/weblogic/config/create-boot-properties.sh)
+- [create-change-password-script.sh](/weblogic/config/create-change-password-script.sh)
+- [create-store-user-config-scripts.sh](/weblogic/config/create-store-user-config-scripts.sh)
+- [create-change-store-user-config-script.sh](/weblogic/config/create-change-store-user-config-script.sh)
+- [create-encrypt-password-script.sh](/weblogic/config/create-encrypt-password-script.sh)
+
+## Server
+
+- [create-server.sh](/weblogic/config/create-server.sh)
+- [create-scripts.sh](/weblogic/config/create-scripts.sh)
+
+## Cluster
+
+- [create-cluster.sh](/weblogic/config/create-cluster.sh)
+
+## Data source
+
+- [create-datasource.sh](/weblogic/config/create-datasource.sh)
+
+## Node manager
+
+- [create-nodemgr.sh](/weblogic/config/create-nodemgr.sh)
+- [create-nodemgr-scripts.sh](/weblogic/config/create-nodemgr-scripts.sh)
+- [create-regen-demo-identity-script.sh](/weblogic/config/create-regen-demo-identity-script.sh)
+
+## Application
+
+- [deploy-app.sh](/weblogic/config/deploy-app.sh)
--- weblogic/config/all.sh
+++ weblogic/config/all.sh
@@ -3,70 +3,73 @@
 ######################################################################
 
 ### CREATE NODEMANAGER
-###
-### ./create-nodemgr.sh NODEMGR_NAME NODEMGR_ADDRESS NODEMGR_PORT
+### bash ./create-nodemgr.sh ${NODEMGR_NAME} ${NODEMGR_ADDRESS} ${NODEMGR_PORT}
+### bash ./create-nodemgr-scripts.sh ${NODEMGR_NAME} ${NODEMGR_ADDRESS} ${NODEMGR_PORT}
 ###
 
-./create-nodemgr.sh BaseMachine1 test.example.com 5556
-# ./create-nodemgr.sh BaseMachine2 test.example.com 5557
+bash ./create-nodemgr.sh BaseMachine1 test.example.com 5556
+# bash ./create-nodemgr.sh BaseMachine2 test.example.com 5557
+bash ./create-nodemgr-scripts.sh BaseMachine1 test.example.com 5556
+# bash ./create-nodemgr-scripts.sh BaseMachine2 test.example.com 5557
 
 ######################################################################
 
 ### CREATE CLUSTER
-###
-### ./create-cluster.sh CLUSTER_NAME UNICAST
-### ./create-cluster.sh CLUSTER_NAME MULTICAST MULTICAST_ADDRESS MULTICAST_PORT
+### bash ./create-cluster.sh ${CLUSTER_NAME} ${UNICAST}
+### bash ./create-cluster.sh ${CLUSTER_NAME} ${MULTICAST} ${MULTICAST_ADDRESS} ${MULTICAST_PORT}
 ###
 
-./create-cluster.sh BaseCluster1 unicast
-# ./create-cluster.sh BaseCluster2 multicast 239.192.0.0 7001
-# ./create-cluster.sh BaseJmsCluster1 unicast
-# ./create-cluster.sh BaseJmsCluster2 multicast 239.192.0.0 7001
+bash ./create-cluster.sh BaseCluster1 unicast
+# bash ./create-cluster.sh BaseCluster2 multicast 239.192.0.0 7001
+# bash ./create-cluster.sh BaseJmsCluster1 unicast
+# bash ./create-cluster.sh BaseJmsCluster2 multicast 239.192.0.0 7001
 
 ######################################################################
 
 ### CREATE SERVER
-###
-### ./create-server.sh MANAGED_SERVER_NAME MANAGED_SERVER_ADDRESS MANAGED_SERVER_PORT
-### ./create-server.sh MANAGED_SERVER_NAME MANAGED_SERVER_ADDRESS MANAGED_SERVER_PORT CLUSTER_NAME
-### ./create-server.sh MANAGED_SERVER_NAME MANAGED_SERVER_ADDRESS MANAGED_SERVER_PORT CLUSTER_NAME NODEMGR_NAME
+### bash ./create-server.sh ${MANAGED_SERVER_NAME} ${MANAGED_SERVER_ADDRESS} ${MANAGED_SERVER_PORT}
+### bash ./create-server.sh ${MANAGED_SERVER_NAME} ${MANAGED_SERVER_ADDRESS} ${MANAGED_SERVER_PORT} ${CLUSTER_NAME}
+### bash ./create-server.sh ${MANAGED_SERVER_NAME} ${MANAGED_SERVER_ADDRESS} ${MANAGED_SERVER_PORT} ${CLUSTER_NAME} ${NODEMGR_NAME}
+### bash ./create-scripts.sh ${SERVER_NAME}
 ###
 
-# ./create-server.sh ManagedServer1 test.example.com 7003
-# ./create-server.sh ManagedServer2 test.example.com 7004
-# ./create-server.sh ManagedServer1 test.example.com 7003 BaseCluster1
-# ./create-server.sh ManagedServer2 test.example.com 7004 BaseCluster1
-./create-server.sh ManagedServer1 test.example.com 7003 BaseCluster1 BaseMachine1
-./create-server.sh ManagedServer2 test.example.com 7004 BaseCluster1 BaseMachine1
-# ./create-server.sh JmsManagedServer1 test.example.com 7005 BaseJmsCluster1
-# ./create-server.sh JmsManagedServer2 test.example.com 7006 BaseJmsCluster1
+# bash ./create-server.sh ManagedServer1 test.example.com 7003
+# bash ./create-server.sh ManagedServer2 test.example.com 7004
+# bash ./create-server.sh ManagedServer1 test.example.com 7003 BaseCluster1
+# bash ./create-server.sh ManagedServer2 test.example.com 7004 BaseCluster1
+bash ./create-server.sh ManagedServer1 test.example.com 7003 BaseCluster1 BaseMachine1
+bash ./create-server.sh ManagedServer2 test.example.com 7004 BaseCluster1 BaseMachine1
+# bash ./create-server.sh JmsManagedServer1 test.example.com 7005 BaseJmsCluster1
+# bash ./create-server.sh JmsManagedServer2 test.example.com 7006 BaseJmsCluster1
+bash ./create-scripts.sh ManagedServer1
+bash ./create-scripts.sh ManagedServer2
+# bash ./create-scripts.sh JmsManagedServer1
+# bash ./create-scripts.sh JmsManagedServer2
 
 ######################################################################
 
 ### DEPLOY APPLICATION
-###
-### ./deploy-app.sh APP_NAME APP_PATH APP_TARGET
+### bash ./deploy-app.sh ${APP_NAME} ${APP_PATH} ${APP_TARGET}
 ###
 
-./deploy-app.sh testweb /svc/app/testweb BaseCluster1
-# ./deploy-app.sh sample1 /svc/app/sample1 ManagedServer1
-# ./deploy-app.sh sample2 /svc/app/sample2 ManagedServer2
+bash ./deploy-app.sh testweb /svc/app/testweb BaseCluster1
+# bash ./deploy-app.sh sample1 /svc/app/sample1 ManagedServer1
+# bash ./deploy-app.sh sample2 /svc/app/sample2 ManagedServer2
 
 ######################################################################
 
 ### CREATE DATASOURCE
-### 
-### ./create-datasource.sh DS_NAME DS_JNDI \
-###     DS_URL \
-###     DS_DRIVER DS_USER DS_PASSWORD DS_INIT DS_MIN DS_MAX 
-###     DS_TARGET_TYPE DS_TARGET
-### 
+### bash ./create-datasource.sh ${DS_NAME} ${DS_JNDI} \
+###     ${DS_URL} \
+###     ${DS_DRIVER} ${DS_USER} ${DS_PASSWORD} ${DS_INIT} ${DS_MIN} ${DS_MAX}
+###     ${DS_TARGET_TYPE} ${DS_TARGET}
+###
 
-./create-datasource.sh BaseDataSource1 "baseds1" \
+bash ./create-datasource.sh BaseDataSource1 "baseds1" \
     jdbc:oracle:thin:@192.168.137.1:1521:orclcdb \
-    oracle.jdbc.OracleDriver system PASSWORD 1 1 15 
+    oracle.jdbc.OracleDriver system PASSWORD 1 1 15
     Cluster "BaseCluster1"
-# ./create-datasource.sh BaseDataSource1 "baseds1" \
+# bash ./create-datasource.sh BaseDataSource1 "baseds1" \
 #     jdbc:oracle:thin:@192.168.137.1:1521:orclcdb \
-#     oracle.jdbc.OracleDriver system PASSWORD 1 1 15 
+#     oracle.jdbc.OracleDriver system PASSWORD 1 1 15
 #     Server "ManagedServer1"
