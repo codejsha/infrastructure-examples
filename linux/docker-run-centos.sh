ca8f54f4e704fbf78f0e1a8659298acb5bdd1600
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-11-04 04:05:35 +0900

    add docker mount options

--- linux/docker-run-centos.sh
+++ linux/docker-run-centos.sh
@@ -5,6 +5,8 @@ docker run \
     --name centos8 \
     --interactive \
     --tty \
+    --mount type=bind,src=/mnt/share,dst=/mnt/share \
+    --mount type=bind,src=/mnt/storage,dst=/mnt/storage \
     centos:8.2.2004
 
 ######################################################################
@@ -13,6 +15,8 @@ docker run \
     --name centos7 \
     --interactive \
     --tty \
+    --mount type=bind,src=/mnt/share,dst=/mnt/share \
+    --mount type=bind,src=/mnt/storage,dst=/mnt/storage \
     centos:7.8.2003
 
 ######################################################################
@@ -21,4 +25,6 @@ docker run \
     --name centos6 \
     --interactive \
     --tty \
+    --mount type=bind,src=/mnt/share,dst=/mnt/share \
+    --mount type=bind,src=/mnt/storage,dst=/mnt/storage \
     centos:6.10
