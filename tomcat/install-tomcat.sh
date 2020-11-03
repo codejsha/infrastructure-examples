0ea69beb05cdcba133dd58c80d7c3e521d4b3ec8
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-11-04 04:06:26 +0900

    update tomcat install

--- tomcat/install-tomcat.sh
+++ tomcat/install-tomcat.sh
@@ -1,15 +1,33 @@
 #!/usr/bin/bash
 
 # TOMCAT_MAJOR_VERSION="8"
-# TOMCAT_VERSION="8.5.57"
+# TOMCAT_VERSION="8.5.59"
 TOMCAT_MAJOR_VERSION="9"
-TOMCAT_VERSION="9.0.37"
+TOMCAT_VERSION="9.0.39"
 # TOMCAT_MAJOR_VERSION="10"
-# TOMCAT_VERSION="10.0.0-M7"
+# TOMCAT_VERSION="10.0.0-M9"
 
 PARENT_TOMCAT_HOME="/svc/appsvr"
+INSTALL_FILE_DIR="/mnt/share/apache-tomcat"
 
-curl -o ${PARENT_TOMCAT_HOME}/apache-tomcat-${TOMCAT_VERSION}.tar.gz -LJO http://mirror.apache-kr.org/tomcat/tomcat-${TOMCAT_MAJOR_VERSION}/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz
-tar -C ${PARENT_TOMCAT_HOME} -xzf ${PARENT_TOMCAT_HOME}/apache-tomcat-${TOMCAT_VERSION}.tar.gz
-mv ${PARENT_TOMCAT_HOME}/apache-tomcat-${TOMCAT_VERSION} ${PARENT_TOMCAT_HOME}/tomcat${TOMCAT_MAJOR_VERSION}
-rm -f ${PARENT_TOMCAT_HOME}/apache-tomcat-${TOMCAT_VERSION}.tar.gz
+######################################################################
+
+function install_from_online {
+    curl -o ${PARENT_TOMCAT_HOME}/apache-tomcat-${TOMCAT_VERSION}.tar.gz \
+        -LJO http://mirror.apache-kr.org/tomcat/tomcat-${TOMCAT_MAJOR_VERSION}/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz
+    tar -C ${PARENT_TOMCAT_HOME} -xzf ${PARENT_TOMCAT_HOME}/apache-tomcat-${TOMCAT_VERSION}.tar.gz
+    mv ${PARENT_TOMCAT_HOME}/apache-tomcat-${TOMCAT_VERSION} ${PARENT_TOMCAT_HOME}/tomcat${TOMCAT_MAJOR_VERSION}
+    rm -f ${PARENT_TOMCAT_HOME}/apache-tomcat-${TOMCAT_VERSION}.tar.gz
+}
+
+function install_from_storage {
+    cp ${INSTALL_FILE_DIR}/apache-tomcat-${TOMCAT_VERSION}.tar.gz ${PARENT_TOMCAT_HOME}
+    tar -C ${PARENT_TOMCAT_HOME} -xzf ${PARENT_TOMCAT_HOME}/apache-tomcat-${TOMCAT_VERSION}.tar.gz
+    mv ${PARENT_TOMCAT_HOME}/apache-tomcat-${TOMCAT_VERSION} ${PARENT_TOMCAT_HOME}/tomcat${TOMCAT_MAJOR_VERSION}
+    rm -f ${PARENT_TOMCAT_HOME}/apache-tomcat-${TOMCAT_VERSION}.tar.gz
+}
+
+######################################################################
+
+# install_from_online
+install_from_storage
