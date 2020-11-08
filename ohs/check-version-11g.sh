5387441cc3501a644746478faf114c89e045832f
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-11-08 23:44:02 +0900

    update syntax

--- ohs/check-version-11g.sh
+++ ohs/check-version-11g.sh
@@ -5,11 +5,11 @@ ORACLE_HOME="${MW_HOME}/oracle_wt1"
 
 ######################################################################
 
-function check_version_by_directory {
+function check_version_with_directory {
     ls ${ORACLE_HOME}/inventory/Components21/oracle.ohs2
 }
 
-function check_version_by_httpd {
+function check_version_with_httpd {
     export ORACLE_HOME=${ORACLE_HOME}
     export LD_LIBRARY_PATH=${ORACLE_HOME}/lib:${ORACLE_HOME}/ohs/lib:${ORACLE_HOME}/oracle_common/lib
     ${ORACLE_HOME}/ohs/bin/httpd -V
@@ -17,5 +17,5 @@ function check_version_by_httpd {
 
 ######################################################################
 
-# check_version_by_directory
-check_version_by_httpd
+# check_version_with_directory
+check_version_with_httpd
--- ohs/check-version-12c.sh
+++ ohs/check-version-12c.sh
@@ -4,11 +4,11 @@ ORACLE_HOME="/usr/local/ohs"
 
 ######################################################################
 
-function check_version_by_directory {
+function check_version_with_directory {
     ls ${ORACLE_HOME}/inventory/Components/oracle.ohs2
 }
 
-function check_version_by_httpd {
+function check_version_with_httpd {
     export ORACLE_HOME=${ORACLE_HOME}
     export LD_LIBRARY_PATH=${ORACLE_HOME}/lib:${ORACLE_HOME}/ohs/lib:${ORACLE_HOME}/oracle_common/lib
     ${ORACLE_HOME}/ohs/bin/httpd -V
@@ -16,5 +16,5 @@ function check_version_by_httpd {
 
 ######################################################################
 
-# check_version_by_directory
-check_version_by_httpd
+# check_version_with_directory
+check_version_with_httpd
