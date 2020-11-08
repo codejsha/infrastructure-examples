2d54375e14d1b4dcb117f908a4bf67fc5091a26a
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-11-09 00:04:30 +0900

    add ntp install

---
+++ linux/install-ntp.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+######################################################################
+
+function install_ntp_with_yum {
+    sudo yum install -y ntp
+}
+
+function install_ntp_with_dnf {
+    sudo dnf install -y ntp
+}
+
+function install_ntp_with_aptget {
+    sudo apt-get install ntp
+}
+
+######################################################################
+
+install_ntp_with_yum
+# install_ntp_with_dnf
+# install_ntp_with_aptget
