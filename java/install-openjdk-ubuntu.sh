cace04a8fdb6a43ae55f2aee20ca3a865b1aad6f
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-11-08 23:39:51 +0900

    update openjdk install

--- java/install-openjdk-ubuntu.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-sudo apt-get install openjdk-8-jdk
-# sudo apt-get install openjdk-11-jdk
--- java/install-openjdk.sh
+++ java/install-openjdk.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/bash
 
-sudo yum install -y java-1.8.0-openjdk-devel
-sudo yum install -y java-11-openjdk-devel
+######################################################################
+
+function install_openjdk_with_yum {
+    sudo yum install -y java-1.8.0-openjdk-devel
+    sudo yum install -y java-11-openjdk-devel
+}
+
+function install_openjdk_with_aptget {
+    sudo apt-get install openjdk-8-jdk
+    sudo apt-get install openjdk-11-jdk
+}
+
+######################################################################
+
+install_openjdk_with_yum
+install_openjdk_with_aptget
