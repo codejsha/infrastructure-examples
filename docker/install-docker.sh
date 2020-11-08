0181b2e513110a2f4315890efb4a0ec4e7afc3aa
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-11-08 23:39:20 +0900

    update docker install

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -1,27 +1,43 @@
 #!/usr/bin/bash
 
-sudo yum install -y yum-utils
-sudo yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce docker-ce-cli containerd.io
+######################################################################
 
-sudo usermod -a -G docker ${USER}
-sudo systemctl enable docker
-sudo systemctl start docker
+function install_docker_with_yum {
+    sudo yum install -y yum-utils
+    sudo yum-config-manager \
+        --add-repo \
+        https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum install -y docker-ce docker-ce-cli containerd.io
 
-# sudo sed -i -E '/^root\tALL=\(ALL\) \tALL$/ a ${USER}\tALL=(ALL) \tALL' /etc/sudoers
-exit
+    sudo usermod -a -G docker ${USER}
+    sudo systemctl enable docker
+    sudo systemctl start docker
+
+    # sudo sed -i -E '/^root\tALL=\(ALL\) \tALL$/ a ${USER}\tALL=(ALL) \tALL' /etc/sudoers
+    exit
+}
+
+function install_docker_with_dnf {
+    sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+    sudo dnf install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.10-3.2.el7.x86_64.rpm
+    sudo dnf install -y docker-ce
+
+    sudo usermod -a -G docker ${USER}
+    sudo systemctl enable --now docker
+    sudo systemctl status docker
+
+    # sudo sed -i -E '/^root\tALL=\(ALL\) \tALL$/ a ${USER}\tALL=(ALL) \tALL' /etc/sudoers
+    exit
+}
+
+function install_docker_with_aptget {
+    curl -fsSL https://get.docker.com | sudo sh
+
+    sudo usermod -a -G docker ${USER}
+}
 
 ######################################################################
 
-# sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-# sudo dnf install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.10-3.2.el7.x86_64.rpm
-# sudo dnf install -y docker-ce
-#
-# sudo usermod -a -G docker ${USER}
-# sudo systemctl enable --now docker
-# sudo systemctl status docker
-#
-# sudo sed -i -E '/^root\tALL=\(ALL\) \tALL$/ a ${USER}\tALL=(ALL) \tALL' /etc/sudoers
-# exit
+install_docker_with_yum
+# install_docker_with_dnf
+# install_docker_with_aptget
