1684ada7612c10589bcf7ed9069b325bfa4f5051
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-11-12 00:17:21 +0900

    add nodejs

---
+++ nodejs/install-nodejs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+# https://github.com/nodesource/distributions/blob/master/README.md
+
+### As root
+# curl -sL https://rpm.nodesource.com/setup_lts.x | bash -
+### No root privileges
+curl -sL https://rpm.nodesource.com/setup_lts.x | sudo bash -
+
+sudo yum remove -y nodejs npm
+sudo yum install -y nodejs
+
+### to build native addons
+# sudo yum install -y gcc-c++ make
---
+++ nodejs/install-yarn.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+sudo yum install -y yarn
