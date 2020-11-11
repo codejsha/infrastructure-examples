a94c71b8c7cf5f77d0cf4cfc899c4e087ba5a7cd
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-11-11 23:47:47 +0900

    fix error

--- wildfly/filter/add-filter.sh
+++ wildfly/filter/add-filter.sh
@@ -10,9 +10,6 @@ function add_filter {
         --controller="${BIND_ADDRESS_MGMT}:${JBOSS_MGMT_HTTP_PORT}" \
 <<EOF
 batch
-/subsystem=undertow/configuration=filter/response-header=x-powered-by-header:add(header-name=X-Powered-By,header-value=Undertow/1)
-/subsystem=undertow/server=default-server/host=default-host/filter-ref=x-powered-by-header:add()
-
 # /subsystem=undertow/configuration=filter/custom-filter=example-handler:add(class-name=com.example.ExampleHandler,module=com.example.undertow.handler)
 # /subsystem=undertow/server=default-server/host=default-host/filter-ref=example-handler:add()
 run-batch
--- wildfly/filter/hide-server-header-info.sh
+++ wildfly/filter/hide-server-header-info.sh
@@ -10,6 +10,8 @@ function add_filter {
         --controller="${BIND_ADDRESS_MGMT}:${JBOSS_MGMT_HTTP_PORT}" \
 <<EOF
 batch
+/subsystem=undertow/configuration=filter/response-header=x-powered-by-header:add(header-name=X-Powered-By,header-value=Undertow/1)
+/subsystem=undertow/server=default-server/host=default-host/filter-ref=x-powered-by-header:add()
 /subsystem=undertow/configuration=filter/response-header=server-header:add(header-name=Server,header-value=WildFly)
 /subsystem=undertow/server=default-server/host=default-host/filter-ref=server-header:add()
 run-batch
