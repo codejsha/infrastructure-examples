4731248d5e3892e70a610b317148c28f747fcc04
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-11-11 21:32:47 +0900

    update docker install

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -3,11 +3,50 @@
 ######################################################################
 
 function install_docker_with_yum {
+    sudo yum remove docker \
+        docker-client \
+        docker-client-latest \
+        docker-common \
+        docker-latest \
+        docker-latest-logrotate \
+        docker-logrotate \
+        docker-engine
+
     sudo yum install -y yum-utils
     sudo yum-config-manager \
         --add-repo \
         https://download.docker.com/linux/centos/docker-ce.repo
     sudo yum install -y docker-ce docker-ce-cli containerd.io
+
+    sudo usermod -a -G docker ${USER}
+    sudo systemctl enable docker
+    sudo systemctl start docker
+
+    # sudo sed -i -E '/^root\tALL=\(ALL\) \tALL$/ a ${USER}\tALL=(ALL) \tALL' /etc/sudoers
+    exit
+}
+
+function install_docker_specific_version_with_yum {
+    DOCKER_VERSION="19.03.13"
+    CONTAINERD_VERSION="1.3.7"
+
+    sudo yum remove docker \
+        docker-client \
+        docker-client-latest \
+        docker-common \
+        docker-latest \
+        docker-latest-logrotate \
+        docker-logrotate \
+        docker-engine
+
+    sudo yum install -y yum-utils
+    sudo yum-config-manager \
+        --add-repo \
+        https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum install -y \
+        docker-ce-${DOCKER_VERSION} \
+        docker-ce-cli-${DOCKER_VERSION} \
+        containerd.io-${CONTAINERD_VERSION}
 
     sudo usermod -a -G docker ${USER}
     sudo systemctl enable docker
