f4cc45bfd9b776713fc9338b3d47b2d5c5884d10
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-11-13 01:01:31 +0900

    fix error

--- ohs/create-scripts-12c.sh
+++ ohs/create-scripts-12c.sh
@@ -13,7 +13,7 @@ cat <<EOF > "${DOMAIN_HOME}/start-nodemanager.sh"
 
 DOMAIN_HOME="${DOMAIN_HOME}"
 LOG_DIR="${LOG_DIR}"
-GET_DATE="$(date +'%Y%m%d_%H%M%S')"
+GET_DATE="\$(date +'%Y%m%d_%H%M%S')"
 
 PID="\$(pgrep -xa java | grep \${DOMAIN_HOME} | grep NodeManager | awk '{print \$1}')"
 if [ -n "\${PID}" ]; then
