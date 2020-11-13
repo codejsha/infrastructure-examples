cffcd4dc856e951e715d3ff653581a7357c06135
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-11-13 18:13:50 +0900

    update git install

--- github/install-git.sh
+++ github/install-git.sh
@@ -2,9 +2,11 @@
 
 GIT_VERSION="2.28.0"
 
+sudo yum install -y epel-release
+sudo yum install -y gcc autoconf
 sudo yum install -y dh-autoreconf curl-devel expat-devel gettext-devel openssl-devel perl-devel zlib-devel
 sudo yum install -y asciidoc xmlto docbook2X
-sudo ln -s /usr/bin/db2x_docbook2texi /usr/bin/docbook2x-texi
+sudo ln -snf /usr/bin/db2x_docbook2texi /usr/bin/docbook2x-texi
 
 curl -LJO https://github.com/git/git/archive/v${GIT_VERSION}.tar.gz
 tar -zxf git-${GIT_VERSION}.tar.gz
@@ -13,3 +15,5 @@ make configure
 ./configure --prefix=/usr
 make all doc info
 sudo make install install-doc install-html install-info
+
+rm -f git-${GIT_VERSION}.tar.gz
