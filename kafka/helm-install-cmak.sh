9ff1835f91bba63b1ddee4ab048b2428d34ef3f0
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-11-23 16:36:50 +0900

    update syntax

--- kafka/helm-install-cmak.sh
+++ kafka/helm-install-cmak.sh
@@ -6,6 +6,7 @@ NAMESPACE="kafka-system"
 
 # helm install my-cmak-operator \
 helm upgrade --install my-cmak-operator \
+    --create-namespace \
     --values cmak-values.yaml \
     --version 0.6.2 \
     cmak-operator/cmak-operator
