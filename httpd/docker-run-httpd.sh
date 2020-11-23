12b47bc533ea922dcd51cd885f2398a0000152b1
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-11-23 13:04:16 +0900

    update httpd docker

--- httpd/docker-run-httpd.sh
+++ httpd/docker-run-httpd.sh
@@ -2,9 +2,13 @@
 # https://hub.docker.com/_/httpd
 # https://hub.docker.com/r/centos/httpd-24-centos7
 
+sudo mkdir -p /mnt/volume/httpd/htdocs
+
 docker run \
     --detach \
-    --name my-httpd \
+    --name httpd24 \
     --publish 8080:80 \
     --mount type=bind,source=/mnt/volume/httpd/htdocs,target=/usr/local/apache2/htdocs \
+    --mount type=bind,src=/mnt/share,dst=/mnt/share \
+    --mount type=bind,src=/mnt/storage,dst=/mnt/storage \
     httpd:2.4
