a045f6ec09d949b761672d56c13cb6a92759341e
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-11-25 22:40:26 +0900

    add iplanet requirement packages

--- iplanet/install-iplanet.sh
+++ iplanet/install-iplanet.sh
@@ -3,4 +3,8 @@
 INSTALL_FILE_DIR="/mnt/share/oracle-iplanet-web-server/Oracle-iPlanet-Web-Server-7.0.27-linux-x64"
 INSTALL_SCRIPT_DIR="/svc/infrastructure/iplanet"
 
+sudo yum install -y \
+    glibc.i686 \
+    compat-libstdc++-33.x86_64
+
 ${INSTALL_FILE_DIR}/setup --silent ${INSTALL_SCRIPT_DIR}/statefile
