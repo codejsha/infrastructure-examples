c39b3fc59249bda4e9a9638cb5aecb3c05fecfff
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-01 01:10:37 +0900

    update debug

--- oracledb/debug.sh
+++ oracledb/debug.sh
@@ -1,32 +1,6 @@
 ### jdbc debug
+
 EXT_PRE_CLASSPATH="${EXT_PRE_CLASSPATH}:jdbc_g.jar"
 JAVA_OPTIONS="${JAVA_OPTIONS} -Doracle.jdbc.Trace=true"
-JAVA_OPTIONS="${JAVA_OPTIONS} -Djava.util.logging.config.file=${DOMAIN_HOME}/oracledb-logging.properties"
-# JAVA_OPTIONS="${JAVA_OPTIONS} -Doracle.jdbc.LogFile=${LOG_DIR}/jdbc.log"
-
-cat <<EOF > ${DOMAIN_HOME}/oracledb-logging.properties
-# handlers=java.util.logging.ConsoleHandler
-# java.util.logging.ConsoleHandler.level=ALL
-# java.util.logging.ConsoleHandler.formatter=java.util.logging.SimpleFormatter
-
-handlers=java.util.logging.FileHandler
-java.util.logging.FileHandler.level=ALL
-java.util.logging.FileHandler.pattern=jdbc%g.log
-java.util.logging.FileHandler.count=10000
-java.util.logging.FileHandler.formatter=java.util.logging.SimpleFormatter
-
-oracle.jdbc.connector.level=FINEST
-oracle.jdbc.driver.level=FINEST
-oracle.jdbc.internal.level=FINEST
-oracle.jdbc.oci.level=FINEST
-oracle.jdbc.oracore.level=FINEST
-oracle.jdbc.pool.level=FINEST
-oracle.jdbc.rowset.level=FINEST
-oracle.jdbc.util.level=FINEST
-oracle.jdbc.xa.level=FINEST
-oracle.jdbc.xa.client.level=FINEST
-oracle.jpub.level=FINEST
-oracle.net.level=FINEST
-oracle.sql.level=FINEST
-oracle.sql.converter.level=FINEST
-EOF
+JAVA_OPTIONS="${JAVA_OPTIONS} -Djava.util.logging.config.file=${DOMAIN_HOME}/jdbc-driver-debug.properties"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Doracle.jdbc.LogFile=${LOG_DIR}/jdbc.log"
--- weblogic/debug.sh
+++ weblogic/debug.sh
@@ -7,6 +7,7 @@ JAVA_OPTIONS="${JAVA_OPTIONS} -verbose:jni"
 
 ### debug
 JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.log.StdoutSeverity=Debug"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.StdoutDebugEnabled=true"
 
 ### debug all
 JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.log.StdoutSeverity=Debug"
@@ -16,24 +17,84 @@ JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.StdoutDebugEnabled=true"
 
 ######################################################################
 
-### ssl debug
+### deployment
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugDeployment=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugDeploymentServiceInternal=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugDeploymentServiceTransport=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugDeploymentServiceTransportHttp=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugDeploymentServiceStatusUpdates=true"
+
+######################################################################
+
+### application container
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugAppContainer=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.descriptor.munger=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.descriptor.merge=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugLibraries=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugClassRedef=true"
+
+######################################################################
+
+### classloading
+
+### verbose
+JAVA_OPTIONS="${JAVA_OPTIONS} -verbose:class"
+
+### 12.1.2 and earlier
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.utils.classloaders.GenericClassLoader.Verbose=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.utils.classloaders.FilteringClassLoader.Verbose=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.utils.classloaders.ChangeAwareClassLoader.Verbose=true"
+
+### 12.1.3 and higher
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugClassLoadingVerbose=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugClassLoadingContextualTrace=true"
+
+######################################################################
+
+### ssl
 JAVA_OPTIONS="${JAVA_OPTIONS} -Djavax.net.debug=all"
 JAVA_OPTIONS="${JAVA_OPTIONS} -Dssl.debug=true"
 JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.StdoutDebugEnabled=true"
 
 ######################################################################
 
-### jta debug
+### jta
 
 ### xa
 JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.log.StdoutSeverity=Debug"
 JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugJTA2PC=true"
 JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugJTAXA=true"
 JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugJTAJDBC=true"
-### JDBCXADebugLevel=20  # connection pool properties
+### JDBCXADebugLevel=20         # connection pool properties (admin console)
 
 ### non-xa
 JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.log.StdoutSeverity=Debug"
 JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugJTANonXA=true"
 JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugJTAJDBC=true"
-### JDBCXADebugLevel=20  # connection pool properties
+### JDBCXADebugLevel=20         # connection pool properties (admin console)
+
+######################################################################
+
+### jms
+
+### pending message
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugJMSBackEnd=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugJMSFrontEnd=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugJMSCommon=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugJMSDispatcher=true"
+
+### pending message (transaction)
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugJMSBackEnd=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugJMSFrontEnd=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugJMSCommon=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugJMSDispatcher=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugJMSXA=true"
+
+### redelivery problem
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugJMSMessagePath=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.debug.DebugJMSXA=true"
+
+### message bridge
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.Debug.DebugMessagingBridgeStartup=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.Debug.DebugMessagingBridgeRuntime=true"
+JAVA_OPTIONS="${JAVA_OPTIONS} -Dweblogic.Debug.DebugMessagingBridgeRuntimeVerbose=true"
