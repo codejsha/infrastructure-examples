6f56783d360af8cd4f71c291bd9a6f90e43f10d2
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-02 06:24:11 +0900

    add prometheus

---
+++ prometheus/docker-run-prometheus.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PROMETHEUS_VOLUME_DIR="/mnt/volume/prometheus"
+sudo mkdir -p ${PROMETHEUS_VOLUME_DIR}
+sudo /usr/bin/cp -f prometheus.yaml ${PROMETHEUS_VOLUME_DIR}/prometheus.yml
+
+docker run \
+    --detach \
+    --name prometheus \
+    --publish 9090:9090 \
+    --mount type=bind,src=${PROMETHEUS_VOLUME_DIR},dst=/etc/prometheus \
+    prom/prometheus
---
+++ prometheus/helm-install-prometheus.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add stable https://charts.helm.sh/stable
+helm repo update
+
+NAMESPACE="prometheus-system"
+
+# helm install my-prometheus \
+helm upgrade --install my-prometheus \
+    --create-namespace \
+    --namespace ${NAMESPACE} \
+    --values prometheus-values.yaml \
+    --version 11.16.9 \
+    prometheus-community/prometheus
