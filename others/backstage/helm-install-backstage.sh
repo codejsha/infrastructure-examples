9c1faf16cde8b87f3be6e1ae7b4c526e0a87c7f3
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-02 05:57:17 +0900

    add backstage

---
+++ others/backstage/helm-install-backstage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+git clone https://github.com/spotify/backstage.git
+
+NAMESPACE="backstage-system"
+
+function helm_install_backstage {
+    helm install my-backstage \
+        --create-namespace \
+        --namespace ${NAMESPACE} \
+        --dependency-update \
+        --values values.yaml \
+        ./backstage/contrib/chart/backstage
+}
+
+function helm_upgrade_install_backstage {
+    helm upgrade --install my-backstage \
+        --create-namespace \
+        --namespace ${NAMESPACE} \
+        --values values.yaml \
+        ./backstage/contrib/chart/backstage
+}
+
+helm_install_backstage
+# helm_upgrade_install_backstage
