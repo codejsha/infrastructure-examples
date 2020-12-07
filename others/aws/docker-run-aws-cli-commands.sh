81960b107fd08044842014e206ef0b752ec8d0e5
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-07 15:12:55 +0900

    update aws cli commands

---
+++ others/aws/docker-run-aws-cli-commands.sh
@@ -0,0 +1,20 @@
+######################################################################
+
+docker run --rm -it amazon/aws-cli --version
+docker run --rm -it -v ~/.aws:/root/.aws amazon/aws-cli --version
+docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli --version
+
+######################################################################
+
+function aws() { echo "+ aws ${@}">&2; command aws ${@}; }
+alias aws="docker run --rm -it -v ~/.aws:/root/.aws -v \$(pwd):/aws amazon/aws-cli"
+
+######################################################################
+
+mkdir ~/.aws
+docker run --rm -it -v ~/.aws:/root/.aws amazon/aws-cli configure
+# aws configure
+### AWS Access Key ID [None]:
+### AWS Secret Access Key [None]:
+### Default region name [None]:
+### Default output format [None]:
