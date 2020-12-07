91b30ad068c93eefa7c5bbceb6737fba5b29ffca
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-07 14:58:58 +0900

    update helm cli commands

--- helm/docker-run-helm-cli.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# https://hub.docker.com/r/alpine/helm
-
-docker run -ti --rm -v $(pwd):/apps -v ~/.kube:/root/.kube -v ~/.helm:/root/.helm alpine/helm
-### function helm() { echo "+ helm ${@}">&2; command helm ${@}; }
-### alias helm="docker run -ti --rm -v $(pwd):/apps -v ~/.kube:/root/.kube -v ~/.helm:/root/.helm alpine/helm"
