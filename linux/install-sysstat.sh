7c7b87ec65bf865785dd8ec94eeae528f8095397
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-08 22:19:41 +0900

    add sysstat

---
+++ linux/install-sysstat.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+######################################################################
+
+function install_sysstat_with_yum {
+    sudo yum install -y sysstat
+}
+
+function install_sysstat_with_dnf {
+    sudo dnf install -y sysstat
+}
+
+function install_sysstat_with_aptget {
+    sudo apt-get install -y sysstat
+}
+
+######################################################################
+
+install_sysstat_with_yum
+# install_sysstat_with_dnf
+# install_sysstat_with_aptget
