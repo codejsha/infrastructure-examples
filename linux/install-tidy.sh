4f1ef8dee8a973fd9444f4537c1b35b38bc14c36
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-08 22:20:14 +0900

    add tidy

---
+++ linux/install-tidy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+######################################################################
+
+function install_tidy_with_yum {
+    sudo yum install -y tidy
+}
+
+function install_tidy_with_dnf {
+    sudo dnf install -y tidy
+}
+
+function install_tidy_with_aptget {
+    sudo apt-get install -y tidy
+}
+
+######################################################################
+
+install_tidy_with_yum
+# install_tidy_with_dnf
+# install_tidy_with_aptget
