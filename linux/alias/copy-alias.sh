ddb13d689fb21287f57b312740bc3c1024f4ef64
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-10 01:59:22 +0900

    move files

---
+++ linux/alias/copy-alias.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/usr/bin/cp -f .bashrc ~/.bashrc
+/usr/bin/cp -rf aliases ~/.aliases
--- linux/aliases/copy-aliases.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p ~/._aliases
-
-/usr/bin/cp -f .bashrc ~/.bashrc
-/usr/bin/cp -f *_aliases ~/._aliases/
