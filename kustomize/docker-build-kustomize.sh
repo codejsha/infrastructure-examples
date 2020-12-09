0e5f33431625929e987857bde3d2527921eec5cc
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-10 01:32:12 +0900

    update docker build

--- kustomize/docker-build-kustomize.sh
+++ kustomize/docker-build-kustomize.sh
@@ -2,3 +2,6 @@
 
 docker build --tag registry.example.com/kustomize-cli:latest .
 docker push registry.example.com/kustomize-cli:latest
+
+# docker build --tag core.harbor.example.com/kustomize-cli:latest .
+# docker push core.harbor.example.com/kustomize-cli:latest
