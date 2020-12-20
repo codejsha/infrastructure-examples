6d995cbebeb46f11111090d929fc9c161c284e54
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-21 02:29:34 +0900

    update postgresql container

--- postgresql/docker-run-postgresql.ps1
+++ postgresql/docker-run-postgresql.ps1
@@ -1,28 +1,54 @@
-docker volume create postgresql11vol
-
-docker run `
-    --detach `
-    --name postgresql11 `
-    --publish 5432:5432 `
-    --env POSTGRES_DB=postgres `
-    --env POSTGRES_USER=postgres `
-    --env POSTGRES_PASSWORD="changeit" `
-    --env PGDATA=/var/lib/postgresql/data/pgdata `
-    --mount type=volume,src=postgresql11vol,dst=/var/lib/postgresql/data `
-    postgres:11.6
+$password="$password"
 
 ######################################################################
 
-docker volume create postgresql12vol
+function New-DockerRunPostgreSQL11 {
+    docker volume create postgresql11vol
 
-docker run `
-    --detach `
-    --name postgresql12 `
-    --publish 5432:5432 `
-    --env POSTGRES_DB=postgres `
-    --env POSTGRES_USER=postgres `
-    --env POSTGRES_PASSWORD="changeit" `
-    --env PGDATA=/var/lib/postgresql/data/pgdata `
-    --mount type=volume,src=postgresql12vol,dst=/var/lib/postgresql/data `
-    postgres:12.3
-    
+    docker run `
+        --detach `
+        --name postgresql11 `
+        --publish 5432:5432 `
+        --env POSTGRES_DB=postgres `
+        --env POSTGRES_USER=postgres `
+        --env POSTGRES_PASSWORD="$password" `
+        --env PGDATA=/var/lib/postgresql/data/pgdata `
+        --mount type=volume,src=postgresql11vol,dst=/var/lib/postgresql/data `
+        postgres:11
+}
+
+function New-DockerRunPostgreSQL12 {
+    docker volume create postgresql12vol
+
+    docker run `
+        --detach `
+        --name postgresql12 `
+        --publish 5432:5432 `
+        --env POSTGRES_DB=postgres `
+        --env POSTGRES_USER=postgres `
+        --env POSTGRES_PASSWORD="$password" `
+        --env PGDATA=/var/lib/postgresql/data/pgdata `
+        --mount type=volume,src=postgresql12vol,dst=/var/lib/postgresql/data `
+        postgres:12
+}
+
+function New-DockerRunPostgreSQL13 {
+    docker volume create postgresql13vol
+
+    docker run `
+        --detach `
+        --name postgresql13 `
+        --publish 5432:5432 `
+        --env POSTGRES_DB=postgres `
+        --env POSTGRES_USER=postgres `
+        --env POSTGRES_PASSWORD="$password" `
+        --env PGDATA=/var/lib/postgresql/data/pgdata `
+        --mount type=volume,src=postgresql13vol,dst=/var/lib/postgresql/data `
+        postgres:13
+}
+
+######################################################################
+
+# New-DockerRunPostgreSQL11
+# New-DockerRunPostgreSQL12
+New-DockerRunPostgreSQL13
--- postgresql/docker-run-postgresql.sh
+++ postgresql/docker-run-postgresql.sh
@@ -1,44 +1,56 @@
 #!/bin/bash
 
-docker volume create postgresql11vol
-
-docker run \
-    --detach \
-    --name postgresql11 \
-    --publish 5432:5432 \
-    --env POSTGRES_DB=postgres \
-    --env POSTGRES_USER=postgres \
-    --env POSTGRES_PASSWORD="${PASSWORD}" \
-    --env PGDATA=/var/lib/postgresql/data/pgdata \
-    --mount type=volume,src=postgresql11vol,dst=/var/lib/postgresql/data \
-    postgres:11
+PASSWORD="${PASSWORD}"
 
 ######################################################################
 
-docker volume create postgresql12vol
+function docker_run_postgresql11 {
+    docker volume create postgresql11vol
 
-docker run \
-    --detach \
-    --name postgresql12 \
-    --publish 5432:5432 \
-    --env POSTGRES_DB=postgres \
-    --env POSTGRES_USER=postgres \
-    --env POSTGRES_PASSWORD="${PASSWORD}" \
-    --env PGDATA=/var/lib/postgresql/data/pgdata \
-    --mount type=volume,src=postgresql12vol,dst=/var/lib/postgresql/data \
-    postgres:12
+    docker run \
+        --detach \
+        --name postgresql11 \
+        --publish 5432:5432 \
+        --env POSTGRES_DB=postgres \
+        --env POSTGRES_USER=postgres \
+        --env POSTGRES_PASSWORD="${PASSWORD}" \
+        --env PGDATA=/var/lib/postgresql/data/pgdata \
+        --mount type=volume,src=postgresql11vol,dst=/var/lib/postgresql/data \
+        postgres:11
+}
+
+function docker_run_postgresql12 {
+    docker volume create postgresql12vol
+
+    docker run \
+        --detach \
+        --name postgresql12 \
+        --publish 5432:5432 \
+        --env POSTGRES_DB=postgres \
+        --env POSTGRES_USER=postgres \
+        --env POSTGRES_PASSWORD="${PASSWORD}" \
+        --env PGDATA=/var/lib/postgresql/data/pgdata \
+        --mount type=volume,src=postgresql12vol,dst=/var/lib/postgresql/data \
+        postgres:12
+}
+
+function docker_run_postgresql13 {
+    docker volume create postgresql13vol
+
+    docker run \
+        --detach \
+        --name postgresql13 \
+        --publish 5432:5432 \
+        --env POSTGRES_DB=postgres \
+        --env POSTGRES_USER=postgres \
+        --env POSTGRES_PASSWORD="${PASSWORD}" \
+        --env PGDATA=/var/lib/postgresql/data/pgdata \
+        --mount type=volume,src=postgresql12vol,dst=/var/lib/postgresql/data \
+        postgres:13
+}
 
 ######################################################################
 
-docker volume create postgresql13vol
-
-docker run \
-    --detach \
-    --name postgresql13 \
-    --publish 5432:5432 \
-    --env POSTGRES_DB=postgres \
-    --env POSTGRES_USER=postgres \
-    --env POSTGRES_PASSWORD="${PASSWORD}" \
-    --env PGDATA=/var/lib/postgresql/data/pgdata \
-    --mount type=volume,src=postgresql12vol,dst=/var/lib/postgresql/data \
-    postgres:13
+# docker_run_postgresql11
+# docker_run_postgresql12
+docker_run_postgresql13
