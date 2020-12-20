7b783c784c39d7ede45d638926bcdd7db4bac3e3
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-21 01:46:14 +0900

    update bookinfo openshift

--- istio/bookinfo-openshift/deploy-bookinfo-openshift.sh
+++ istio/bookinfo-openshift/deploy-bookinfo-openshift.sh
@@ -2,54 +2,8 @@
 
 oc new-project bookinfo
 
-cat <<EOF > ./bookinfo-smcp.yaml
-apiVersion: maistra.io/v1
-kind: ServiceMeshControlPlane
-metadata:
-  name: basic-install
-  namespace: istio-system
-spec:
-  version: v1.1
-  istio:
-    gateways:
-      istio-egressgateway:
-        autoscaleEnabled: false
-      istio-ingressgateway:
-        autoscaleEnabled: false
-        ior_enabled: false
-    mixer:
-      policy:
-        autoscaleEnabled: false
-      telemetry:
-        autoscaleEnabled: false
-    pilot:
-      autoscaleEnabled: false
-      traceSampling: 100
-    kiali:
-      enabled: true
-    grafana:
-      enabled: true
-    tracing:
-      enabled: true
-      jaeger:
-        template: all-in-one
-EOF
-
 oc apply -f bookinfo-smcp.yaml
-
-cat <<EOF > ./bookinfo-smmr.yaml
-apiVersion: maistra.io/v1
-kind: ServiceMeshMemberRoll
-metadata:
-  name: default
-  namespace: istio-system
-spec:
-  members:
-    - bookinfo
-EOF
-
 oc apply -f bookinfo-smmr.yaml
-
 oc apply -n bookinfo -f https://raw.githubusercontent.com/Maistra/istio/maistra-1.1/samples/bookinfo/platform/kube/bookinfo.yaml
 oc apply -n bookinfo -f https://raw.githubusercontent.com/Maistra/istio/maistra-1.1/samples/bookinfo/networking/bookinfo-gateway.yaml
 oc apply -n bookinfo -f https://raw.githubusercontent.com/Maistra/istio/maistra-1.1/samples/bookinfo/networking/destination-rule-all.yaml
