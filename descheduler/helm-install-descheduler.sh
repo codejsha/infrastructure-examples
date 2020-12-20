2eb0cbdf5f3b2ecdf3cb8c11a94e6662d401f255
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-21 01:25:44 +0900

    update helm chart version

--- descheduler/helm-install-descheduler.sh
+++ descheduler/helm-install-descheduler.sh
@@ -5,7 +5,7 @@ helm repo update
 
 # helm install my-descheduler \
 helm upgrade --install my-descheduler \
+    --version 0.19.1 \
     descheduler/descheduler-helm-chart
 
     # --values chart-values.yaml \
-    # --version 0.19.0 \
