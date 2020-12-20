4cd4a5c90761b41c41b89a79541fd34d7fcc7455
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-21 02:28:33 +0900

    update ubuntu container

--- linux/docker-run-ubuntu.sh
+++ linux/docker-run-ubuntu.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-docker run \
-    --interactive \
-    --tty \
-    --name ubuntu20 \
-    --mount type=bind,src=/mnt/share,dst=/mnt/share \
-    --mount type=bind,src=/mnt/storage,dst=/mnt/storage \
-    ubuntu:20.04
+function docker_run_ubuntu20 {
+    docker run \
+        --interactive \
+        --tty \
+        --name ubuntu20 \
+        --mount type=bind,src=/mnt/share,dst=/mnt/share \
+        --mount type=bind,src=/mnt/storage,dst=/mnt/storage \
+        ubuntu:20.04
+}
+
+######################################################################
+
+docker_run_ubuntu20
