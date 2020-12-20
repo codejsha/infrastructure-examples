0ccae078d8fa958575f56e9da3cfab492476f3d0
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-21 02:29:18 +0900

    update mysql container

--- mysql/docker-run-mysql.ps1
+++ mysql/docker-run-mysql.ps1
@@ -1,19 +1,30 @@
-docker run `
-    --detach `
-    --name mysql8 `
-    --publish 3306:3306 `
-    --env MYSQL_ROOT_PASSWORD="changeit" `
-    --env PGDATA=/var/lib/postgresql/data/pgdata `
-    --mount type=bind,src=$env:USERPROFILE\volume\mysql,dst=/var/lib/mysql `
-    mysql:8.0.21
+$password = "$password"
 
 ######################################################################
 
-docker run `
-    --detach `
-    --name mysql5 `
-    --publish 3306:3306 `
-    --env MYSQL_ROOT_PASSWORD="changeit" `
-    --env PGDATA=/var/lib/postgresql/data/pgdata `
-    --mount type=bind,src=$env:USERPROFILE\volume\mysql,dst=/var/lib/mysql `
-    mysql:5.7.31
+function New-DockerRunMySQL8 {
+    docker run `
+        --detach `
+        --name mysql8 `
+        --publish 3306:3306 `
+        --env MYSQL_ROOT_PASSWORD="$password" `
+        --env PGDATA=/var/lib/postgresql/data/pgdata `
+        --mount type=bind,src=$env:USERPROFILE\volume\mysql,dst=/var/lib/mysql `
+        mysql:8.0.21
+}
+
+function New-DockerRunMySQL5 {
+    docker run `
+        --detach `
+        --name mysql5 `
+        --publish 3306:3306 `
+        --env MYSQL_ROOT_PASSWORD="$password" `
+        --env PGDATA=/var/lib/postgresql/data/pgdata `
+        --mount type=bind,src=$env:USERPROFILE\volume\mysql,dst=/var/lib/mysql `
+        mysql:5.7.31
+}
+
+######################################################################
+
+New-DockerRunMySQL8
+# New-DockerRunMySQL5
--- mysql/docker-run-mysql.sh
+++ mysql/docker-run-mysql.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-docker run \
-    --detach \
-    --name mysql8 \
-    --publish 3306:3306 \
-    --env MYSQL_ROOT_PASSWORD="${PASSWORD}" \
-    --env PGDATA=/var/lib/postgresql/data/pgdata \
-    --mount type=bind,src=/mnt/volume/mysql,dst=/var/lib/mysql \
-    mysql:8.0.21
+PASSWORD="${PASSWORD}"
+
+function docker_run_mysql8 {
+    docker run \
+        --detach \
+        --name mysql8 \
+        --publish 3306:3306 \
+        --env MYSQL_ROOT_PASSWORD="${PASSWORD}" \
+        --env PGDATA=/var/lib/postgresql/data/pgdata \
+        --mount type=bind,src=/mnt/volume/mysql,dst=/var/lib/mysql \
+        mysql:8.0.21
+}
+
+function docker_run_mysql5 {
+    docker run \
+        --detach \
+        --name mysql5 \
+        --publish 3306:3306 \
+        --env MYSQL_ROOT_PASSWORD="${PASSWORD}" \
+        --env PGDATA=/var/lib/postgresql/data/pgdata \
+        --mount type=bind,src=/mnt/volume/mysql,dst=/var/lib/mysql \
+        mysql:5.7.31
+}
 
 ######################################################################
 
-docker run \
-    --detach \
-    --name mysql8 \
-    --publish 3306:3306 \
-    --env MYSQL_ROOT_PASSWORD="${PASSWORD}" \
-    --env PGDATA=/var/lib/postgresql/data/pgdata \
-    --mount type=bind,src=/mnt/volume/mysql,dst=/var/lib/mysql \
-    mysql:5.7.31
+docker_run_mysql8
+# docker_run_mysql5
