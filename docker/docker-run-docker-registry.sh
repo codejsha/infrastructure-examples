56b04fb39c5e9d077d52bb9bacda26527d949146
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-21 01:27:22 +0900

    add docker registry container

---
+++ docker/docker-run-docker-registry.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+USERNAME="admin"
+PASSWORD="${PASSWORD}"
+
+REGISTRY_VOLUME_DIR="/mnt/volume/registry"
+sudo mkdir -p ${REGISTRY_VOLUME_DIR}
+
+sudo htpasswd -cB -b ${REGISTRY_VOLUME_DIR}/htpasswd ${USERNAME} ${PASSWORD}
+
+docker run \
+    --detach \
+    --restart unless-stopped \
+    --name registry-server \
+    --publish 5000:5000 \
+    --mount type=bind,src=${REGISTRY_VOLUME_DIR},dst=/var/lib/registry \
+    --mount type=bind,src=${REGISTRY_VOLUME_DIR}/htpasswd,dst=/auth/htpasswd \
+    --env REGISTRY_AUTH="htpasswd" \
+    --env REGISTRY_AUTH_HTPASSWD_REALM="localhost" \
+    --env REGISTRY_AUTH_HTPASSWD_PATH="/auth/htpasswd" \
+    registry:2
