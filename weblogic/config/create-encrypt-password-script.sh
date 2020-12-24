f023c25be096944d777a5fc37cacb6e0e9e616b5
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-24 20:31:24 +0900

    fix error

--- weblogic/config/create-encrypt-password-script.sh
+++ weblogic/config/create-encrypt-password-script.sh
@@ -4,7 +4,7 @@ source ./env-base.sh
 
 ######################################################################
 
-cat << EOF > ${DOMAIN_HOME}/scripts/encrypt-password-script.sh
+cat << EOF > ${DOMAIN_HOME}/scripts/encrypt-password.sh
 #!/bin/bash
 export PS4="\e[33;1m+ \e[0m"; set -x
 
@@ -13,9 +13,9 @@ WL_HOME="${WL_HOME}"
 PLAINTEXT_PASSWORD="\${1}"
 
 ENCRYPTED_PASSWORD=\$(\${JAVA_HOME}/bin/java -classpath \${WL_HOME}/server/lib/weblogic.jar weblogic.security.Encrypt \${PLAINTEXT_PASSWORD})
-echo ${ENCRYPTED_PASSWORD}
+echo \${ENCRYPTED_PASSWORD}
 EOF
 
 ######################################################################
 
-chmod 750 ${DOMAIN_HOME}/scripts/encrypt-password-script.sh
+chmod 750 ${DOMAIN_HOME}/scripts/encrypt-password.sh
