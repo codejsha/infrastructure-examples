0c2ea80f1b5c53b27fa7639ef1295045022ed595
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-28 06:01:54 +0900

    update jenkins helm chart

--- jenkins/helm-install-jenkins.sh
+++ jenkins/helm-install-jenkins.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-PASSWORD="${PASSWORD}"
+helm repo add jenkins https://charts.jenkins.io
+helm repo update
+
+export PASSWORD="${PASSWORD}"
+
+envsubst < ./chart-values.yaml > ./chart-values-temp.yaml
 
 NAMESPACE="jenkins-system"
 
@@ -8,28 +13,6 @@ NAMESPACE="jenkins-system"
 helm upgrade --install my-jenkins \
     --create-namespace \
     --namespace ${NAMESPACE} \
-    --set master.adminUser="admin" \
-    --set master.adminPassword="${PASSWORD}" \
-    --set master.ingress.enabled="true" \
-    --set master.ingress.annotations."kubernetes\.io/ingress\.class"="nginx" \
-    --set master.ingress.hostName="jenkins.example.com" \
-    --set master.ingress.path="/" \
-    --set agent.resources.requests.cpu="128m" \
-    --set agent.resources.requests.memory="512Mi" \
-    --set agent.resources.limits.cpu="256m" \
-    --set agent.resources.limits.memory="512Mi" \
-    --set persistence.enabled="true" \
-    --set persistence.storageClass="rook-ceph-block" \
-    --version 2.4.1 \
-    stable/jenkins
-
-    ### agent
-    # --set agent.tag="4.3-7-jdk11" \
-    # --set agent.tag="4.3-7" \
-
-    ### prefix
-    # --set master.jenkinsUriPrefix="/jenkins" \
-
-    ### for loadbalancer
-    # --set master.serviceType="LoadBalancer" \
-    # --set master.loadBalancerIP="10.10.10.50" \
+    --values chart-values-temp.yaml \
+    --version 3.0.12 \
+    jenkins/jenkins
