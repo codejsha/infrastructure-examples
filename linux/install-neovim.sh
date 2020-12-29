8239245db1f1ec7fba721a0eb5c6b86f02528299
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-29 11:13:35 +0900

    update neovim install

--- linux/install-neovim.sh
+++ linux/install-neovim.sh
@@ -2,14 +2,49 @@
 
 NEOVIM_VERSION="0.4.4"
 
-sudo yum install -y fuse fuse-sshfs
+######################################################################
 
-if [ ! -e "/usr/local/bin/nvim" ]; then
-    sudo curl -o /usr/local/bin/nvim -LJO https://github.com/neovim/neovim/releases/download/v${NEOVIM_VERSION}/nvim.appimage
-    sudo chmod 755 /usr/local/bin/nvim
-fi
+function install_neovim_with_download {
+    sudo yum install -y fuse fuse-sshfs
+
+    if [ ! -e "/usr/local/bin/nvim" ]; then
+        sudo curl -o /usr/local/bin/nvim -LJO https://github.com/neovim/neovim/releases/download/v${NEOVIM_VERSION}/nvim.appimage
+        sudo chmod 755 /usr/local/bin/nvim
+    fi
+}
+
+function install_neovim_with_yum {
+    # sudo yum install -y epel-release
+    sudo yum install -y neovim python36-neovim
+}
+
+function install_neovim_with_apt {
+    sudo apt-get install neovim
+    sudo apt-get install python3-neovim
+}
+
+######################################################################
+
+# install_neovim_with_download
+install_neovim_with_yum
+# install_neovim_with_apt
 
 mkdir -p ${HOME}/.config/nvim
+
+# cat <<EOF > ${HOME}/.config/nvim/init.vim
+# syntax on
+# set termguicolors
+# set background=dark
+# 
+# set shiftwidth=4
+# 
+# set tabstop=4
+# set softtabstop=4
+# set expandtab
+# 
+# set pastetoggle=<F12>
+# EOF
+
 cat <<EOF > ${HOME}/.config/nvim/init.vim
 syntax on
 set termguicolors
@@ -23,7 +58,7 @@ set tabstop=4               # number of spaces that <Tab> in file uses
 set softtabstop=4           # number of spaces that <Tab> uses while editing
 set expandtab               # use spaces when <Tab> is inserted
 
-set pastetoggle=<F11>       # key code that causes 'paste' to toggle
+set pastetoggle=<F12>       # key code that causes 'paste' to toggle
 EOF
 
 GREP_VAR="$(grep -q nvim ${HOME}/.bashrc)"
