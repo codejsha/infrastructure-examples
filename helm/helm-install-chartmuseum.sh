8cc5d7f94b67a823c628bf355f12e258f1062e2a
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-29 10:04:21 +0900

    update helm chartmuseum chart

--- helm/helm-install-chartmuseum.sh
+++ helm/helm-install-chartmuseum.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+export AWS_ACCESS_KEY="${AWS_ACCESS_KEY}"
+export AWS_SECRET_KEY="${AWS_SECRET_KEY}"
+
+envsubst < ./chart-values.yaml > ./chart-values-temp.yaml
+
 NAMESPACE="chart-system"
 
 # helm install my-chartmuseum \
 helm upgrade --install my-chartmuseum \
     --create-namespace \
     --namespace ${NAMESPACE} \
-    --values chart-values.yaml \
+    --values chart-values-temp.yaml \
     --version 2.14.2 \
     stable/chartmuseum
