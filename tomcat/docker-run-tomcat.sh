a2d5c421c767691c2adb74af2b63fe0335c00b28
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-29 10:03:56 +0900

    update docker tomcat container

--- tomcat/docker-run-tomcat.sh
+++ tomcat/docker-run-tomcat.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 docker run \
-    --interactive \
-    --tty \
+    --detach \
     --name tomcat9 \
+    --publish 8080:8080 \
     --mount type=bind,src=/mnt/share,dst=/mnt/share \
     --mount type=bind,src=/mnt/storage,dst=/mnt/storage \
-    tomcat:9.0.40-jdk11-openjdk
+    tomcat:9.0.41-jdk11-openjdk
