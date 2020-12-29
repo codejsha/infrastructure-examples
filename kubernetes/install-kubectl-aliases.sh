f5117edb9c322fd7e7566920e3b6fcaac12dcdb0
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-29 10:59:28 +0900

    update aliases

--- kubernetes/install-kubectl-aliases.sh
+++ kubernetes/install-kubectl-aliases.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 curl -o ~/.aliases/.kubectl_aliases -LJO https://raw.githubusercontent.com/ahmetb/kubectl-aliases/master/.kubectl_aliases
+
+# function kubectl() { echo "+ kubectl ${@}">&2; command kubectl ${@}; }
+# [ -f ~/.aliases/.kubectl_aliases ] && source ~/.aliases/.kubectl_aliases
--- linux/alias/copy-alias-ubuntu.sh
+++ linux/alias/copy-alias-ubuntu.sh
@@ -2,7 +2,7 @@
 
 rm -rf ~/.aliases
 
-/usr/bin/cp -f .bashrc ~/.bashrc
+/usr/bin/cp -f .bashrc_ubuntu ~/.bashrc
 /usr/bin/cp -rf aliases ~/.aliases
 
 curl -o ~/.aliases/.kubectl_aliases -LJO https://raw.githubusercontent.com/ahmetb/kubectl-aliases/master/.kubectl_aliases
