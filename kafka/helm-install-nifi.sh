5b6aadb1068e5693ad561ddc24d7156325d31c42
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-29 10:05:41 +0900

    update nifi helm chart

--- kafka/helm-install-nifi.sh
+++ kafka/helm-install-nifi.sh
@@ -9,22 +9,6 @@ NAMESPACE="kafka-system"
 helm upgrade --install my-nifi \
     --create-namespace \
     --namespace ${NAMESPACE} \
-    --set jvmMemory="512m" \
-    --set zookeeper.enabled="false" \
-    --set zookeeper.url="my-kafka-zookeeper" \
-    --set service.type="ClusterIP" \
-    --set ingress.enabled="true" \
-    --set ingress.annotations."kubernetes\.io/ingress\.class"="nginx" \
-    --set ingress.hosts={"nifi.example.com"} \
-    --set ingress.path="/" \
-    --set persistence.enabled="true" \
-    --set persistence.storageClass="rook-ceph-block" \
-    --set registry.enabled="true" \
-    --set registry.persistence.enabled="true" \
-    --set registry.persistence.database.storageClass="rook-ceph-block" \
-    --set registry.persistence.flowStorage.storageClass="rook-ceph-block" \
-    --version 0.6.0 \
+    --values nifi-chart-values.yaml \
+    --version 0.6.1 \
     cetic/nifi
-
-    ### for loadbalancer
-    # --set service.loadBalancerIP="10.10.10.50" \
