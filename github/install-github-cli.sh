b9d97a73b7e9d08c41692098fca99a467b78451b
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-29 10:01:49 +0900

    add github cli apt install

--- github/install-github-cli.sh
+++ github/install-github-cli.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
-sudo dnf install -y gh
+function install_github_cli_with_dnf {
+    sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+    sudo dnf install -y gh
+}
 
+function install_github_cli_with_apt {
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+    sudo apt-add-repository https://cli.github.com/packages
+    sudo apt update
+    sudo apt install gh
+}
+
+######################################################################
+
+install_github_cli_with_dnf
+# install_github_cli_with_apt
+
+# gh auth login
 # gh config set git_protocol ssh
