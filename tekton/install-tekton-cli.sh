71bf54d237b74f0bfbde243803f6d8c309cab8e6
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2020-12-29 10:11:14 +0900

    update tekton cli version

--- tekton/install-tekton-cli.sh
+++ tekton/install-tekton-cli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TEKTON_CLI_VERSION="0.11.0"
+TEKTON_CLI_VERSION="0.15.0"
 
 curl -LJO https://github.com/tektoncd/cli/releases/download/v${TEKTON_CLI_VERSION}/tkn_${TEKTON_CLI_VERSION}_Linux_x86_64.tar.gz
 sudo tar -zxf tkn_${TEKTON_CLI_VERSION}_Linux_x86_64.tar.gz -C /usr/local/bin/ tkn
