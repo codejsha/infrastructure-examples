ff18197e1cfc4c20cf996106bbc0f679e4af4a70
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-01-02 18:51:32 +0900

    add kustomize tomcat install

---
+++ tomcat/kustomize-install-tomcat.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+kustomize build ./kustomize/base | kubectl apply --filename -
+
+kustomize build https://github.com/codejsha/infrastructure/tomcat/kustomize/base | kubectl apply --filename -
+kustomize build https://github.com/codejsha/infrastructure/tomcat/kustomize/overlays/development | kubectl apply --filename -
+kustomize build https://github.com/codejsha/infrastructure/tomcat/kustomize/overlays/prodction | kubectl apply --filename -
