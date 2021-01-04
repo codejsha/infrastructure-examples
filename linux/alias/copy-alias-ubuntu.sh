486336991782a9bea92eaa343daaac0eee8b22b2
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-01-05 00:53:12 +0900

    move files

--- linux/alias/copy-alias-ubuntu.sh
+++ linux/alias/copy-alias-ubuntu.sh
@@ -5,4 +5,4 @@ rm -rf ~/.aliases
 /usr/bin/cp -f .bashrc_ubuntu ~/.bashrc
 /usr/bin/cp -rf aliases ~/.aliases
 
-curl -o ~/.aliases/.kubectl_aliases -LJO https://raw.githubusercontent.com/ahmetb/kubectl-aliases/master/.kubectl_aliases
+curl -o ~/.aliases/kubectl_aliases -LJO https://raw.githubusercontent.com/ahmetb/kubectl-aliases/master/.kubectl_aliases
