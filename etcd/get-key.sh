a0d93e8ff5cb361689859f4d8f578f76959342c7
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-01-13 01:26:35 +0900

    change addresses

--- etcd/get-key.sh
+++ etcd/get-key.sh
@@ -4,7 +4,7 @@ KEYWORD="${1}"
 
 export ETCDCTL_API=3
 /usr/local/bin/etcdctl \
-    --endpoints=https://10.10.10.11:2379,https://10.10.10.12:2379,https://10.10.10.13:2379 \
+    --endpoints=https://controlplane1:2379,https://controlplane2:2379,https://controlplane3:2379 \
     --cacert=/etc/ssl/etcd/ssl/ca.pem \
     --cert=/etc/ssl/etcd/ssl/member-controlplane1.pem \
     --key=/etc/ssl/etcd/ssl/member-controlplane1-key.pem \
--- etcd/save-snapshot.sh
+++ etcd/save-snapshot.sh
@@ -5,7 +5,7 @@ FILENAME="etcd-snapshot-${GET_DATE}"
 
 export ETCDCTL_API=3
 /usr/local/bin/etcdctl \
-    --endpoints=https://10.10.10.11:2379 \
+    --endpoints=https://controlplane1:2379 \
     --cacert=/etc/ssl/etcd/ssl/ca.pem \
     --cert=/etc/ssl/etcd/ssl/member-controlplane1.pem \
     --key=/etc/ssl/etcd/ssl/member-controlplane1-key.pem \
