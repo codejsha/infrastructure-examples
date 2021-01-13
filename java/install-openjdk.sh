1d547d289e9727207a3d60ed7bc75a8abe04d209
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-01-14 00:56:12 +0900

    update openjdk install

--- java/install-openjdk.sh
+++ java/install-openjdk.sh
@@ -3,11 +3,13 @@
 ######################################################################
 
 function install_openjdk_with_yum {
+    sudo yum install -y java-1.7.0-openjdk-devel
     sudo yum install -y java-1.8.0-openjdk-devel
     sudo yum install -y java-11-openjdk-devel
 }
 
 function install_openjdk_with_apt {
+    sudo apt-get install openjdk-7-jdk
     sudo apt-get install openjdk-8-jdk
     sudo apt-get install openjdk-11-jdk
 }
