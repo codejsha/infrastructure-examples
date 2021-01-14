9f58a5bc40d48b8afc3468bcc7a778230778acf5
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-01-14 19:06:36 +0900

    add verbose option in bsu command

--- weblogic/patch/patch-with-bsu.sh
+++ weblogic/patch/patch-with-bsu.sh
@@ -84,7 +84,7 @@ function bsu_install {
 function bsu_view_applied {
     cd ${MW_HOME}/utils/bsu
     echo "[INFO] View applied patches..."
-    ${MW_HOME}/utils/bsu/bsu.sh -view -status=applied -prod_dir=${MW_HOME}/wlserver_10.3
+    ${MW_HOME}/utils/bsu/bsu.sh -view -status=applied -prod_dir=${MW_HOME}/wlserver_10.3 -verbose
 }
 
 ######################################################################
