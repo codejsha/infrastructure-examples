8389c3eac2450d46b83c9ed45bbcc24cffde08a8
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-01-19 15:21:46 +0900

    update helper

--- tomcat/helper.sh
+++ tomcat/helper.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 JAVA_HOME="\/usr\/lib\/jvm\/java-11"
-# JAVA_HOME="\/usr\/java\/current"
+JRE_HOME="\/usr\/lib\/jvm\/jre-11"
+# JAVA_HOME="\/usr\/lib\/jvm\/java-1.8.0"
+# JRE_HOME="\/usr\/lib\/jvm\/jre-1.8.0"
 CATALINA_HOME="\/usr\/local\/tomcat"
 
 find . -type f -name "env-base.sh" | xargs perl -pi -e "s/JAVA_HOME=.*/JAVA_HOME=\"${JAVA_HOME}\"/"
+find . -type f -name "env-base.sh" | xargs perl -pi -e "s/JRE_HOME=.*/JRE_HOME=\"${JRE_HOME}\"/"
 find . -type f -name "env-base.sh" | xargs perl -pi -e "s/CATALINA_HOME=.*/CATALINA_HOME=\"${CATALINA_HOME}\"/"
