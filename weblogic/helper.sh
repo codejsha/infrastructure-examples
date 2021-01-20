b04616f0239d410565f88c5e17a724cf4cc3d315
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-01-20 10:40:39 +0900

    update helper

--- weblogic/helper.sh
+++ weblogic/helper.sh
@@ -2,12 +2,14 @@
 
 JAVA_HOME="\/usr\/java\/current"
 ORACLE_HOME="\/usr\/local\/weblogic"
+DOMAIN_NAME="base_domain"
 ADMIN_SERVER_LISTEN_ADDRESS="test.example.com"
 ADMIN_SERVER_LISTEN_PORT="7001"
 INSTALL_SCRIPT_DIR="\/svc\/infrastructure\/weblogic"
 
 find . -type f -name "env-base.sh" | xargs perl -pi -e "s/JAVA_HOME=.*/JAVA_HOME=\"${JAVA_HOME}\"/"
 find . -type f -name "env-base.sh" | xargs perl -pi -e "s/ORACLE_HOME=.*/ORACLE_HOME=\"${ORACLE_HOME}\"/"
+find . -type f -name "env-base.sh" | xargs perl -pi -e "s/DOMAIN_NAME=.*/DOMAIN_NAME=\"${DOMAIN_NAME}\"/"
 find . -type f -name "env-base.sh" | xargs perl -pi -e "s/ADMIN_SERVER_LISTEN_ADDRESS=.*/ADMIN_SERVER_LISTEN_ADDRESS=\"${ADMIN_SERVER_LISTEN_ADDRESS}\"/"
 find . -type f -name "env-base.sh" | xargs perl -pi -e "s/ADMIN_SERVER_LISTEN_PORT=.*/ADMIN_SERVER_LISTEN_PORT=\"${ADMIN_SERVER_LISTEN_PORT}\"/"
 find . -type f -name "env-base.sh" | xargs perl -pi -e "s/INSTALL_SCRIPT_DIR=.*/INSTALL_SCRIPT_DIR=\"${INSTALL_SCRIPT_DIR}\"/"
