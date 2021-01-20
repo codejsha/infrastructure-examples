3ba7ca49a7614b0baf4edb855c64be54202525f2
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-01-20 15:19:14 +0900

    update all.sh

--- wildfly/security/all.sh
+++ wildfly/security/all.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-bash ./disable-directory-index.sh
+bash ./disable-directory-index.sh --port-offset=0
+bash ./disable-directory-index.sh --port-offset=1
---
+++ wildfly/undertow/all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+bash ./set-instance-id.sh --instance=inst1 --port-offset=0
+bash ./set-instance-id.sh --instance=inst2 --port-offset=1
