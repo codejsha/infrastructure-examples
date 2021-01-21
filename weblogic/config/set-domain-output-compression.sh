e27884512b48ccedcb1e2770b9e1ff25b0feb2b5
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-01-21 22:33:53 +0900

    update output compression

--- weblogic/config/set-domain-output-compression.sh
+++ weblogic/config/set-domain-output-compression.sh
@@ -11,7 +11,9 @@ export COMPRESS_CONTENT_TYPE
 
 export CONFIG_JVM_ARGS="${CONFIG_JVM_ARGS} -Djava.security.egd=file:///dev/urandom"
 if [[ ${WEBLOGIC_VERSION} =~ ^10.3 ]]; then
-    ${MW_HOME}/wlserver_10.3/common/bin/wlst.sh set_domain_output_compression.py
-elif [[ ${WEBLOGIC_VERSION} =~ ^12.|^14.1 ]]; then
+    echo "[INFO] Not available in this version."
+elif [[ ${WEBLOGIC_VERSION} =~ ^12.1 ]]; then
+    echo "[INFO] Not available in this version."
+elif [[ ${WEBLOGIC_VERSION} =~ ^12.2|^14.1 ]]; then
     ${ORACLE_HOME}/oracle_common/common/bin/wlst.sh set_domain_output_compression.py
 fi
