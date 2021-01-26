8029e173d4d1f47d6a440be4dff7306d1578c3a3
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-01-26 10:50:40 +0900

    update outofmemoryerror options

--- java/out-of-memory-error.sh
+++ java/out-of-memory-error.sh
@@ -2,8 +2,10 @@
 
 ### java.lang.OutOfMemoryError: Java heap space
 
-### class histogram
+### heap dump
+-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${LOG_DIR}/dump
 
+### class histogram
 ### live objects
 ${JAVA_HOME}/bin/jcmd ${PID} GC.class_histogram
 ${JAVA_HOME}/bin/jmap -histo:live ${PID}
@@ -15,6 +17,9 @@ ${JAVA_HOME}/bin/jmap -histo:live ${PID}
 ### gc logging - metaspace monitoring
 -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+PrintHeapAtGC -Xloggc:${LOG_DIR}/gc.${SERVER_NAME}.log
 -Xlog:gc*=info:file=${LOG_DIR}/gc.${SERVER_NAME}.log:time,pid,tid,level,tags
+
+### heap dump
+-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${LOG_DIR}/dump
 
 ### class loading/unloading trace
 -XX:+TraceClassLoading -XX:+TraceClassUnloading
@@ -37,6 +42,9 @@ ${JAVA_HOME}/bin/jcmd ${PID} VM.native_memory
 ### gc logging - perm gen monitoring
 -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+PrintHeapAtGC -Xloggc:${LOG_DIR}/gc.${SERVER_NAME}.log
 -Xlog:gc*=info:file=${LOG_DIR}/gc.${SERVER_NAME}.log:time,pid,tid,level,tags
+
+### heap dump
+-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${LOG_DIR}/dump
 
 ### class loading/unloading trace
 -XX:+TraceClassLoading -XX:+TraceClassUnloading
@@ -61,3 +69,26 @@ ${JAVA_HOME}/bin/jmap -permstat ${PID}
 ######################################################################
 
 ### java.lang.OutOfMemoryError: unable to create new native thread
+
+### max user processes
+
+### check
+ulimit -u
+ulimit -a | grep "max user processes"
+grep nproc /etc/security/limits.conf
+
+### temporary
+ulimit -u 65536
+
+### permanent (/etc/security/limits.conf)
+prouser          soft    nproc           65536
+prouser          hard    nproc           65536
+
+### ThreadStackSize
+
+### check
+${JAVA_HOME}/bin/java -XX:+PrintFlagsFinal -version | grep ThreadStackSize
+${JAVA_HOME}/bin/jcmd ${PID} VM.flags -all | grep ThreadStackSize
+
+### set
+-Xss256k
