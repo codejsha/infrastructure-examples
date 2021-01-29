b8b32a0b96e5fc8cd4b11dff828ad8beb6f1c6a6
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-01-30 06:00:43 +0900

    fix error

--- nginx/install-nginx.sh
+++ nginx/install-nginx.sh
@@ -3,7 +3,7 @@
 cat <<EOF | sudo tee /etc/yum.repos.d/nginx.repo
 [nginx]
 name=nginx repo
-baseurl=https://nginx.org/packages/centos/$releasever/$basearch/
+baseurl=https://nginx.org/packages/centos/\$releasever/\$basearch/
 gpgcheck=0
 enabled=1
 EOF
