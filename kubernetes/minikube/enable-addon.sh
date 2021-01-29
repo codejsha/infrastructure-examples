25173339fe2073d32e7a41224f3ab4e712f6af57
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-01-30 08:02:42 +0900

    update minikube

---
+++ kubernetes/minikube/enable-addon.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+minikube addons enable ambassador
+# minikube addons enable csi-hostpath-driver
+minikube addons enable metrics-server
+minikube addons enable dashboard
+minikube addons enable default-storageclass
+minikube addons enable efk
+# minikube addons enable freshpod
+# minikube addons enable gcp-auth
+# minikube addons enable gvisor
+# minikube addons enable helm-tiller
+minikube addons enable ingress
+minikube addons enable ingress-dns
+minikube addons enable istio-provisioner
+minikube addons enable istio
+# minikube addons enable kubevirt
+minikube addons enable logviewer
+minikube addons enable metallb
+# minikube addons enable nvidia-driver-installer
+# minikube addons enable nvidia-gpu-device-plugin
+minikube addons enable olm
+# minikube addons enable pod-security-policy
+minikube addons enable registry
+minikube addons enable registry-aliases
+minikube addons enable registry-creds
+minikube addons enable storage-provisioner
+# minikube addons enable storage-provisioner-gluster
+# minikube addons enable volumesnapshots
---
+++ kubernetes/minikube/set-config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+minikube config set memory 32768
+minikube config set cpus 4
