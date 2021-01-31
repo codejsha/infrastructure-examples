c63a833a9513ffe51b1da2257b5a2919d8594927
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-01-31 18:52:33 +0900

    update syntax

--- kubernetes/kubespray/helper.ps1
+++ kubernetes/kubespray/helper.ps1
@@ -1,4 +1,5 @@
 Get-ChildItem -Path *.yaml, *.sh |
 ForEach-Object {
     (Get-Content -Path $_) -replace "192.168.140", "192.168.137" | Set-Content -Path $_
+    # (Get-Content -Path $_) -replace "192.168.140", "10.10.10" | Set-Content -Path $_
 }
