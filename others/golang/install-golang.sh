f4b0cd3ed843a05404949444e57edeec459a8a1b
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-02-01 23:56:03 +0900

    update golang version

--- others/golang/install-golang.sh
+++ others/golang/install-golang.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="1.15.5"
+VERSION="1.15.6"
 OS="linux"
 ARCH="amd64"
 
@@ -8,4 +8,4 @@ curl -LJO https://golang.org/dl/go${VERSION}.${OS}-${ARCH}.tar.gz
 sudo tar -C /usr/local -xzf go${VERSION}.${OS}-${ARCH}.tar.gz
 rm -f https://golang.org/dl/go${VERSION}.${OS}-${ARCH}.tar.gz
 
-export PATH="${PATH}:/usr/local/go/bin"
+# export PATH="${PATH}:/usr/local/go/bin"
