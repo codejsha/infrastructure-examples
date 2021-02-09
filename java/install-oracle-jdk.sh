a974edc6c0ef6efcf76c7ad77874389c10b78a1b
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-02-09 22:18:09 +0900

    update oracle jdk install

--- java/install-oracle-jdk.sh
+++ java/install-oracle-jdk.sh
@@ -6,7 +6,8 @@ function install_oracle_jdk6 {
 
     cp ${INSTALL_FILE_DIR}/${INSTALL_FILE} /usr/java
     cd /usr/java
-    bash ./jdk-6u211-linux-x64.bin
+    bash ./${INSTALL_FILE}
+    rm ${INSTALL_FILE}
 
     ln -snf /usr/java/jdk1.6.0_211 /usr/java/java-1.6.0
 }
@@ -15,8 +16,7 @@ function install_oracle_jdk7 {
     INSTALL_FILE_DIR="/mnt/share/oracle-jdk/7"
     INSTALL_FILE="jdk-7u281-linux-x64.tar.gz"
 
-    cp ${INSTALL_FILE_DIR}/${INSTALL_FILE} /usr/java
-    tar -C /usr/java -xzf /usr/java/${INSTALL_FILE}
+    tar -C /usr/java -xzf ${INSTALL_FILE_DIR}/${INSTALL_FILE}
 
     ln -snf /usr/java/jdk1.7.0_281 /usr/java/java-1.7.0
 }
@@ -25,8 +25,7 @@ function install_oracle_jdk8 {
     INSTALL_FILE_DIR="/mnt/share/oracle-jdk/8"
     INSTALL_FILE="jdk-8u271-linux-x64.tar.gz"
 
-    cp ${INSTALL_FILE_DIR}/${INSTALL_FILE} /usr/java
-    tar -C /usr/java -xzf /usr/java/${INSTALL_FILE}
+    tar -C /usr/java -xzf ${INSTALL_FILE_DIR}/${INSTALL_FILE}
 
     ln -snf /usr/java/jdk1.8.0_271 /usr/java/java-1.8.0
 }
@@ -35,8 +34,7 @@ function install_oracle_jdk11 {
     INSTALL_FILE_DIR="/mnt/share/oracle-jdk/11"
     INSTALL_FILE="jdk-11.0.7_linux-x64_bin.tar.gz"
 
-    cp ${INSTALL_FILE_DIR}/${INSTALL_FILE} /usr/java
-    tar -C /usr/java -xzf /usr/java/${INSTALL_FILE}
+    tar -C /usr/java -xzf ${INSTALL_FILE_DIR}/${INSTALL_FILE}
 
     ln -snf /usr/java/jdk-11.0.7 /usr/java/java-11
 }
