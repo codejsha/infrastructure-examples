3e7b878f524d80addc838065feae8500b475ebb8
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-02-09 22:20:21 +0900

    update creating domain

--- weblogic/create-domain.sh
+++ weblogic/create-domain.sh
@@ -9,20 +9,34 @@ MW_HOME="${MW_HOME}"           # for 11g
 
 export CONFIG_JVM_ARGS="${CONFIG_JVM_ARGS} -Djava.security.egd=file:///dev/urandom"
 
-function weblogic_create_domain_11g {
+function create_domain_11g {
     ${MW_HOME}/wlserver_10.3/common/bin/wlst.sh create_domain_11g.py
 }
 
-function weblogic_create_domain_12cr1 {
+function create_domain_12cr1 {
     ${ORACLE_HOME}/oracle_common/common/bin/wlst.sh create_domain_12cr1.py
 }
 
-function weblogic_create_domain {
+function create_domain_12c {
     ${ORACLE_HOME}/oracle_common/common/bin/wlst.sh create_domain.py
+}
+
+function create_domain_14c {
+    ${ORACLE_HOME}/oracle_common/common/bin/wlst.sh create_domain.py
+}
+
+function create_domain {
+    if [[ ${WEBLOGIC_VERSION} =~ ^10.3 ]]; then
+        create_domain_11g
+    elif [[ ${WEBLOGIC_VERSION} =~ ^12.1 ]]; then
+        create_domain_12cr1
+    elif [[ ${WEBLOGIC_VERSION} =~ ^12.2 ]]; then
+        create_domain_12c
+    elif [[ ${WEBLOGIC_VERSION} =~ ^14. ]]; then
+        create_domain_14c
+    fi
 }
 
 ######################################################################
 
-# weblogic_create_domain_11g
-# weblogic_create_domain_12cr1
-weblogic_create_domain
+create_domain
