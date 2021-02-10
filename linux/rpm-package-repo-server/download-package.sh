d064550789e95330eeac0f5d14784b6bfc706ba1
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-02-10 18:21:40 +0900

    update package repo

--- linux/rpm-package-repo-server/download-package.sh
+++ linux/rpm-package-repo-server/download-package.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PACKAGE_REPO_VOLUME_DIR="/mnt/volume/example-repo"
+sudo mkdir -p ${PACKAGE_REPO_VOLUME_DIR}/package
 cd ${PACKAGE_REPO_VOLUME_DIR}/package
 
 # sudo repotrack ${PACKAGE}
@@ -10,3 +11,5 @@ sudo repotrack make
 sudo repotrack unzip
 
 sudo createrepo --update ${PACKAGE_REPO_VOLUME_DIR}
+sudo yum clean all
+sudo yum repolist
