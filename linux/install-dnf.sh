5a93123155e8ad581c7258fd7c3f55fa721582ef
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-02-10 18:22:29 +0900

    update dnf install

--- linux/install-dnf.sh
+++ linux/install-dnf.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sudo yum install -y dnf
+
 sudo yum install -y dnf-plugins-core
+# sudo dnf install 'dnf-command(config-manager)'
 
 # sudo dnf config-manager --set-enabled PowerTools
