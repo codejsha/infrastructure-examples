6e93ac80c272320d57b86ee18ebbb5d6b01244f7
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-02-15 20:32:33 +0900

    update install

--- kafka/strimzi/operator-install-strimzi-kafka.sh
+++ kafka/strimzi/operator-install-strimzi-kafka.sh
@@ -11,22 +11,24 @@ function create_namespaces {
     kubectl create namespace ${STRIMZI_NAMESPACE}
 }
 
-function download_operator_install_file {
-    curl \
-        --output strimzi-${STRIMZI_VERSION}.zip \
-        --location \
-        --remote-header-name \
-        --remote-name \
-        https://github.com/strimzi/strimzi-kafka-operator/releases/download/${STRIMZI_VERSION}/strimzi-${STRIMZI_VERSION}.zip
-    # curl \
-    #     --output strimzi-${STRIMZI_VERSION}.tar.gz \
-    #     --location \
-    #     --remote-header-name \
-    #     --remote-name \
-    #     https://github.com/strimzi/strimzi-kafka-operator/releases/download/${STRIMZI_VERSION}/strimzi-${STRIMZI_VERSION}.tar.gz
+function download_install_file {
+    if [ ! -f "strimzi-${STRIMZI_VERSION}.zip" ]; then
+        curl \
+            --output strimzi-${STRIMZI_VERSION}.zip \
+            --location \
+            --remote-header-name \
+            --remote-name \
+            https://github.com/strimzi/strimzi-kafka-operator/releases/download/${STRIMZI_VERSION}/strimzi-${STRIMZI_VERSION}.zip
+        # curl \
+        #     --output strimzi-${STRIMZI_VERSION}.tar.gz \
+        #     --location \
+        #     --remote-header-name \
+        #     --remote-name \
+        #     https://github.com/strimzi/strimzi-kafka-operator/releases/download/${STRIMZI_VERSION}/strimzi-${STRIMZI_VERSION}.tar.gz
+    fi
 }
 
-function extract_operator_install_file {
+function extract_install_file {
     unzip strimzi-${STRIMZI_VERSION}.zip
     # tar -xzf strimzi-${STRIMZI_VERSION}.tar.gz
 }
@@ -54,12 +56,6 @@ function give_permission_to_strimzi_operator {
         -n ${STRIMZI_NAMESPACE}
 }
 
-function create_kafka_cluster {
-    kubectl apply \
-        --filename strimzi-kafka-cluster.yaml \
-        -n ${STRIMZI_NAMESPACE}
-}
-
 function create_entity_operator {
     kubectl apply \
         --filename strimzi-${STRIMZI_VERSION}/install/topic-operator/ \
@@ -69,12 +65,38 @@ function create_entity_operator {
         -n ${STRIMZI_NAMESPACE}
 }
 
+function delete_strimzi_operator {
+    kubectl delete \
+        --filename strimzi-${STRIMZI_VERSION}/install/topic-operator/ \
+        -n ${STRIMZI_NAMESPACE}
+    kubectl delete \
+        --filename strimzi-${STRIMZI_VERSION}/install/user-operator/ \
+        -n ${STRIMZI_NAMESPACE}
+
+    kubectl delete \
+        --filename strimzi-${STRIMZI_VERSION}/install/cluster-operator/020-RoleBinding-strimzi-cluster-operator.yaml \
+        -n ${STRIMZI_NAMESPACE}
+    kubectl delete \
+        --filename strimzi-${STRIMZI_VERSION}/install/cluster-operator/032-RoleBinding-strimzi-cluster-operator-topic-operator-delegation.yaml \
+        -n ${STRIMZI_NAMESPACE}
+    kubectl delete \
+        --filename strimzi-${STRIMZI_VERSION}/install/cluster-operator/031-RoleBinding-strimzi-cluster-operator-entity-operator-delegation.yaml \
+        -n ${STRIMZI_NAMESPACE}
+
+    kubectl delete \
+        --filename strimzi-${STRIMZI_VERSION}/install/cluster-operator/ \
+        -n ${STRIMZI_OPERATOR_NAMESPACE}
+    # kubectl delete \
+    #     --filename strimzi-${STRIMZI_VERSION}/install/strimzi-admin/ \
+    #     -n ${STRIMZI_OPERATOR_NAMESPACE}
+}
+
 ######################################################################
 
 create_namespaces
-download_operator_install_file
-extract_operator_install_file
+download_install_file
+extract_install_file
 deploy_resource
 give_permission_to_strimzi_operator
-create_kafka_cluster
 # create_entity_operator
+# delete_strimzi_operator
