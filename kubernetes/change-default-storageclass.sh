5155d43e9868a478dcb58523d60b7553370df63b
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-02-22 17:03:43 +0900

    change default storageclass

--- kubernetes/change-default-storageclass.sh
+++ kubernetes/change-default-storageclass.sh
@@ -3,10 +3,10 @@
 # kubectl annotate --overwrite storageclass standard storageclass.kubernetes.io/is-default-class=true
 # kubectl annotate --overwrite storageclass standard storageclass.kubernetes.io/is-default-class=false
 
-kubectl annotate --overwrite storageclass local-path storageclass.kubernetes.io/is-default-class=true
+# kubectl annotate --overwrite storageclass local-path storageclass.kubernetes.io/is-default-class=true
 # kubectl annotate --overwrite storageclass local-path storageclass.kubernetes.io/is-default-class=false
 
-# kubectl annotate --overwrite storageclass rook-ceph-block storageclass.kubernetes.io/is-default-class=true
+kubectl annotate --overwrite storageclass rook-ceph-block storageclass.kubernetes.io/is-default-class=true
 # kubectl annotate --overwrite storageclass rook-ceph-block storageclass.kubernetes.io/is-default-class=false
 
 # kubectl get storageclass
