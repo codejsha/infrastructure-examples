9ea47567106cd12c7282364c2c5334fb43b2b12c
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-02-22 14:07:46 +0900

    add connector

---
+++ debezium/register-oracledb-connector.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+KAFKA_CONNECT_URL="kafka-connect.example.com:8083"
+# KAFKA_CONNECT_URL="kafka-connect1.example.com:8083"
+# KAFKA_CONNECT_URL="kafka-connect2.example.com:8083"
+
+######################################################################
+
+function register_oracledb_connector {
+    curl \
+        --include \
+        --request POST \
+        --header "Accept:application/json" \
+        --header "Content-Type:application/json" \
+        --data @data-oracledb.json \
+        ${KAFKA_CONNECT_URL}/connectors
+}
+
+function get_connector_list {
+    curl \
+        --header "Accept:application/json" \
+        ${KAFKA_CONNECT_URL}/connectors
+}
+
+function get_connector_task {
+    CONNECTOR_NAME="${1}"
+
+    curl \
+        --include \
+        --header "Accept:application/json" \
+        --request GET \
+        ${KAFKA_CONNECT_URL}/connectors/${CONNECTOR_NAME}
+}
+
+######################################################################
+
+register_postgresql_connector
+
+get_connector_list
+# get_connector_task "oracledb-connector"
