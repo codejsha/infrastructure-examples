eb27ed967a94c9937fac109430d62a8e91f5eacb
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-02-22 14:18:17 +0900

    update elastic operator

--- elasticstack/operator-install-elastic-operator.sh
+++ elasticstack/operator-install-elastic-operator.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-kubectl apply -f https://download.elastic.co/downloads/eck/1.3.1/all-in-one.yaml
-kubectl logs -f statefulset.apps/elastic-operator -n elastic-system
+kubectl apply -f https://download.elastic.co/downloads/eck/1.4.0/all-in-one.yaml
+# kubectl logs -f statefulset.apps/elastic-operator -n elastic-system
+
+### delete
+# kubectl delete -f https://download.elastic.co/downloads/eck/1.4.0/all-in-one.yaml
