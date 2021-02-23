f4435e11836dc915ed4d938744c150c3dfde67d6
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-02-24 03:39:08 +0900

    update minio commands

--- minio/minio-client-commands.sh
+++ minio/minio-client-commands.sh
@@ -3,14 +3,21 @@
 ### host config
 
 MINIO_ALIAS="my-minio"
-MINIO_ALIAS="minio-tenant-1"
 MINIO_URL="http://minio.example.com"
+
+MINIO_ALIAS="minio-tenant-1"
 MINIO_URL="https://minio-tenant-1.example.com"
+
 AWS_ACCESS_KEY="${AWS_ACCESS_KEY}"
 AWS_SECRET_KEY="${AWS_SECRET_KEY}"
 
 mc config host add ${MINIO_ALIAS} ${MINIO_URL} ${AWS_ACCESS_KEY} ${AWS_SECRET_KEY} --api s3v4
 mc config host ls
+
+######################################################################
+
+mc ls my-minio/
+mc ls minio-tenant-1/
 
 ######################################################################
 
