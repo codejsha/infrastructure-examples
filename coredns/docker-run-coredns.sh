15baade9c10a530a5a4eba0e9f103a6b46056f3a
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-02 03:57:47 +0900

    update coredns install

--- coredns/docker-run-coredns.sh
+++ coredns/docker-run-coredns.sh
@@ -4,11 +4,31 @@ COREDNS_VOLUME_DIR="/mnt/volume/coredns"
 sudo mkdir -p ${COREDNS_VOLUME_DIR}
 sudo /usr/bin/cp -f corefile-server.conf ${COREDNS_VOLUME_DIR}/Corefile
 
-docker container run \
-    --detach \
-    --name coredns \
-    --restart=unless-stopped \
-    --publish 53:53/udp \
-    --mount type=bind,src=${COREDNS_VOLUME_DIR},dst=/root/coredns \
-    coredns/coredns:latest \
-    -conf /root/coredns/Corefile
+######################################################################
+
+function docker_run_coredns {
+    docker container run \
+        --detach \
+        --name coredns \
+        --restart=unless-stopped \
+        --publish 53:53/udp \
+        --mount type=bind,src=${COREDNS_VOLUME_DIR},dst=/root/coredns \
+        coredns/coredns:latest \
+        -conf /root/coredns/Corefile
+}
+
+function podman_run_coredns {
+    sudo podman container run \
+        --detach \
+        --name coredns \
+        --restart=unless-stopped \
+        --publish 53:53/udp \
+        --mount type=bind,src=${COREDNS_VOLUME_DIR},dst=/root/coredns \
+        coredns/coredns:latest \
+        -conf /root/coredns/Corefile
+}
+
+######################################################################
+
+docker_run_coredns
+# podman_run_coredns
