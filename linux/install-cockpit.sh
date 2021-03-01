e1ff42b29c27577d91a6b1f28a0aaf9ea626fa59
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-02 05:19:31 +0900

    update cockpit

--- linux/install-cockpit.sh
+++ linux/install-cockpit.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
 sudo yum install -y cockpit
+sudo yum install -y cockpit-networkmanager
+sudo yum install -y cockpit-packagekit
+sudo yum install -y cockpit-storaged
+sudo yum install -y cockpit-pcp
+
+sudo yum install -y cockpit-dashboard
+
+sudo yum install -y cockpit-machines
+sudo yum install -y cockpit-podman
+
+sudo systemctl enable cockpit.socket
+sudo systemctl start cockpit.socket
+sudo systemctl start cockpit.service
+
+sudo systemctl enable pmcd
+sudo systemctl start pmcd
+sudo systemctl enable pmlogger
+sudo systemctl start pmlogger
+
 sudo firewall-cmd --permanent --zone=public --add-service=cockpit
 sudo firewall-cmd --reload
 
