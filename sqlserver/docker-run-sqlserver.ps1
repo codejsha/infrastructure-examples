0440b8d00797b3d99ce2ffe11775228a8a007e9c
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-04 03:54:15 +0900

    update sqlserver docker

--- sqlserver/docker-run-sqlserver.ps1
+++ sqlserver/docker-run-sqlserver.ps1
@@ -1,7 +1,32 @@
-docker container run `
-    --detach `
-    --name sqlserver2017 `
-    --publish 1433:1433 `
-    --env "ACCEPT_EULA=Y" `
-    --env "SA_PASSWORD=changeit" `
-    mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04
+$password = "$password"
+
+######################################################################
+
+function docker_run_sqlserver2017 {
+    docker container run `
+        --detach `
+        --name sqlserver2017 `
+        --publish 1433:1433 `
+        --env ACCEPT_EULA="Y" `
+        --env MSSQL_PID="Developer" `
+        --env SA_PASSWORD="${PASSWORD}" `
+        --env MSSQL_AGENT_ENABLED="True" `
+        mcr.microsoft.com/mssql/server:2017-latest
+}
+
+function docker_run_sqlserver2019 {
+    docker container run `
+        --detach `
+        --name sqlserver2019 `
+        --publish 1433:1433 `
+        --env ACCEPT_EULA="Y" `
+        --env MSSQL_PID="Developer" `
+        --env SA_PASSWORD="${PASSWORD}" `
+        --env MSSQL_AGENT_ENABLED="True" `
+        mcr.microsoft.com/mssql/server:2019-latest
+}
+
+######################################################################
+
+# docker_run_sqlserver2017
+docker_run_sqlserver2019
