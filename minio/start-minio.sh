a81d39d46d13dd19fef29bedf13808d6dc72210b
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-04 10:37:37 +0900

    add minio scripts

---
+++ minio/start-minio.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PASSWORD="${PASSWORD}"
+
+SERVER_NAME="minio"
+export MINIO_ROOT_USER="admin"
+export MINIO_ROOT_PASSWORD="${PASSWORD}"
+MINIO_DIR="/mnt/minio"
+DATA_DIR="${MINIO_DIR}/data"
+LOG_DIR="${MINIO_DIR}/logs"
+
+sudo mkdir -p ${DATA_DIR}
+sudo mkdir -p ${LOG_DIR}/backup
+sudo chown -R $(id -un):$(id -gn) ${MINIO_DIR}
+
+GET_DATE="$(date +'%Y%m%d_%H%M%S')"
+if [ -f "${LOG_DIR}/nohup.${SERVER_NAME}.out" ]; then
+    mv ${LOG_DIR}/nohup.${SERVER_NAME}.out ${LOG_DIR}/backup/nohup.${SERVER_NAME}.${GET_DATE}.out
+fi
+
+nohup minio server ${DATA_DIR} > ${LOG_DIR}/nohup.${SERVER_NAME}.out 2>&1 &
---
+++ minio/stop-minio.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pgrep minio | xargs kill -9
