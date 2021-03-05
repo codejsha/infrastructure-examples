a3630e3c9b09f7f9a732f7c9376662acbcd07847
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-05 15:23:15 +0900

    update confluent hub plugin install

--- kafka/confluent/install-confluent-hub-plugin.sh
+++ kafka/confluent/install-confluent-hub-plugin.sh
@@ -4,23 +4,31 @@ CONFLUENT_HOME="/usr/local/confluent"
 
 ### source
 ${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-datagen:latest
-${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-oracle-cdc:latest
 ${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-s3-source:latest
+${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-azure-blob-storage-source:latest
+${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-gcs-source:latest
 ${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-hdfs3-source:latest
+${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-jms:latest
+${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-oracle-cdc:latest
 ${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt debezium/debezium-connector-postgresql:latest
 ${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt debezium/debezium-connector-mysql:latest
 ${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt debezium/debezium-connector-sqlserver:latest
 ${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt debezium/debezium-connector-mongodb:latest
 
 ### sink
-${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-elasticsearch:latest
 ${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-s3:latest
-${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-http:latest
+${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-azure-blob-storage:latest
+${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-gcs:latest
 ${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-hdfs3:latest
+${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-elasticsearch:latest
+${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-http:latest
 ${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-cassandra:latest
+${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-jms-sink:1.3.2
 ${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt wepay/kafka-connect-bigquery:latest
 
 ### source and sink
 ${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-replicator:latest
 ${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-jdbc:latest
+${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-mqtt:latest
+${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-influxdb:latest
 ${CONFLUENT_HOME}/bin/confluent-hub install --no-prompt mongodb/kafka-connect-mongodb:latest
