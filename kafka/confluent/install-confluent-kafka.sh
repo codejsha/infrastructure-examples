79ff09717f90e7e1a3518b4f855dcdf348a01c66
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-05 20:33:38 +0900

    update kafka install

--- kafka/confluent/install-confluent-kafka.sh
+++ kafka/confluent/install-confluent-kafka.sh
@@ -1,19 +1,57 @@
 #!/bin/bash
+set -o errexit -o errtrace
 
 function install_confluent_kafka_with_download {
-    CONFLUENT_MAJOR_VERSION="6.0"
-    CONFLUENT_VERSION="6.0.1"
+    CONFLUENT_HOME="/usr/local/confluent"
+    INSTALL_FILE_DIR="/mnt/share/confluent-platform"
+    INSTALL_FILE="confluent-6.1.0.tar.gz"
 
-    curl -LJO http://packages.confluent.io/archive/${CONFLUENT_MAJOR_VERSION}/confluent-${CONFLUENT_VERSION}.tar.gz
-    tar -xzf confluent-${CONFLUENT_VERSION}.tar.gz
+    PARENT_CONFLUENT_HOME="$(readlink --canonicalize-missing ${PARENT_CONFLUENT_HOME}/../)"
+    CONFLUENT_VERSION="$(echo ${INSTALL_FILE} | grep -o -E "([^confluent-].*[^\.tar\.gz])")"
+    CONFLUENT_MAJOR_VERSION="$(echo ${CONFLUENT_VERSION} | grep -o -E "^[0-9]{1,3}")"
 
-    sudo mv confluent-${CONFLUENT_VERSION} /usr/local/confluent
-    sudo /usr/local/confluent/confluent completion bash > /etc/bash_completion.d/confluent
-    source /etc/bash_completion.d/confluent
+    function check_confluent_home {
+        if [ -d "${CONFLUENT_HOME}" ]; then
+            echo "[ERROR] The CONFLUENT_HOME (${CONFLUENT_HOME}) already exists!"
+            exit
+        fi
+    }
 
-    ### add to .bashrc
+    function download_install_file {
+        if [ ! -f "${INSTALL_FILE_DIR}/${INSTALL_FILE}" ]; then
+            sudo curl -o ${INSTALL_FILE_DIR}/${INSTALL_FILE} \
+                -LJO http://packages.confluent.io/archive/${CONFLUENT_MAJOR_VERSION}/${INSTALL_FILE}
+        fi
+    }
+
+    function check_install_file {
+        if [ ! -f "${INSTALL_FILE_DIR}/${INSTALL_FILE}" ]; then
+            echo "[ERROR] The install file (${INSTALL_FILE_DIR}/${INSTALL_FILE}) does not exist!"
+            exit
+        fi
+    }
+
+    function install_confluent {
+        sudo tar -xzf ${INSTALL_FILE_DIR}/${INSTALL_FILE} -C ${PARENT_CONFLUENT_HOME}
+        sudo mv ${PARENT_CONFLUENT_HOME}/confluent-${CONFLUENT_VERSION} ${CONFLUENT_HOME}
+        sudo chown -R $(id -un):$(id -gn) ${CONFLUENT_HOME}
+    }
+
+    function install_bash_completion {
+        sudo yum install -y bash-completion
+        sudo /usr/local/confluent/bin/confluent completion bash | sudo tee /etc/bash_completion.d/confluent
+    }
+
+    check_confluent_home
+    download_install_file
+    check_install_file
+    install_confluent
+    install_bash_completion
+
+    ### add path
     # CONFLUENT_HOME="/usr/local/confluent"
     # PATH="${PATH}:${CONFLUENT_HOME}/bin"
+    # export PATH
 }
 
 function install_confluent_kafka_with_yum {
