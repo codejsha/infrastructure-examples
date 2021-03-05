550c246261fdbaffad5e315164a30077023c2745
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-05 17:57:21 +0900

    update helm chart install

--- kafka/confluent/kubernetes/helm-install-confluent-control-center.sh
+++ kafka/confluent/kubernetes/helm-install-confluent-control-center.sh
@@ -5,10 +5,13 @@ RELEASE_NAME="confluent-control-center"
 KUBE_CONTEXT="$(kubectl config current-context)"
 
 # helm install ${RELEASE_NAME} \
+# helm upgrade ${RELEASE_NAME} \
 helm upgrade --install ${RELEASE_NAME} \
     --kube-context ${KUBE_CONTEXT} \
     --create-namespace \
     --namespace ${NAMESPACE} \
     --values helm-chart-values-private.yaml \
+    --values helm-chart-values-license.yaml \
     --set controlcenter.enabled=true \
     ./confluent-operator/helm/confluent-operator
+
--- kafka/confluent/kubernetes/helm-install-confluent-operator.sh
+++ kafka/confluent/kubernetes/helm-install-confluent-operator.sh
@@ -5,10 +5,12 @@ RELEASE_NAME="confluent-operator"
 KUBE_CONTEXT="$(kubectl config current-context)"
 
 # helm install ${RELEASE_NAME} \
+# helm upgrade ${RELEASE_NAME} \
 helm upgrade --install ${RELEASE_NAME} \
     --kube-context ${KUBE_CONTEXT} \
     --create-namespace \
     --namespace ${NAMESPACE} \
     --values helm-chart-values-private.yaml \
+    --values helm-chart-values-license.yaml \
     --set operator.enabled=true \
     ./confluent-operator/helm/confluent-operator
