c3b86d9d04dd973b9af7b9b946354675e8c27bff
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-06 22:36:27 +0900

    update exclude

--- trim-end-whitespace.ps1
+++ trim-end-whitespace.ps1
@@ -1,4 +1,4 @@
-Get-ChildItem -Recurse -File -Exclude .\contents |
+Get-ChildItem -Recurse -File | Where-Object { $_.FullName -notlike "*\contents\*" } |
 ForEach-Object {
     # Write-Host "[INFO] file path: $_"
     (Get-Content -Path $_).TrimEnd() | Set-Content -Path $_
