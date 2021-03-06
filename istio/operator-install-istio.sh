965de117334727db1b58eec5fa34799efeda9d2f
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-06 23:45:37 +0900

    update istio install

--- istio/operator-install-istio.sh
+++ istio/operator-install-istio.sh
@@ -4,12 +4,15 @@ set -o errexit
 trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: func ${FUNCNAME[0]}: status ${?}"' ERR
 
 curl -sL https://istio.io/downloadIstioctl | sh -
+# curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.9.1 TARGET_ARCH=x86_64 sh -
+
 export PATH=$PATH:$HOME/.istioctl/bin
 istioctl version
 
 istioctl operator init
 kubectl create namespace istio-system
 
+### profile list
 istioctl profile list
 ### Istio configuration profiles:
 ###     minimal
@@ -19,7 +22,11 @@ istioctl profile list
 ###     demo
 ###     empty
 
+### install
 # istioctl install --set profile=default
 istioctl install --skip-confirmation --set profile=default
 # istioctl install --skip-confirmation --set profile=default \
 #    --set meshConfig.accessLogFile=/dev/stdout
+
+### verify
+istioctl manifest generate --set profile=default | istioctl verify-install -f -
