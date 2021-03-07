f1954ddc0931599bc0a9a14e493b80be79b7e2be
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-07 22:56:40 +0900

    update all.sh

--- kafka/confluent/kubernetes/all.sh
+++ kafka/confluent/kubernetes/all.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+set -o errtrace
+set -o errexit
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: func ${FUNCNAME[0]}: status ${?}"' ERR
+
+kubectl create namespace confluent-operator
+
+curl -LJO https://platform-ops-bin.s3-us-west-1.amazonaws.com/operator/confluent-operator-1.7.0.tar.gz
+mkdir confluent-operator
+tar -xzf confluent-operator-1.7.0.tar.gz -C confluent-operator
 
 bash ./helm-install-confluent-operator.sh
 bash ./helm-install-confluent-zookeeper.sh
@@ -6,5 +15,5 @@ bash ./helm-install-confluent-kafka.sh
 bash ./helm-install-confluent-schema-registry.sh
 bash ./helm-install-confluent-kafka-connect.sh
 bash ./helm-install-confluent-replicator.sh
-bash ./helm-install-confluent-ksql.sh
+bash ./helm-install-confluent-ksqldb.sh
 bash ./helm-install-confluent-control-center.sh
