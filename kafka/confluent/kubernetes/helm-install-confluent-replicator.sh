3b42375df1bb6435924eb14290c51a78fcc29392
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-08 22:02:38 +0900

    rename file

--- kafka/confluent/kubernetes/helm-install-confluent-replicator.sh
+++ kafka/confluent/kubernetes/helm-install-confluent-replicator.sh
@@ -10,7 +10,7 @@ helm upgrade --install ${RELEASE_NAME} \
     --kube-context ${KUBE_CONTEXT} \
     --create-namespace \
     --namespace ${NAMESPACE} \
-    --values helm-chart-values-private.yaml \
+    --values helm-chart-values.yaml \
     --values helm-chart-values-license.yaml \
     --set replicator.enabled=true \
     ./confluent-operator/helm/confluent-operator
