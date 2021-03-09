7e915b93b949b0e06c451f140fa3eb5158b97090
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-09 17:02:14 +0900

    update confluent kubernetes

--- kafka/confluent/kubernetes/all.sh
+++ kafka/confluent/kubernetes/all.sh
@@ -3,8 +3,6 @@ set -o errtrace
 set -o errexit
 trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: func ${FUNCNAME[0]}: status ${?}"' ERR
 
-kubectl create namespace confluent-operator
-
 if [ ! -d "confluent-operator" ]; then
     curl -LJO https://platform-ops-bin.s3-us-west-1.amazonaws.com/operator/confluent-operator-1.7.0.tar.gz
     mkdir confluent-operator
