6c1b9825d24c07024986e8d43ab72e3b2321a71f
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-09 09:38:03 +0900

    add confluent openshift

---
+++ kafka/confluent/openshift/all.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -o errtrace
+set -o errexit
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: func ${FUNCNAME[0]}: status ${?}"' ERR
+
+kubectl create namespace confluent-operator
+
+if [ ! -d "confluent-operator" ]; then
+    curl -LJO https://platform-ops-bin.s3-us-west-1.amazonaws.com/operator/confluent-operator-1.7.0.tar.gz
+    mkdir confluent-operator
+    tar -xzf confluent-operator-1.7.0.tar.gz -C confluent-operator
+fi
+
+cd ../kubernetes
+
+bash ./helm-install-confluent-operator.sh
+bash ./wait-pod-ready-status.sh "app=cc-operator"
+
+cd ../openshift
+bash ./set-security-context.sh
+cd ../kubernetes
+
+bash ./helm-install-confluent-zookeeper.sh
+bash ./wait-pod-ready-status.sh "type=zookeeper"
+
+bash ./helm-install-confluent-kafka.sh
+bash ./wait-pod-ready-status.sh "type=kafka"
+
+bash ./helm-install-confluent-schema-registry.sh
+bash ./wait-pod-ready-status.sh "type=schemaregistry"
+
+bash ./helm-install-confluent-kafka-connect.sh
+bash ./wait-pod-ready-status.sh "type=connectors"
+
+bash ./helm-install-confluent-replicator.sh
+bash ./wait-pod-ready-status.sh "type=replicator"
+
+bash ./helm-install-confluent-ksqldb.sh
+bash ./wait-pod-ready-status.sh "type=ksql"
+
+bash ./helm-install-confluent-control-center.sh
+bash ./wait-pod-ready-status.sh "type=controlcenter"
+
+### uninstall
+# helm uninstall confluent-control-center
+# helm uninstall confluent-ksqldb
+# helm uninstall confluent-replicator
+# helm uninstall confluent-kafka-connect
+# helm uninstall confluent-schema-registry
+# helm uninstall confluent-kafka
+# helm uninstall confluent-zookeeper
+# helm uninstall confluent-operator
---
+++ kafka/confluent/openshift/set-security-context.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+oc create -f confluent-operator/scripts/openshift/customUID/scc.yaml
