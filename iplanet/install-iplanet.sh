cd4a41330d8f944a1c23b1449008679e20009007
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-30 16:51:48 +0900

    update iplanet

--- iplanet/install-iplanet.sh
+++ iplanet/install-iplanet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -o errtrace
 set -o errexit
-trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: func ${FUNCNAME[0]}: status ${?}"' ERR
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
 
 INSTALL_FILE_DIR="/mnt/share/oracle-iplanet-web-server/Oracle-iPlanet-Web-Server-7.0.27-linux-x64"
 INSTALL_SCRIPT_DIR="/svc/infrastructure/iplanet"
