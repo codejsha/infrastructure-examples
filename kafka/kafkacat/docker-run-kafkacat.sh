2ad6d78ad6e84caaf3bf8f325914ecbbec6d0cb6
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-30 17:34:58 +0900

    add kafkacat

---
+++ kafka/kafkacat/docker-run-kafkacat.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -o errtrace
+set -o errexit
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+
+docker run -it --network=host edenhill/kafkacat:1.6.0
