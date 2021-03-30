254acc2b9d603db319fbe26f1a2ee3def243903d
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-30 16:43:41 +0900

    update gitlab

--- git/gitlab/docker-run-gitlab.ps1
+++ git/gitlab/docker-run-gitlab.ps1
@@ -1,20 +1,28 @@
-# $GitLabVolumeDir="C:\volume\gitlab"
-$GitLabVolumeDir="$env:USERPROFILE\volume\gitlab"
+# $GitLabVolumeDir = "C:\volume\gitlab"
+$GitLabVolumeDir = "$env:USERPROFILE\volume\gitlab"
 New-Item -Path $GitLabVolumeDir -ItemType Directory -Force
 New-Item -Path $GitLabVolumeDir\config -ItemType Directory -Force
-New-Item -Path $GitLabVolumeDir\logs -ItemType Directory -Force
 New-Item -Path $GitLabVolumeDir\data -ItemType Directory -Force
+New-Item -Path $GitLabVolumeDir\logs -ItemType Directory -Force
 
-docker container run `
-    --detach `
-    --name my-gitlab `
-    --restart unless-stopped `
-    --hostname gitlab `
-    --publish 9443:443 `
-    --publish 9480:9480 `
-    --publish 9422:22 `
-    --env GITLAB_OMNIBUS_CONFIG="external_url 'http://gitlab.example.com:9480'; gitlab_rails['lfs_enabled'] = true; gitlab_rails['gitlab_shell_ssh_port'] = 9422;" `
-    --mount type=bind,src=$GitLabVolumeDir/config,dst=/etc/gitlab `
-    --mount type=bind,src=$GitLabVolumeDir/logs,dst=/var/log/gitlab `
-    --mount type=bind,src=$GitLabVolumeDir/data,dst=/var/opt/gitlab `
-    gitlab/gitlab-ce:latest
+######################################################################
+
+function New-DockerRunGitLab {
+    docker container run `
+        --detach `
+        --name my-gitlab `
+        --restart unless-stopped `
+        --hostname gitlab `
+        --publish 9443:443 `
+        --publish 9480:9480 `
+        --publish 9422:22 `
+        --env GITLAB_OMNIBUS_CONFIG="external_url 'http://gitlab.example.com:9480'; gitlab_rails['lfs_enabled'] = true; gitlab_rails['gitlab_shell_ssh_port'] = 9422;" `
+        --mount type="bind",src="$GitLabVolumeDir/config",dst="/etc/gitlab" `
+        --mount type="bind",src="$GitLabVolumeDir/data",dst="/var/opt/gitlab" `
+        --mount type="bind",src="$GitLabVolumeDir/logs",dst="/var/log/gitlab" `
+        gitlab/gitlab-ce:latest
+}
+
+######################################################################
+
+New-DockerRunGitLab
--- git/gitlab/docker-run-gitlab.sh
+++ git/gitlab/docker-run-gitlab.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 set -o errtrace
 set -o errexit
-trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: func ${FUNCNAME[0]}: status ${?}"' ERR
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
 
 GITLAB_VOLUME_DIR="/mnt/volume/gitlab"
-sudo rm -rf ${GITLAB_VOLUME_DIR}
-sudo mkdir ${GITLAB_VOLUME_DIR}
-sudo mkdir ${GITLAB_VOLUME_DIR}/config
-sudo mkdir ${GITLAB_VOLUME_DIR}/logs
-sudo mkdir ${GITLAB_VOLUME_DIR}/data
+sudo mkdir -p ${GITLAB_VOLUME_DIR}/{config,logs,data}
 
-docker container run \
-    --detach \
-    --name my-gitlab \
-    --restart unless-stopped \
-    --hostname gitlab \
-    --publish 9443:443 \
-    --publish 9480:9480 \
-    --publish 9422:22 \
-    --env GITLAB_OMNIBUS_CONFIG="external_url 'http://gitlab.example.com:9480'; gitlab_rails['lfs_enabled'] = true; gitlab_rails['gitlab_shell_ssh_port'] = 9422;" \
-    --mount type=bind,src=${GITLAB_VOLUME_DIR}/config,dst=/etc/gitlab \
-    --mount type=bind,src=${GITLAB_VOLUME_DIR}/logs,dst=/var/log/gitlab \
-    --mount type=bind,src=${GITLAB_VOLUME_DIR}/data,dst=/var/opt/gitlab \
-    gitlab/gitlab-ce:latest
+######################################################################
+
+function docker_run_gitlab {
+    docker container run \
+        --detach \
+        --name gitlab \
+        --restart unless-stopped \
+        --hostname gitlab \
+        --publish 9443:443 \
+        --publish 9480:9480 \
+        --publish 9422:22 \
+        --env GITLAB_OMNIBUS_CONFIG="external_url 'http://gitlab.example.com:9480'; gitlab_rails['lfs_enabled'] = true; gitlab_rails['gitlab_shell_ssh_port'] = 9422;" \
+        --mount type="bind",src="${GITLAB_VOLUME_DIR}/config",dst="/etc/gitlab" \
+        --mount type="bind",src="${GITLAB_VOLUME_DIR}/data",dst="/var/opt/gitlab" \
+        --mount type="bind",src="${GITLAB_VOLUME_DIR}/logs",dst="/var/log/gitlab" \
+        gitlab/gitlab-ce:latest
+}
+
+######################################################################
+
+docker_run_gitlab
