a43381ce176d971856681d2524dcc66bb3d97810
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-31 13:10:31 +0900

    update ohs

--- ohs/domain/create-component.sh
+++ ohs/domain/create-component.sh
@@ -9,5 +9,4 @@ source ./env-component.sh
 ORACLE_HOME="${ORACLE_HOME}"
 
 export CONFIG_JVM_ARGS="${CONFIG_JVM_ARGS} -Djava.security.egd=file:///dev/urandom"
-
 ${ORACLE_HOME}/oracle_common/common/bin/wlst.sh create_component.py
