ae9f8d61826f0670ef49cf90cdb7d7ab4bb4e799
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-31 13:35:38 +0900

    update redis

--- redis/docker-run-redis.sh
+++ redis/docker-run-redis.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -o errtrace
 set -o errexit
-trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: func ${FUNCNAME[0]}: status ${?}"' ERR
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
 
-docker container run \
-    --detach \
-    --name redis-server \
-    --publish 6379:6379 \
-    redis:latest
+######################################################################
+
+function docker_run_redis {
+    docker container run \
+        --detach \
+        --name redis-server \
+        --publish 6379:6379 \
+        redis:latest
+}
+
+######################################################################
+
+docker_run_redis
