b68e30b29a2f6bed7faa1b7983986c778a04917d
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-31 11:37:58 +0900

    update ohs scripts

--- ohs/scripts/12cr2-metrics/get-metrics-system-ohs1.sh
+++ ohs/scripts/12cr2-metrics/get-metrics-system-ohs1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -o errtrace
 set -o errexit
-trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: func ${FUNCNAME[0]}: status ${?}"' ERR
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
 export PS4="\e[33;1m+ \e[0m"
 set -o xtrace
 
