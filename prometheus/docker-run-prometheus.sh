99d6a83161ecc7ae87658502ae9b14a898534ff7
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-31 13:35:15 +0900

    update prometheus

--- prometheus/docker-run-prometheus.sh
+++ prometheus/docker-run-prometheus.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 set -o errtrace
 set -o errexit
-trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: func ${FUNCNAME[0]}: status ${?}"' ERR
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
 
 PROMETHEUS_VOLUME_DIR="/mnt/volume/prometheus"
 sudo mkdir -p ${PROMETHEUS_VOLUME_DIR}
 sudo /bin/cp -f prometheus.yaml ${PROMETHEUS_VOLUME_DIR}/prometheus.yml
 
-docker container run \
-    --detach \
-    --name prometheus \
-    --publish 9090:9090 \
-    --mount type=bind,src=${PROMETHEUS_VOLUME_DIR},dst=/etc/prometheus \
-    prom/prometheus
+######################################################################
+
+function docker_run_prometheus {
+    docker container run \
+        --detach \
+        --name prometheus \
+        --publish 9090:9090 \
+        --mount type="bind",src="${PROMETHEUS_VOLUME_DIR}",dst="/etc/prometheus" \
+        prom/prometheus
+}
+
+######################################################################
+
+docker_run_prometheus
