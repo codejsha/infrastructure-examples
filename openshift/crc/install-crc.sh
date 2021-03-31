aa5fd9fb307cb9fb8f0bd97a95f045b0c5fab30a
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-31 13:11:08 +0900

    update openshift crc

--- openshift/crc/install-crc.sh
+++ openshift/crc/install-crc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -o errtrace
 set -o errexit
-trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: func ${FUNCNAME[0]}: status ${?}"' ERR
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
 
 INSTALL_FILE_DIR="/mnt/share/redhat-codeready-containers"
 INSTALL_FILE="crc-linux-amd64.tar.xz"
