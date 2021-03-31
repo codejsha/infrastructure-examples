be8826bc552bd94a31b6629500fafeafbfd80827
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-03-31 10:58:04 +0900

    add sonobuoy

---
+++ kubernetes/sonobuoy/install-sonobuoy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o errtrace
+set -o errexit
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+
+SONOBUOY_VERSION="0.50.0"
+
+curl -LJO https://github.com/vmware-tanzu/sonobuoy/releases/download/v${SONOBUOY_VERSION}/sonobuoy_${SONOBUOY_VERSION}_linux_amd64.tar.gz
+tar -xzf sonobuoy_${SONOBUOY_VERSION}_linux_amd64.tar.gz
+sudo mv sonobuoy /usr/local/bin
+sudo chown root:root /usr/local/bin/sonobuoy
+
+rm -f LICENSE sonobuoy_${SONOBUOY_VERSION}_linux_amd64.tar.gz
