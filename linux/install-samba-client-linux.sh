edd184b8df4a887409110d1b1f646a9df626bbf7
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-04-06 22:36:45 +0900

    update samba client

--- linux/install-samba-client-linux.sh
+++ linux/install-samba-client-linux.sh
@@ -19,7 +19,7 @@ sudo yum install -y \
 # sudo mount -t cifs -o user=shareuser,password=${PASSWORD} //192.168.137.1/storage /mnt/storage
 
 ### auto
-echo "//192.168.137.1/share   /mnt/share              cifs    username=shareuser,password=${PASSWORD},file_mode=0555,dir_mode=0555 0 0" | sudo tee -a /etc/fstab
+echo "//192.168.137.1/share   /mnt/share              cifs    username=shareuser,password=${PASSWORD},file_mode=0755,dir_mode=0755 0 0" | sudo tee -a /etc/fstab
 echo "//192.168.137.1/storage /mnt/storage            cifs    username=shareuser,password=${PASSWORD},uid=prouser,gid=progroup 0 0" | sudo tee -a /etc/fstab
 
 ### umount
