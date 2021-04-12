888314ae94e1b3b161fa9c747b6404e496cdf6b6
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-04-12 09:38:30 +0900

    update git choco install

--- git/choco-install-git.ps1
+++ git/choco-install-git.ps1
@@ -1,1 +1,15 @@
-choco install --confirm git.install
+# choco uninstall --confirm git.install
+choco install --confirm git.install --package-parameters="/NoShellIntegration"
+
+### package parameters
+###
+### /GitOnlyOnPath
+### /GitAndUnixToolsOnPath
+### /NoAutoCrlf
+### /WindowsTerminal
+### /NoShellIntegration
+### /NoGuiHereIntegration
+### /NoShellHereIntegration
+### /NoCredentialManager
+### /NoGitLfs
+### /SChannel
