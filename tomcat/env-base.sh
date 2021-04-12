e2f92df23a4f4f17e609cf6543c3c0cab54f4aa6
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-04-12 09:37:02 +0900

    update env base

--- tomcat/env-base.sh
+++ tomcat/env-base.sh
@@ -4,6 +4,9 @@ set -o errexit
 trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
 
 export JAVA_HOME="/usr/lib/jvm/java-11"
+export JRE_HOME="/usr/lib/jvm/jre-11"
+# export JAVA_HOME="/usr/lib/jvm/java-1.8.0"
+# export JRE_HOME="/usr/lib/jvm/jre-1.8.0"
 export CATALINA_HOME="/usr/local/tomcat"
 
 ######################################################################
