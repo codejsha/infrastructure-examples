2a7e46f886bb4455f7ced1d4e3462d6750bbdc4c
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-04-12 11:16:33 +0900

    add spring boot

---
+++ java/spring-boot/spring-boot-cli-commands.ps1
@@ -0,0 +1,8 @@
+######################################################################
+
+### view supported dependencies, project types, parameters
+spring init --list
+
+######################################################################
+
+spring init --name=demo --artifactId=demo --dependencies=web,lombok my-app
