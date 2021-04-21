48e38551a55df35c8319ef02acfc3eb6ce8c312c
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-04-22 01:30:10 +0900

    update external postgresql for harbor

--- postgresql/helm/helm-install-postgresql.sh
+++ postgresql/helm/helm-install-postgresql.sh
@@ -10,12 +10,33 @@ export PASSWORD="${PASSWORD}"
 
 envsubst < ./helm-chart-values.yaml > ./helm-chart-values-temp.yaml
 
-NAMESPACE="postgres-system"
+######################################################################
 
-# helm install my-postgres \
-helm upgrade --install my-postgres \
-    --create-namespace \
-    --namespace ${NAMESPACE} \
-    --values helm-chart-values-temp.yaml \
-    --version 10.3.3 \
-    bitnami/postgresql
+function helm_install_postgresql {
+    local NAMESPACE="postgres-system"
+
+    # helm install my-postgres \
+    helm upgrade --install my-postgres \
+        --create-namespace \
+        --namespace ${NAMESPACE} \
+        --values helm-chart-values-temp.yaml \
+        --version 10.3.18 \
+        bitnami/postgresql
+}
+
+function helm_install_postgresql_harbor {
+    local NAMESPACE="harbor-system"
+
+    # helm install my-harbor-postgres \
+    helm upgrade --install my-harbor-postgres \
+        --create-namespace \
+        --namespace ${NAMESPACE} \
+        --values helm-chart-values-temp.yaml \
+        --version 10.3.18 \
+        bitnami/postgresql
+}
+
+######################################################################
+
+helm_install_postgresql
+helm_install_postgresql_harbor
