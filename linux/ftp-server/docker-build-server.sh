67f547fc09839be7087e0196d84a662c96858542
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-04-22 00:48:51 +0900

    add sftp server

--- linux/ftp-server/docker-build-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -o errtrace
-set -o errexit
-trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
-
-# docker image build .
-docker image build --tag ftp-server:latest .
-# docker image build --file Dockerfile --tag ftp-server:latest .
---
+++ linux/ftp-server/docker-run-sftp-server.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -o errtrace
+set -o errexit
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+
+USERNAME="user"
+PASSWORD="${PASSWORD}"
+
+######################################################################
+
+function docker_run_sftp_server_share {
+    docker container run \
+        --detach \
+        --name sftp-server \
+        --publish 2222:22 \
+        --mount type="bind",src="/mnt/share",dst="/home/${USERNAME}/share" \
+        atmoz/sftp \
+        ${USERNAME}:${PASSWORD}:1000
+}
+
+function docker_run_sftp_server_pub {
+    local SFTP_SERVER_VOLUME_DIR="/mnt/volume/ftp/pub"
+    sudo mkdir -p ${SFTP_SERVER_VOLUME_DIR}
+    # sudo mkdir -p ${SFTP_SERVER_VOLUME_DIR}/{data,error,finished}
+    # sudo chown -R 1000 ${SFTP_SERVER_VOLUME_DIR}
+
+    docker container run \
+        --detach \
+        --name sftp-server \
+        --publish 2222:22 \
+        --mount type="bind",src="${SFTP_SERVER_VOLUME_DIR}",dst="/home/${USERNAME}/pub" \
+        atmoz/sftp \
+        ${USERNAME}:${PASSWORD}:1000
+}
+
+######################################################################
+
+docker_run_sftp_server_share
+# docker_run_sftp_server_pub
