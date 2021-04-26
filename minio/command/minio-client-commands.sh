a2281ccae698607d9b06d93c1e0713e4fa7cf4a9
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-04-26 20:38:33 +0900

    update commands

--- minio/command/minio-client-commands.sh
+++ minio/command/minio-client-commands.sh
@@ -2,14 +2,14 @@
 
 ### host config
 
-MINIO_ALIAS="test-minio"
-MINIO_URL="http://test.example.com:9000"
+MINIO_ALIAS="minio-tenant-1"
+MINIO_URL="https://minio-tenant-1.example.com"
 
 MINIO_ALIAS="my-minio"
 MINIO_URL="http://minio.example.com"
 
-MINIO_ALIAS="minio-tenant-1"
-MINIO_URL="https://minio-tenant-1.example.com"
+MINIO_ALIAS="test-minio"
+MINIO_URL="http://test.example.com:9000"
 
 AWS_ACCESS_KEY="${AWS_ACCESS_KEY}"
 AWS_SECRET_KEY="${AWS_SECRET_KEY}"
@@ -19,18 +19,19 @@ mc config host ls
 
 ######################################################################
 
+mc ls --insecure minio-tenant-1/
 mc ls my-minio/
-mc ls minio-tenant-1/
 
 ######################################################################
 
 ### make bucket
 
-mc mb my-minio/default
-mc mb my-minio/harbor-storage
-
 mc mb --insecure minio-tenant-1/default
 mc mb --insecure minio-tenant-1/harbor-storage
+mc mb --insecure minio-tenant-1/kafka-sink-storage
+
+mc mb my-minio/default
+mc mb my-minio/harbor-storage
 
 ######################################################################
 
