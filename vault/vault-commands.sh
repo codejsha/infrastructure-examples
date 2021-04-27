aca97f9bf7bfaaa364873a16c9f201649d364a86
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-04-27 23:16:59 +0900

    update vault commands

--- vault/vault-commands.sh
+++ vault/vault-commands.sh
@@ -46,5 +46,27 @@ vault write auth/kubernetes/config \
 
 ######################################################################
 
+### kv
+
 vault secrets enable -path=infra kv-v2
 vault secrets enable -path=internal kv-v2
+
+######################################################################
+
+### pki
+
+vault secrets enable pki
+vault secrets tune -max-lease-ttl=8760h pki
+
+vault write pki/root/generate/internal \
+
+vault write pki/config/urls \
+    issuing_certificates="http://127.0.0.1:8200/v1/pki/ca" \
+    crl_distribution_points="http://127.0.0.1:8200/v1/pki/crl"
+
+vault policy write pki - <<EOF
+path "pki*"                        { capabilities = ["read", "list"] }
+path "pki/roles/*"   { capabilities = ["create", "update"] }
+path "pki/sign/*"    { capabilities = ["create", "update"] }
+path "pki/issue/*"   { capabilities = ["create"] }
+EOF
