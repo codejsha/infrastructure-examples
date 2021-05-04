d5e02aada6d3464954cc3bdba9dd73638be598eb
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-05-04 21:14:08 +0900

    add datagen users

---
+++ kafka/connector/datagen/register-datagen-connector-users.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -o errtrace
+set -o errexit
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+
+KAFKA_CONNECT_URL="http://localhost:8083"
+# KAFKA_CONNECT_URL="http://kafka-connect1:8083"
+# KAFKA_CONNECT_URL="http://kafka-connect.example.com"
+
+######################################################################
+
+function register_connector {
+    curl \
+        --include \
+        --request POST \
+        --header "Accept:application/json" \
+        --header "Content-Type:application/json" \
+        --data @datagen-connector-users.json \
+        ${KAFKA_CONNECT_URL}/connectors
+    # curl \
+    #     --include \
+    #     --request POST \
+    #     --header "Accept:application/json" \
+    #     --header "Content-Type:application/json" \
+    #     --data @datagen-connector-users-kubernetes.json \
+    #     ${KAFKA_CONNECT_URL}/connectors
+
+    echo
+}
+
+function delete_connector {
+    curl \
+        --include \
+        --request DELETE \
+        --header "Accept:application/json" \
+        ${KAFKA_CONNECT_URL}/connectors/datagen-users
+}
+
+######################################################################
+
+# delete_connector
+register_connector
