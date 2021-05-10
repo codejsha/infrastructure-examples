3d9abc06453906dcf47061993f0ed25c8053e6e7
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-05-10 14:11:28 +0900

    update sudoers

--- linux/user/add-user-to-sudoers.sh
+++ linux/user/add-user-to-sudoers.sh
@@ -3,5 +3,8 @@ set -o errtrace
 set -o errexit
 trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
 
-# sudo sed -i -E '/^root\tALL=\(ALL\) \tALL$/ a prouser\tALL=(ALL) \tALL' /etc/sudoers
-sudo sed -i -E '/^root\tALL=\(ALL\) \tALL$/ a prouser\tALL=(ALL) \tNOPASSWD:ALL' /etc/sudoers
+USER="prouser"
+
+cat <<EOF > /etc/sudoers.d/${USER}
+${USER} ALL=(ALL) NOPASSWD:ALL
+EOF
