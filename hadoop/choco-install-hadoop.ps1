1f742568f89f13474d7b1467bb59c97c60f679d9
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-05-12 02:38:16 +0900

    update hadoop choco install

--- hadoop/choco-install-hadoop.ps1
+++ hadoop/choco-install-hadoop.ps1
@@ -1,3 +1,4 @@
-choco install --confirm hadoop
+# choco install --confirm hadoop
+choco install --confirm hadoop -params "/unzipLocation:$env:USERPROFILE\portable"
 
 # choco uninstall --confirm hadoop
