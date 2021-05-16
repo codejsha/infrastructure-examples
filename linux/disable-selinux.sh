3a9e97c9aea4be4cd8170ac846a59cbce11be7df
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-05-17 04:08:26 +0900

    update selinux

--- linux/disable-selinux.sh
+++ linux/disable-selinux.sh
@@ -5,3 +5,4 @@ trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func $
 
 setenforce 0
 sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+# sed -i 's/^SELINUX=enforcing$/SELINUX=disabled/' /etc/selinux/config
