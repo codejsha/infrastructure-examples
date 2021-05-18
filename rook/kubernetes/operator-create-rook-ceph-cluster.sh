aa3a480304d69219e7a27689c7c53576f4c26d50
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-05-18 13:43:53 +0900

    move files

---
+++ rook/kubernetes/operator-create-rook-ceph-cluster.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -o errtrace
+set -o errexit
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+
+/bin/cp -f cluster.yaml ../rook/cluster/examples/kubernetes/ceph/cluster.yaml
+
+kubectl apply --filename ../rook/cluster/examples/kubernetes/ceph/cluster.yaml
+kubectl apply --filename ../rook/cluster/examples/kubernetes/ceph/toolbox.yaml
+kubectl apply --filename ../rook/cluster/examples/kubernetes/ceph/csi/rbd/storageclass.yaml
+
+# kubectl apply --filename ../rook/cluster/examples/kubernetes/ceph/dashboard-ingress-https.yaml
+# kubectl apply --filename ../rook/cluster/examples/kubernetes/ceph/dashboard-loadbalancer.yaml
+kubectl apply --filename ../dashboard-ingress.yaml
+
+### delete
+# kubectl delete --filename dashboard-ingress.yaml
+# kubectl delete --filename ../rook/cluster/examples/kubernetes/ceph/dashboard-loadbalancer.yaml
+# kubectl delete --filename ../rook/cluster/examples/kubernetes/ceph/dashboard-ingress-https.yaml
+# kubectl delete --filename ../rook/cluster/examples/kubernetes/ceph/csi/rbd/storageclass.yaml
+# kubectl delete --filename ../rook/cluster/examples/kubernetes/ceph/toolbox.yaml
+# kubectl delete --filename ../rook/cluster/examples/kubernetes/ceph/cluster.yaml
