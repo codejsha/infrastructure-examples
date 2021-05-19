d02ca9ed23f5b9eb84558700a69aaf460ad2ed1c
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-05-19 15:48:28 +0900

    update docker service

--- docker/override-docker-service.sh
+++ docker/override-docker-service.sh
@@ -14,7 +14,19 @@ EOF
 # cat <<EOF | sudo tee /etc/systemd/system/docker.service.d/override.conf
 # [Service]
 # ExecStart=
+# ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock --host tcp://0.0.0.0:2375 --dns 8.8.8.8 --dns 168.126.63.1
+# EOF
+
+# cat <<EOF | sudo tee /etc/systemd/system/docker.service.d/override.conf
+# [Service]
+# ExecStart=
 # ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock --host tcp://0.0.0.0:2375 --dns 192.168.137.1 --dns 8.8.8.8 --dns 168.126.63.1
+# EOF
+
+# cat <<EOF | sudo tee /etc/systemd/system/docker.service.d/override.conf
+# [Service]
+# ExecStart=
+# ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock --host tcp://0.0.0.0:2375 --dns 192.168.138.1 --dns 8.8.8.8 --dns 168.126.63.1
 # EOF
 
 sudo systemctl daemon-reload
