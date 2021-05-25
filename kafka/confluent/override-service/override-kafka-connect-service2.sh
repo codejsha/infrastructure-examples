7128e55df077739e1aef62345c5946a61356cd43
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-05-26 02:09:16 +0900

    fix error

--- kafka/confluent/override-service/override-kafka-connect-service2.sh
+++ kafka/confluent/override-service/override-kafka-connect-service2.sh
@@ -18,9 +18,9 @@ Environment="CLASSPATH=/usr/share/java/kafka-connect-replicator/*"
 # Environment="CLASSPATH="/usr/local/confluent/share/java/kafka-connect-replicator/*"
 
 ExecStart=
-# ExecStart=/usr/bin/connect-distributed /etc/kafka/connect-standalone.properties
+# ExecStart=/usr/bin/connect-standalone /etc/kafka/connect-standalone.properties
+# ExecStart=/usr/bin/connect-standalone /etc/schema-registry/connect-avro-standalone.properties
 # ExecStart=/usr/bin/connect-distributed /etc/kafka/connect-distributed.properties
-# ExecStart=/usr/bin/connect-distributed /etc/schema-registry/connect-avro-standalone.properties
 # ExecStart=/usr/bin/connect-distributed /etc/schema-registry/connect-avro-distributed.properties
 ExecStart=/usr/bin/connect-distributed /usr/local/confluent/properties/kafka-connect2.properties
 EOF
