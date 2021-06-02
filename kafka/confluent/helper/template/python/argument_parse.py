3dcac87637ba104b3bde5bfd6d6821bb46c8921b
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-06-02 21:32:14 +0900

    update syntax

--- kafka/confluent/helper/template/python/argument_parse.py
+++ kafka/confluent/helper/template/python/argument_parse.py
@@ -7,8 +7,8 @@ import pathlib
 def get_value_file():
     parser = argparse.ArgumentParser(description='Helper App')
     parser.add_argument('value_file',
-                        default='values.yaml',
                         nargs='?',
+                        default='values.yaml',
                         help='Specify a YAML file (default: values.yaml)')
     args = parser.parse_args()
     value_file = pathlib.Path(args.value_file)
