71aab1786b6ee5490dce1ee3dc9cd7f42d4e6a07
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-06-08 06:17:53 +0900

    fix error

--- weblogic/domain/set_domain_log.py
+++ weblogic/domain/set_domain_log.py
@@ -14,7 +14,7 @@ admin_password = os.environ['ADMIN_PASSWORD']
 def set_domain_log_config(_domain_name, _log_dir):
     cd('/Log/' + _domain_name)
     cmo.setFileName(_log_dir + '/domain/'
-                    'domain_' + '_domain_name' + '_%%yyyy%%%%MM%%%%dd%%_%%HH%%%%mm%%%%ss%%.log')
+                    'domain.' + _domain_name + '.%%yyyy%%%%MM%%%%dd%%_%%HH%%%%mm%%%%ss%%.log')
     # cmo.setFileName('/dev/null')
     cmo.setRotationType('byTime')
     cmo.setRotationTime('00:00')
