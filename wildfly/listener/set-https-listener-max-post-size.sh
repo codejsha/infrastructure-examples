330d2209ec4d4617291f94410d6eb97bdd54830a
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-06-23 10:07:22 +0900

    add max post size

---
+++ wildfly/listener/set-https-listener-max-post-size.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+set -o errexit
+set -o errtrace
+
+source ../env-base.sh
+
+######################################################################
+
+function set_listener_max_post_size {
+    ${JBOSS_HOME}/bin/jboss-cli.sh \
+        --connect \
+        --controller="${BIND_ADDRESS_MGMT}:${JBOSS_MGMT_HTTP_PORT}" \
+        --user="${USERNAME}" \
+        --password="${PASSWORD}" \
+<<EOF
+batch
+/subsystem=undertow/server=default-server/https-listener=https:write-attribute(name=max-post-size,value=10485760)
+run-batch
+quit
+EOF
+}
+
+function reload_server {
+    ${JBOSS_HOME}/bin/jboss-cli.sh \
+        --connect \
+        --controller="${BIND_ADDRESS_MGMT}:${JBOSS_MGMT_HTTP_PORT}" \
+        --user="${USERNAME}" \
+        --password="${PASSWORD}" \
+        --echo-command \
+        --command=":reload()"
+}
+
+######################################################################
+
+set_listener_timeout
+reload_server
