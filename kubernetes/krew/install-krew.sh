caa2dbcd69a70c9e846644d7fbeebd47e85fdc16
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-06-24 12:52:14 +0900

    update krew install

--- kubernetes/krew/install-krew.sh
+++ kubernetes/krew/install-krew.sh
@@ -7,10 +7,12 @@ set -o errtrace
 ### git
 
 (
-    cd "$(mktemp -d)" &&
+    set -x; cd "$(mktemp -d)" &&
+    OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+    ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
     curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.tar.gz" &&
     tar zxvf krew.tar.gz &&
-    KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" &&
+    KREW=./krew-"${OS}_${ARCH}" &&
     "$KREW" install krew
 )
 
