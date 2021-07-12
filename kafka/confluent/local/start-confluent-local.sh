99f0bf5b98512fc60b97644adcfe02d6e6b2c6ae
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-07-13 08:58:39 +0900

    update confluent local

--- kafka/confluent/local/start-confluent-local.sh
+++ kafka/confluent/local/start-confluent-local.sh
@@ -3,10 +3,10 @@ trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func $
 set -o errexit
 set -o errtrace
 
-confluent local services zookeeper start
+confluent local services zookeeper start --config zookeeper.properties
 confluent local services kafka start --config kafka.properties
-confluent local services schema-registry start
-confluent local services connect start
-confluent local services kafka-rest start
-confluent local services ksql-server start
-confluent local services control-center start
+confluent local services schema-registry start --config schema-registry.properties
+confluent local services connect start --config kafka-connect.properties
+confluent local services kafka-rest start --config kafka-rest.properties
+confluent local services ksql-server start --config ksqldb.properties
+confluent local services control-center start --config control-center.properties
