f673f377b4e3fcd7b70dd2c28762a43335f1019e
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-07-13 08:52:09 +0900

    update prometheus

--- prometheus/start-prometheus.sh
+++ prometheus/start-prometheus.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 PROMETHEUS_HOME="/usr/local/prometheus"
+PROMETHEUS_CONFIG_FILE="${PROMETHEUS_HOME}/prometheus.yml"
+LOG_DIR="${PROMETHEUS_HOME}"
 
+### create log directory
+if [ ! -d "${LOG_DIR}/backup" ]; then
+    mkdir -p ${LOG_DIR}/backup
+fi
+
+### backup stdout log
+DATETIME="$(date +'%Y%m%d_%H%M%S')"
+if [ -f "${LOG_DIR}/nohup.prometheus.out" ]; then
+    mv ${LOG_DIR}/nohup.prometheus.out ${LOG_DIR}/backup/nohup.prometheus.${DATETIME}.out
+fi
+
+### start
 nohup ${PROMETHEUS_HOME}/prometheus \
-    --config.file ${PROMETHEUS_HOME}/prometheus.yml \
-    --web.enable-admin-api > ${PROMETHEUS_HOME}/nohup.prometheus.out 2>&1 &
+    --config.file ${PROMETHEUS_HOME} \
+    --web.enable-admin-api > ${LOG_DIR}/nohup.prometheus.out 2>&1 &
--- prometheus/stop-prometheus.sh
+++ prometheus/stop-prometheus.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# PROMETHEUS_HOME="/usr/local/prometheus"
+# PROMETHEUS_CONFIG_FILE="${PROMETHEUS_HOME}/prometheus.yml"
+
 pkill -9 -ecx prometheus
+# pkill -15 -ecx prometheus
+
+# pkill -9 -ecf ${PROMETHEUS_CONFIG_FILE}
+# pkill -15 -ecf ${PROMETHEUS_CONFIG_FILE}
