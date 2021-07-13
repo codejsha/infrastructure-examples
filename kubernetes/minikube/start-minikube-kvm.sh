15ade214e05553fd6f45e74217600d235fba1a6d
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-07-13 09:39:48 +0900

    update minikube

---
+++ kubernetes/minikube/start-minikube-kvm.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+set -o errexit
+set -o errtrace
+
+minikube start \
+    --driver=kvm2 \
+    --nodes 4 \
+    --addons metallb \
+    --addons ingress \
+    --addons ingress-dns \
+    --addons istio-provisioner \
+    --addons istio \
+    --addons dashboard \
+    --addons metrics-server \
+    --addons efk \
+    --addons default-storageclass \
+    --addons storage-provisioner \
+    --addons logviewer \
+    --addons registry \
+    --addons registry-aliases
+
+    # --addons ambassador \
+    # --addons csi-hostpath-driver \
+    # --addons freshpod \
+    # --addons gcp-auth \
+    # --addons gvisor \
+    # --addons helm-tiller \
+    # --addons kubevirt \
+    # --addons nvidia-driver-installer \
+    # --addons nvidia-gpu-device-plugin \
+    # --addons olm \
+    # --addons pod-security-policy \
+    # --addons registry-creds \
+    # --addons storage-provisioner-gluster \
+    # --addons volumesnapshots \
