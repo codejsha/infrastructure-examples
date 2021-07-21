c43c50c610975b2cc79503e869d306c2d50fd394
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-07-21 17:41:33 +0900

    update scripts

--- kafka/confluent/scripts/stop-kafka-connect.sh
+++ kafka/confluent/scripts/stop-kafka-connect.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-CONFLUENT_HOME="/usr/local/confluent"
+# CONFLUENT_HOME="/usr/local/confluent"
 
-# PID="$(pgrep -xa java | grep ${CONFLUENT_HOME} | grep org.apache.kafka.connect.cli.ConnectDistributed | awk '{print $1}')"
-# PID="$(pgrep -xa java | grep ${CONFLUENT_HOME} | grep org.apache.kafka.connect.cli.ConnectStandalone | awk '{print $1}')"
+# PID="$(pgrep -xa java | grep ${CONFLUENT_HOME} | grep "org\.apache\.kafka\.connect\.cli\.ConnectDistributed" | awk '{print $1}')"
 # kill -9 ${PID}
+# kill -15 ${PID}
 
 pkill -9 -ecf "org\.apache\.kafka\.connect\.cli\.ConnectDistributed"
 # pkill -15 -ecf "org\.apache\.kafka\.connect\.cli\.ConnectDistributed"
-
-# pkill -9 -ecf "org\.apache\.kafka\.connect\.cli\.ConnectStandalone"
-# pkill -15 -ecf "org\.apache\.kafka\.connect\.cli\.ConnectStandalone"
