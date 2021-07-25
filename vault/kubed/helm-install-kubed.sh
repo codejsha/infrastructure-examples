c53e3270787f3cd9179b07ff936184a674d2294d
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-07-26 05:46:50 +0900

    add kubed

---
+++ vault/kubed/helm-install-kubed.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+set -o errexit
+set -o errtrace
+
+helm repo add appscode https://charts.appscode.com/stable
+helm repo update
+
+NAMESPACE="kube-system"
+
+# helm install my-vault \
+helm upgrade --install my-kubed \
+    --create-namespace \
+    --namespace ${NAMESPACE} \
+    --version v0.12.0 \
+    appscode/kubed
