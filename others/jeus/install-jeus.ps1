fd8f9821c1e010e953eaab9e3cd7da8fe9f4ad38
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-07-26 05:44:48 +0900

    update jeus

---
+++ others/jeus/install-jeus.ps1
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+install_file_dir="F:\share\tmax-jeus"
+install_file="jeus7_unix_generic_ko.bin"
+install_script_dir="."
+
+"${install_file_dir}\${install_file}" -f "${install_script_dir}\jeus_install_windows.properties"
