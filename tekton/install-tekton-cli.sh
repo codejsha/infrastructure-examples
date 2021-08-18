8ae1779af5d1ed9deb03b656a5652313f16be711
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-08-19 01:40:38 +0900

    update tekton

--- tekton/install-tekton-cli.sh
+++ tekton/install-tekton-cli.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
-set -o errexit
-set -o errtrace
+# trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+# set -o errexit
+# set -o errtrace
 
-TEKTON_CLI_VERSION="0.19.1"
+TEKTON_CLI_VERSION="0.20.0"
 
 curl -LJO https://github.com/tektoncd/cli/releases/download/v${TEKTON_CLI_VERSION}/tkn_${TEKTON_CLI_VERSION}_Linux_x86_64.tar.gz
 sudo tar -zxf tkn_${TEKTON_CLI_VERSION}_Linux_x86_64.tar.gz -C /usr/local/bin/ tkn
--- tekton/install-tekton-trigger.sh
+++ tekton/install-tekton-trigger.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
-set -o errexit
-set -o errtrace
+# trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+# set -o errexit
+# set -o errtrace
 
 NAMESPACE="tekton-pipelines"
 kubectl create namespace ${NAMESPACE}
-kubectl config set-context --current --namespace "${NAMESPACE}"
 
 # kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
-TEKTON_TRIGGER_VERSION="0.14.2"
-kubectl apply --filename https://github.com/tektoncd/triggers/releases/download/v${TEKTON_TRIGGER_VERSION}/release.yaml
+TEKTON_TRIGGER_VERSION="0.15.0"
+kubectl apply --namespace ${NAMESPACE} --filename https://github.com/tektoncd/triggers/releases/download/v${TEKTON_TRIGGER_VERSION}/release.yaml
