e05242ec15f8d81528fcf538f4c2a840f5f1a097
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-08-19 01:40:38 +0900

    update tekton

--- tekton/install-tekton-dashboard.sh
+++ tekton/install-tekton-dashboard.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
-set -o errexit
-set -o errtrace
+# trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+# set -o errexit
+# set -o errtrace
 
 NAMESPACE="tekton-pipelines"
 kubectl create namespace ${NAMESPACE}
-kubectl config set-context --current --namespace "${NAMESPACE}"
 
 # kubectl apply --filename https://storage.googleapis.com/tekton-releases/dashboard/latest/tekton-dashboard-release.yaml
-TEKTON_DASHBOARD_VERSION="0.18.0"
-kubectl apply --filename https://github.com/tektoncd/dashboard/releases/download/v${TEKTON_DASHBOARD_VERSION}/tekton-dashboard-release.yaml
+TEKTON_DASHBOARD_VERSION="0.19.0"
+kubectl apply --namespace ${NAMESPACE} --filename https://github.com/tektoncd/dashboard/releases/download/v${TEKTON_DASHBOARD_VERSION}/tekton-dashboard-release.yaml
--- tekton/install-tekton-pipeline.sh
+++ tekton/install-tekton-pipeline.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
-set -o errexit
-set -o errtrace
+# trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+# set -o errexit
+# set -o errtrace
 
 NAMESPACE="tekton-pipelines"
 kubectl create namespace ${NAMESPACE}
-kubectl config set-context --current --namespace "${NAMESPACE}"
 
 # kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
-TEKTON_PIPELINE_VERSION="0.25.0"
-kubectl apply --filename https://github.com/tektoncd/pipeline/releases/download/v${TEKTON_PIPELINE_VERSION}/release.yaml
+TEKTON_PIPELINE_VERSION="0.27.0"
+kubectl apply --namespace ${NAMESPACE} --filename https://github.com/tektoncd/pipeline/releases/download/v${TEKTON_PIPELINE_VERSION}/release.yaml
