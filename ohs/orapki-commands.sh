c9dbd479797b8569dd7ff71cb2e199b1f6dcf3a9
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-08-20 00:52:24 +0900

    update orapki commands

--- ohs/orapki-commands.sh
+++ ohs/orapki-commands.sh
@@ -1,3 +1,10 @@
 ######################################################################
 
+### display wallet
 ${ORACLE_HOME}/oracle_common/bin/orapki wallet display -wallet ${WALLET_LOCATION}
+${ORACLE_HOME}/oracle_common/bin/orapki wallet display -complete -wallet ${WALLET_LOCATION}
+
+######################################################################
+
+### import certificate
+orapki wallet add -wallet ./remote_wallet -trusted_cert -cert RemoteProxyRoot.pem -pwd ${PASSWORD}
