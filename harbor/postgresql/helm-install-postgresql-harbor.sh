851f3aeee81adf47f6b598800cbcdb9a409fd3de
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-08-20 00:43:38 +0900

    update harbor postgresql

---
+++ harbor/postgresql/helm-install-postgresql-harbor.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+set -o errexit
+set -o errtrace
+
+# helm repo add bitnami https://charts.bitnami.com/bitnami
+# helm repo update
+
+export PASSWORD="${PASSWORD}"
+
+envsubst < ./values.yaml > ./values-temp.yaml
+
+######################################################################
+
+function helm_install_postgresql_harbor {
+    local NAMESPACE="harbor-system"
+
+    # helm install my-harbor-postgres \
+    helm upgrade --install my-harbor-postgres \
+        --create-namespace \
+        --namespace ${NAMESPACE} \
+        --values values-temp.yaml \
+        --values values-harbor.yaml \
+        bitnami/postgresql
+}
+
+######################################################################
+
+helm_install_postgresql_harbor
