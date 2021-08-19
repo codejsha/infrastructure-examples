d53cc2808b79fcfd1374551c0ad9546fa3404d00
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-08-20 00:55:20 +0900

    add putty commands

---
+++ linux/putty/putty-commands.sh
@@ -0,0 +1,11 @@
+######################################################################
+
+### putty
+
+putty.exe -ssh prouser@testmachine
+
+######################################################################
+
+### plink
+
+plink.exe -ssh -t prouser@testmachine -pw 1234 -no-antispoof
