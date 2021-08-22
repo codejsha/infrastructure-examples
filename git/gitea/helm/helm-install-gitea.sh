68bc3ac2db05fb0346312733e9ab16dc82bc73b2
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-08-23 02:43:47 +0900

    add gitea

---
+++ git/gitea/helm/helm-install-gitea.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+set -o errexit
+set -o errtrace
+
+# helm repo add gitea-charts https://dl.gitea.io/charts
+# helm repo update
+
+NAMESPACE="gitea-system"
+
+# helm install my-gitea \
+helm upgrade --install my-gitea \
+    --create-namespace \
+    --namespace ${NAMESPACE} \
+    --values values.yaml \
+    --version 4.0.3 \
+    gitea-charts/gitea
