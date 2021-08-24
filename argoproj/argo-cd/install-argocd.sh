95fc02a4505a76b71f47f57357b51a587a898054
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-08-24 17:25:36 +0900

    update argocd version

--- argoproj/argo-cd/install-argocd.sh
+++ argoproj/argo-cd/install-argocd.sh
@@ -3,10 +3,14 @@ trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func $
 set -o errexit
 set -o errtrace
 
-ARGOCD_VERSION="v1.8.7"
+ARGOCD_VERSION="v2.1.0"
 
 kubectl create namespace argocd
+
 ### Non HA
+# kubectl apply --namespace argocd --filename https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl apply --namespace argocd --filename https://raw.githubusercontent.com/argoproj/argo-cd/${ARGOCD_VERSION}/manifests/install.yaml
+
 ### HA
+# kubectl apply --namespace argocd --filename https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/ha/install.yaml
 # kubectl apply --namespace argocd --filename https://raw.githubusercontent.com/argoproj/argo-cd/${ARGOCD_VERSION}/manifests/ha/install.yaml
