5f413782e8a6e072475d64fd7f0fb2acaf12bab5
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-08-30 07:22:54 +0900

    update git commands

--- git/git-commands.sh
+++ git/git-commands.sh
@@ -7,8 +7,14 @@ git status --untracked-files=no
 
 ######################################################################
 
+git commit -m "update" --allow-empty
+
+######################################################################
+
 ### stash
 
 git stash
 git stash list
 git stash pop
+
+git restore --source=stash@{0} -- ./tomcat/helper.sh
