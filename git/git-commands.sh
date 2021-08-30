ff7fc8bd61691fe778acabbcb35b4ca47c4d2a1d
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-08-30 19:32:15 +0900

    update git commands

--- git/git-commands.sh
+++ git/git-commands.sh
@@ -1,11 +1,36 @@
 ######################################################################
 
+### repo
+
+git init
+
+######################################################################
+
+### config
+
+git config user.name developer
+git config user.email developer
+
+######################################################################
+
+### remote
+
+git remote add origin http://git.example.com/developer/my-app-ci.git
+git remote add origin http://git.example.com/developer/my-app-cd.git
+
+git remote set-url origin http://git.example.com/developer/my-app-ci.git
+git remote set-url origin http://git.example.com/developer/my-app-cd.git
+
+######################################################################
+
 ### status
 
 git status
 git status --untracked-files=no
 
 ######################################################################
+
+### commit
 
 git commit -m "update" --allow-empty
 
