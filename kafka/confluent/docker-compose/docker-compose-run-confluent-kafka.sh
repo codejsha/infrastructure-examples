cf4cc58b1a25dd1577bca21c4e39d869060fde69
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-09-01 13:05:54 +0900

    update syntax

--- kafka/confluent/docker-compose/docker-compose-run-confluent-kafka.sh
+++ kafka/confluent/docker-compose/docker-compose-run-confluent-kafka.sh
@@ -12,6 +12,7 @@ GRAFANA_VOLUME_DIR="${KAFKA_VOLUME_DIR}/grafana"
 JMX_EXPORTER_VOLUME_DIR="${KAFKA_VOLUME_DIR}/jmx_exporter"
 KAFKA_LAG_EXPORTER_VOLUME_DIR="${KAFKA_VOLUME_DIR}/kafka-lag-exporter"
 
+SHARE_DIR="/mnt/share"
 INSTALL_SCRIPT_DIR="/svc/infrastructure/kafka/confluent/docker-compose"
 
 sudo rm -rf ${KAFKA_VOLUME_DIR}
@@ -22,12 +23,12 @@ sudo /bin/cp -f prometheus.yml alert.rules ${PROMETHEUS_VOLUME_DIR}
 
 ### grafana
 sudo mkdir -p ${GRAFANA_VOLUME_DIR}/provisioning
-sudo /bin/cp -rf /mnt/share/confluent-platform/grafana/{dashboards,datasources} ${GRAFANA_VOLUME_DIR}/provisioning
+sudo /bin/cp -rf ${SHARE_DIR}/confluent-platform/grafana/{dashboards,datasources} ${GRAFANA_VOLUME_DIR}/provisioning
 
 ### jmx-exporter
 sudo mkdir -p ${JMX_EXPORTER_VOLUME_DIR}
-sudo /bin/cp -f /mnt/share/prometheus/jmx_prometheus_javaagent-0.15.0.jar ${KAFKA_VOLUME_DIR}/jmx_exporter
-sudo /bin/cp -f /mnt/share/confluent-platform/jmx-exporter/*.yml ${KAFKA_VOLUME_DIR}/jmx_exporter
+sudo /bin/cp -f ${SHARE_DIR}/prometheus/jmx_prometheus_javaagent-0.15.0.jar ${KAFKA_VOLUME_DIR}/jmx_exporter
+sudo /bin/cp -f ${SHARE_DIR}/confluent-platform/jmx-exporter/*.yml ${KAFKA_VOLUME_DIR}/jmx_exporter
 
 ### kafka-lag-exporter
 sudo mkdir -p ${KAFKA_LAG_EXPORTER_VOLUME_DIR}
