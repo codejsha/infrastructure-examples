68dd36cbfb5a093cb2ea676049cb8a0f1671f739
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-09-06 14:27:03 +0900

    add openldap

---
+++ security/openldap/phpldapadmin/helm/helm-install-phpldapadmin.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+set -o errexit
+set -o errtrace
+
+# helm repo add cetic https://cetic.github.io/helm-charts
+# helm repo update
+
+NAMESPACE="ldap-system"
+
+# helm install my-ldapadmin \
+helm upgrade --install my-ldapadmin \
+    --create-namespace \
+    --namespace ${NAMESPACE} \
+    --values values.yaml \
+    cetic/phpldapadmin
