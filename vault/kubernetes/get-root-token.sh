8f7e16b52957614a3fe8242106073242f3f5070e
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-09-08 19:58:06 +0900

    update vault

--- vault/kubernetes/get-root-token.sh
+++ vault/kubernetes/get-root-token.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
-set -o errexit
-set -o errtrace
+# trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+# set -o errexit
+# set -o errtrace
 
-NAMESPACE="vault-system"
-
-ROOT_TOKEN_LINE="$(grep -E "Initial Root Token: " ~/.vault/unseal_keys_and_root_token.txt)"
-ROOT_TOKEN="$(echo ${ROOT_TOKEN_LINE} | perl -p -e "s/Initial Root Token: //")"
-echo ${ROOT_TOKEN} > ~/.vault/root_token.txt
+cat ~/.vault/root_token.txt
