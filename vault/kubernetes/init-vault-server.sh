79913e2bceb2f55f379bc2a7f7adc7094b6d0039
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-09-08 19:58:06 +0900

    update vault

--- vault/kubernetes/init-vault-server.sh
+++ vault/kubernetes/init-vault-server.sh
@@ -6,4 +6,9 @@ set -o errtrace
 NAMESPACE="vault-system"
 
 mkdir -p ~/.vault
+
 kubectl exec -it --namespace ${NAMESPACE} my-vault-0 -- vault operator init > ~/.vault/unseal_keys_and_root_token.txt
+
+ROOT_TOKEN_LINE="$(grep -E "Initial Root Token: " ~/.vault/unseal_keys_and_root_token.txt)"
+ROOT_TOKEN="$(echo ${ROOT_TOKEN_LINE} | perl -p -e "s/Initial Root Token: //")"
+echo ${ROOT_TOKEN} > ~/.vault/root_token.txt
