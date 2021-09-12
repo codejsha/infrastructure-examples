79520d8580c91cae5ac8f01289769495479a5331
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-09-13 06:31:56 +0900

    move to other repository

--- kafka/confluent/helper/template/pssh/start-all-schema-registry-template.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-CONFLUENT_HOME=""
-
-pssh --hosts=${CONFLUENT_HOME}/pssh/hosts/schema-registry.hosts --askpass --inline "${SCRIPTS_DIR}/start.sh"
--- kafka/confluent/helper/template/pssh/stop-all-control-center-template.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-CONFLUENT_HOME=""
-
-pssh --hosts=${CONFLUENT_HOME}/pssh/hosts/control-center.hosts --askpass --inline "${SCRIPTS_DIR}/stop.sh"
