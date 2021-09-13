88487ce6c5038422c82b46808163458b7e5b7943
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-09-13 11:22:29 +0900

    update etcd commands

--- etcd/get-key.sh
+++ etcd/get-key.sh
@@ -13,14 +13,3 @@ sudo /usr/local/bin/etcdctl \
     --key=/etc/ssl/etcd/ssl/member-controlplane1-key.pem \
     get / --prefix --keys-only \
     | grep ${KEYWORD}
-
-# export ETCDCTL_API=3
-# sudo /usr/local/bin/etcdctl \
-#     --endpoints=https://controlplane1:2379,https://controlplane2:2379,https://controlplane3:2379 \
-#     --cacert=/etc/ssl/etcd/ssl/ca.pem \
-#     --cert=/etc/ssl/etcd/ssl/member-controlplane1.pem \
-#     --key=/etc/ssl/etcd/ssl/member-controlplane1-key.pem \
-#     get / --prefix --keys-only \
-#     | grep ${KEYWORD}
-
-etcdctl --insecure-transport=true --insecure-skip-tls-verify=true  --endpoints=https://localhost:2379 get / --prefix --keys-only --debug=true
