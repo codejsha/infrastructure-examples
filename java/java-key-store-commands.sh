ddac0e69d8433ebcfff5cba46caa5e96840b1495
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-09-14 15:17:27 +0900

    update keystore commands

--- java/java-key-store-commands.sh
+++ java/java-key-store-commands.sh
@@ -14,21 +14,31 @@ keytool -list -v -keystore ${JAVA_HOME}/lib/security/cacerts
 keytool -list -keystore ${JAVA_HOME}/jre/lib/security/cacerts
 keytool -list -v -keystore ${JAVA_HOME}/jre/lib/security/cacerts
 
+keytool -list -alias example -keystore keystore.jks
 keytool -list -v -alias example -keystore keystore.jks
 
 ######################################################################
 
-keytool -printcert -v -file example.com.crt
-
-######################################################################
-
-keytool -import -trustcacerts -alias example -file example.com.crt -keystore keystore.jks
-
-######################################################################
+### create empty keystore
+keytool -genkey -keyalg RSA -alias example -storepass changeit -keypass changeit -keystore keystore.jks -dname "CN=Developer, OU=Department, O=Company, L=City, ST=State, C=CA"
+keytool -delete -alias example -keystore keystore.jks
 
 ### create self-signed certificate
 keytool -genkey -keyalg RSA -alias example -keystore keystore.jks -storepass changeit -validity 365 -keysize 4096 -storetype pkcs12
 
 ######################################################################
 
+### import cert
+keytool -import -trustcacerts -alias example -file example.com.crt -keystore keystore.jks
+keytool -import -trustcacerts -alias rootca -file rootca.crt -keystore keystore.jks
+keytool -import -trustcacerts -alias test -file test.example.com.crt -keystore keystore.jks
+
+######################################################################
+
+### print cert
+keytool -printcert -v -file example.com.crt
+
+######################################################################
+
+### delete alias
 keytool -delete -alias example -keystore keystore.jks
