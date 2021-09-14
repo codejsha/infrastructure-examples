601d3decb3a5cb89aca0a570a04baf900c9cf9a1
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-09-15 08:03:07 +0900

    update openssl

---
+++ openssl/certificate/create-root-cert-commands.sh
@@ -0,0 +1,17 @@
+######################################################################
+
+mkdir cert
+cd cert
+
+
+### create root key
+
+openssl genrsa -des3 -out ca.key 4096
+### Enter pass phrase for ca.key:
+### Verifying - Enter pass phrase for ca.key:
+
+
+### create and self sign the root certificate
+
+openssl req -x509 -new -nodes -key ca.key -sha256 -days 1024 -out ca.crt -subj "/C=KR/ST=Seoul/CN=CA Certificate"
+### Enter pass phrase for ca.key:
