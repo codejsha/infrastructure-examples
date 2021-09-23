e30f948365cd79f0027d7052fd8dc7e2ebc3892d
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-09-23 10:15:54 +0900

    update keycloak

---
+++ keycloak/vault/operator/create-pki-role.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+set -o errexit
+set -o errtrace
+
+export VAULT_TOKEN="$(cat ~/.vault/root_token.txt)"
+export VAULT_ADDR="http://vault.example.com"
+
+vault write pki/roles/keycloak \
+    allowed_domains=keycloak.example.com,keycloak,keycloak.my-keycloak-operator,keycloak.my-keycloak-operator.svc.cluster.local,*.keycloak.my-keycloak-operator.svc.cluster.local \
+    allow_subdomains=true \
+    allow_bare_domains=true \
+    allow_glob_domains=true \
+    max_ttl=4380h
