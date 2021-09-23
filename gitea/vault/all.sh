18f8c8aae5117db9943317910e047a223bf63216
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-09-23 09:46:27 +0900

    update certificate

---
+++ gitea/vault/all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+set -o errexit
+set -o errtrace
+
+NAMESPACE="gitea-system"
+
+bash ./create-vault-issuer.sh
+bash ./create-kubernetes-role.sh
+bash ./create-pki-role.sh
+kubectl apply --namespace ${NAMESPACE} --filename gitea-certificate.yaml
---
+++ gitea/vault/create-vault-issuer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+set -o errexit
+set -o errtrace
+
+NAMESPACE="gitea-system"
+
+kubectl create serviceaccount --namespace ${NAMESPACE} gitea-issuer
+export GITEA_ISSUER_SECRET_REF="$(kubectl get serviceaccount --namespace ${NAMESPACE} gitea-issuer -o json | jq -r ".secrets[].name")"
+
+envsubst < ./vault-issuer.yaml > ./vault-issuer-temp.yaml
+kubectl apply --namespace ${NAMESPACE} --filename ./vault-issuer-temp.yaml
