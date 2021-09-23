64d87c2e2b9b50a790aed524b747a0516d96b022
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-09-23 09:44:19 +0900

    update cert-manager version

--- cert-manager/install-cert-manager.sh
+++ cert-manager/install-cert-manager.sh
@@ -3,5 +3,5 @@ trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func $
 set -o errexit
 set -o errtrace
 
-# kubectl apply --filename https://github.com/jetstack/cert-manager/releases/latest/download/cert-manager.yaml
-kubectl apply --filename https://github.com/jetstack/cert-manager/releases/download/v1.4.0/cert-manager.yaml
+kubectl apply --filename https://github.com/jetstack/cert-manager/releases/latest/download/cert-manager.yaml
+# kubectl apply --filename https://github.com/jetstack/cert-manager/releases/download/v1.5.3/cert-manager.yaml
