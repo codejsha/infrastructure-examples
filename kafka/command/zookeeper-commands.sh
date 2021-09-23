2131b73531e3d29bfc6cc031a3c6357669f7fa2a
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-09-23 09:58:44 +0900

    update zookeeper commands

--- kafka/command/zookeeper-commands.sh
+++ kafka/command/zookeeper-commands.sh
@@ -3,7 +3,6 @@
 ######################################################################
 
 ### broker list (kubernetes)
-
 kubectl exec -it my-cluster-zookeeper-0 -- bin/zookeeper-shell.sh localhost:2181 ls /brokers/ids
 
 
@@ -13,14 +12,23 @@ kubectl exec -it my-cluster-zookeeper-0 -- bin/zookeeper-shell.sh localhost:2181
 ### confluent kafka
 ######################################################################
 
-### broker list
+### cluster controller
+./zookeeper-shell localhost:2181 ls /controller
 
+### broker id
 ./zookeeper-shell localhost:2181 ls /brokers/ids
-./zookeeper-shell zookeeper1:2181,zookeeper2:2181,zookeeper3:2181 ls /brokers/ids
-
-### clusterlocal znode
 ./zookeeper-shell localhost:2181 ls /clusterlocal/brokers/ids
+./zookeeper-shell zookeeper1:2181,zookeeper2:2181,zookeeper3:2181 ls /brokers/ids
 ./zookeeper-shell zookeeper1:2181,zookeeper2:2181,zookeeper3:2181 ls /clusterlocal/brokers/ids
+
+### topic
+./zookeeper-shell localhost:2181 ls /brokers/topics
+
+### config
+./zookeeper-shell localhost:2181 ls /config
+
+### consumers (__consumer_offsets)
+# ./zookeeper-shell localhost:2181 ls /consumers
 
 ######################################################################
 
