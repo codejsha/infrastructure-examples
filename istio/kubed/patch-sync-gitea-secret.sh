1c91c3f8f30539dfd89ef1176cdbe263ef8731c3
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-09-23 09:49:49 +0900

    add kubed

---
+++ istio/kubed/patch-sync-gitea-secret.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+set -o errexit
+set -o errtrace
+
+kubectl patch namespace istio-system --patch-file patch-sync-gitea-secret.yaml
