9faaa142c28dd6fa7096661317390b9613750a36
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-09-23 10:18:37 +0900

    update istio

--- istio/install-istioctl.sh
+++ istio/install-istioctl.sh
@@ -3,14 +3,12 @@ trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func $
 set -o errexit
 set -o errtrace
 
-ISTIO_VERSION="1.11.0"
-PARENT_ISTIO_DIR="${HOME}"
-ISTIO_DIR="${PARENT_ISTIO_DIR}/istio-${ISTIO_VERSION}"
+curl -L https://istio.io/downloadIstio | sh -
 
-# curl -L https://istio.io/downloadIstio | sh -
-curl -L https://istio.io/downloadIstio | ISTIO_VERSION="${ISTIO_VERSION}" TARGET_ARCH="x86_64" sh -
+# ISTIO_VERSION="1.11.2"
+# curl -L https://istio.io/downloadIstio | ISTIO_VERSION="${ISTIO_VERSION}" TARGET_ARCH="x86_64" sh -
 
-cd ${ISTIO_DIR}
+cd istio-*
 cd bin
 chmod +x istioctl
 sudo mv istioctl /usr/local/bin
