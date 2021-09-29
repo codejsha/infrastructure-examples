0ff2494982945f288ccd99a22f06b929b7d23d8d
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-09-29 17:55:21 +0900

    update velero

--- velero/helm/helm-install-velero.sh
+++ velero/helm/helm-install-velero.sh
@@ -3,4 +3,15 @@ trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func $
 set -o errexit
 set -o errtrace
 
-helm repo add vmware-tanzu https://vmware-tanzu.github.io/helm-charts
+# helm repo add vmware-tanzu https://vmware-tanzu.github.io/helm-charts
+# helm repo update
+
+NAMESPACE="velero"
+kubectl create namespace ${NAMESPACE}
+
+# helm install my-velero \
+helm upgrade --install my-velero \
+    --create-namespace \
+    --namespace ${NAMESPACE} \
+    --version 2.23.8 \
+    vmware-tanzu/velero
