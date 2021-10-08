1b286d443f219db719f95d589a62457578481586
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-10-08 23:16:13 +0900

    add debug

---
+++ confluent/debug.sh
@@ -0,0 +1,22 @@
+######################################################################
+
+### security debug
+
+### key and certificate
+keytool -list -v -keystore keystore.jks
+keytool -list -v -keystore truststore.jks
+
+### root logger
+log4j.rootLogger=INFO, stdout
+log4j.rootLogger=DEBUG, stdout
+
+### authorization logging
+log4j.logger.kafka.authorizer.logger=DEBUG, authorizerAppender
+
+### ssl debug
+-Djavax.net.debug=all
+export KAFKA_OPTS="${KAFKA_OPTS} -Djavax.net.debug=all"
+
+### sasl debug
+-Dsun.security.krb5.debug=true
+export KAFKA_OPTS="${KAFKA_OPTS} -Dsun.security.krb5.debug=true"
