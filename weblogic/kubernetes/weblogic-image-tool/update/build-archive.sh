fdb167b6ecb27db205028cad77a6d1793d39545b
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-10-11 22:27:35 +0900

    update imagetool build

--- weblogic/kubernetes/weblogic-image-tool/update/build-archive.sh
+++ weblogic/kubernetes/weblogic-image-tool/update/build-archive.sh
@@ -12,11 +12,17 @@ if [ -z ${JAVA_HOME} ] || [ ! -e ${JAVA_HOME}/bin/jar ]; then
 fi
 
 rm -rf wlsdeploy
-mkdir -p wlsdeploy/applications
 
+### applications
+mkdir -p wlsdeploy/applications
 /bin/cp -rf ${SHARE_DIR}/app/weblogic-application/failovertest wlsdeploy/applications/
 /bin/cp -rf ${SHARE_DIR}/app/weblogic-application/jdbcdrivertest wlsdeploy/applications/
 /bin/cp -rf ${SHARE_DIR}/app/weblogic-application/cachetest wlsdeploy/applications/
 /bin/cp -rf ${SHARE_DIR}/app/weblogic-application/session-persistence-failover-app wlsdeploy/applications/
+
+### classpathLibraries
+mkdir -p wlsdeploy/classpathLibraries
+/bin/cp -rf ${SHARE_DIR}/oracle-mysql/connector/mysql-connector-java-8.0.26/mysql-connector-java-8.0.26.jar wlsdeploy/classpathLibraries/
+/bin/cp -rf ${SHARE_DIR}/tmax-tibero/tibero6-jdbc-driver/tibero6-jdbc.jar wlsdeploy/classpathLibraries/
 
 ${JAVA_HOME}/bin/jar -cf archive.zip wlsdeploy
