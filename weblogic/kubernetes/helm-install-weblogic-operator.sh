af8d8277b18a244f3ed98ba4e957eaf8d184c12f
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-10-12 00:46:01 +0900

    update values

--- weblogic/kubernetes/helm-install-weblogic-operator.sh
+++ weblogic/kubernetes/helm-install-weblogic-operator.sh
@@ -12,5 +12,6 @@ helm upgrade --install sample-weblogic-operator \
 ### openshift
 # helm upgrade --install sample-weblogic-operator \
 #     --namespace sample-weblogic-operator-ns \
+#     --values values.yaml \
 #     --values values-openshift.yaml \
 #     weblogic-kubernetes-operator/kubernetes/charts/weblogic-operator
