e4bf79b75e56f54e168031e1fccbc30b15f7dc09
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-10-11 22:01:16 +0900

    update weblogic kubernetes

--- weblogic/kubernetes/weblogic-image-tool/create/imagetool-create-build-weblogic.sh
+++ weblogic/kubernetes/weblogic-image-tool/create/imagetool-create-build-weblogic.sh
@@ -7,13 +7,6 @@ JAVA_HOME="/usr/java/java-11"
 # JAVA_HOME="/usr/java/java-1.8.0"
 export JAVA_HOME
 
-### copy install files
-SHARE_DIR="/mnt/share"
-/bin/cp -f ${SHARE_DIR}/oracle-java/11/jdk-11.0.12_linux-x64_bin.tar.gz  .
-/bin/cp -f ${SHARE_DIR}/oracle-weblogic-server/wls14.1.1/fmw_14.1.1.0.0_wls_lite_Disk1_1of1.zip .
-/bin/cp -f ${SHARE_DIR}/oracle-weblogic-server/opatch/p28186730_139426_Generic.zip .
-/bin/cp -f ${SHARE_DIR}/oracle-weblogic-server/wls14.1.1/p33069656_141100_Generic.zip .
-
 ### delete cache
 rm -rf ~/cache
 
