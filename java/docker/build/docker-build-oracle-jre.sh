3b737452261ce0a3504de0f0daa2598cd7a338af
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-10-13 13:24:37 +0900

    update java docker build

--- java/docker/build/docker-build-oracle-jre.sh
+++ java/docker/build/docker-build-oracle-jre.sh
@@ -16,19 +16,7 @@ cd docker-images/OracleJava
 
 ######################################################################
 
-function docker_image_build_jdk11_oraclelinux8 {
-    cd 11
-    /bin/cp -f ${SHARE_DIR}/oracle-jdk/11/jdk-11.0.12_linux-x64_bin.tar.gz .
-    /bin/cp -f ${SCRIPT_DIR}/Dockerfile.jdk11 Dockerfile.8
-    bash ./build.sh 8
-    rm -f *.tar.gz
-
-    # /bin/cp -f ${SHARE_DIR}/oracle-jdk/11/jdk-11.0.12_linux-x64_bin.tar.gz .
-    # docker build -f Dockerfile.jdk11 -t oracle/jdk:11-oraclelinux8 .
-    # rm -f *.tar.gz
-}
-
-function docker_image_build_8_oraclelinux8 {
+function docker_image_build_jre8_oraclelinux8 {
     cd 8
     /bin/cp -f ${SHARE_DIR}/oracle-jdk/8/server-jre-8u301-linux-x64.tar.gz .
     # /bin/cp -f ${SCRIPT_DIR}/Dockerfile.jdk8 Dockerfile.8
@@ -42,4 +30,4 @@ function docker_image_build_8_oraclelinux8 {
 
 ######################################################################
 
-docker_image_build_jdk11_oraclelinux8
+docker_image_build_jre8_oraclelinux8
