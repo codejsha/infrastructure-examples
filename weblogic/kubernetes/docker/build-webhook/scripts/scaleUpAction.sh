f380eabaefecaf7ce3ad65d2b753b4365742a312
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-10-18 17:17:43 +0900

    add webhook docker build

---
+++ weblogic/kubernetes/docker/build-webhook/scripts/scaleUpAction.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "scale up action" >> scaleUpAction.log
+
+bash /var/scripts/scalingAction.sh \
+    --action=scaleUp \
+    --domain_uid=sample-domain1 \
+    --cluster_name=cluster-1 \
+    --wls_domain_namespace=sample-domain1-ns \
+    --operator_service_name=internal-weblogic-operator-svc \
+    --operator_service_account=sample-weblogic-operator-sa \
+    --operator_namespace=sample-weblogic-operator-ns \
+    --scaling_size=1
