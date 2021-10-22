49fdd6b567532ee4a95e6ef5918602e4b7b3f174
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-10-22 16:43:53 +0900

    update weblogic kubernetes

--- weblogic/kubernetes/weblogic-image-tool/update/create-domain-credentials.sh
+++ weblogic/kubernetes/weblogic-image-tool/update/create-domain-credentials.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
-set -o errexit
-set -o errtrace
+# trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+# set -o errexit
+# set -o errtrace
 
 NAMESPACE="sample-domain1-ns"
 DOMAIN_NAME="sample-domain1"
---
+++ weblogic/kubernetes/weblogic-image-tool/update/docker/build-wldf/stopServer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "scale up action" >> scaleUpAction.log
+
+bash /u01/oracle/user_projects/domains/sample-domain1/bin/scripts/stopServer.sh
