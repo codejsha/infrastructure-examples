df09ef519ca156f7071159a59b2f627f8b6fd717
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-10-27 18:43:13 +0900

    add ldap docker

---
+++ openldap/docker/docker-run-openldap.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+set -o errexit
+set -o errtrace
+
+sudo mkdir -p /mnt/docker/openldap/database
+sudo mkdir -p /mnt/docker/openldap/config
+
+docker run \
+    --detach \
+    --name openldap \
+    --publish 389:389 \
+    --publish 636:636 \
+    --env LDAP_ORGANISATION="My Company" \
+    --env LDAP_DOMAIN="example.com" \
+    --env LDAP_ADMIN_PASSWORD="admin" \
+    --volume /mnt/docker/openldap/database:/var/lib/ldap \
+    --volume /mnt/docker/openldap/config:/etc/ldap/slapd.d \
+    osixia/openldap:latest
+
+# docker exec openldap ldapsearch -x -H ldap://localhost -b dc=example,dc=com -D "cn=admin,dc=example,dc=com" -w admin
