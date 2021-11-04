8f69355f13451188354e873e5c086b88390c15e9
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-11-05 00:37:39 +0900

    update syntax

--- openldap/phpldapadmin/docker/docker-run-ldapadmin.sh
+++ openldap/phpldapadmin/docker/docker-run-ldapadmin.sh
@@ -9,6 +9,6 @@ docker run \
     --publish 6080:80 \
     --publish 6443:443 \
     --env PHPLDAPADMIN_HTTPS=false \
-    --env PHPLDAPADMIN_LDAP_HOSTS=10.0.1.80 \
+    --env PHPLDAPADMIN_LDAP_HOSTS=test.example.com \
     --env PHPLDAPADMIN_TRUST_PROXY_SSL=true \
     osixia/phpldapadmin:latest
