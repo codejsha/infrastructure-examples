d6be6ca2f8f98da386a06bf7e1d12542fa730473
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-11-08 12:56:46 +0900

    update confluent version

--- confluent/docker/build-connect/docker-build-kafka-connect.sh
+++ confluent/docker/build-connect/docker-build-kafka-connect.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-### 6.2.1
-docker image build --file Dockerfile.kafka-connect --tag harbor.example.com/confluentinc/confluentinc/cp-custom-server-connect:6.2.1 .
-docker image push harbor.example.com/confluentinc/confluentinc/cp-custom-server-connect:6.2.1
+### 7.0.0
+docker image build --file Dockerfile.kafka-connect --tag harbor.example.com/confluentinc/confluentinc/cp-custom-server-connect:7.0.0 .
+docker image push harbor.example.com/confluentinc/confluentinc/cp-custom-server-connect:7.0.0
 
 ### latest
 # docker image build --file Dockerfile.kafka-connect --tag harbor.example.com/confluentinc/cp-custom-server-connect:latest .
