3aad6432e47ead0408b63a51c0175546e6a5ab15
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-11-15 10:59:24 +0900

    update connect build

---
+++ confluent/docker/build-connet-jdbc/docker-build-kafka-connect.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+### 7.0.0
+docker image build --file Dockerfile --tag harbor.example.com/confluentinc/cp-server-connect-jdbc:7.0.0 .
+docker image push harbor.example.com/confluentinc/cp-server-connect-jdbc:7.0.0
+
+### latest
+# docker image build --file Dockerfile --tag harbor.example.com/confluentinc/cp-server-connect-jdbc:latest .
+# docker image push harbor.example.com/confluentinc/cp-server-connect-jdbc:latest
