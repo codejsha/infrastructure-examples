2960f9e2fc546f92d3ab176b51061246dc222022
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-11-22 20:57:12 +0900

    fix error

--- ohs/helper-11g.sh
+++ ohs/helper-11g.sh
@@ -14,12 +14,14 @@ INSTALL_SCRIPT_DIR="/svc/infrastructure/ohs"
 MW_HOME="${MW_HOME//\//\\/}"
 ORACLE_HOME="${ORACLE_HOME//\//\\/}"
 INSTANCE_NAME="${INSTANCE_NAME//\//\\/}"
-INSTALL_SCRIPT_DIR="${INSTALL_SCRIPT_DIR//\//\\/}"
 
-######################################################################
+perl -pi -e "s/^MIDDLEWARE_HOME=.*/MIDDLEWARE_HOME=${MW_HOME}/" ${INSTALL_SCRIPT_DIR}/response-sw-only-11g.rsp
+perl -pi -e "s/^ORACLE_HOME=.*/ORACLE_HOME=${ORACLE_HOME}/" ${INSTALL_SCRIPT_DIR}/response-sw-only-11g.rsp
+
+### escape forward slash
+INSTALL_SCRIPT_DIR="${INSTALL_SCRIPT_DIR//\//\\/}"
 
 perl -pi -e "s/^MW_HOME=.*/MW_HOME=\"${MW_HOME}\"/" env-base.sh
 perl -pi -e "s/^ORACLE_HOME=.*/ORACLE_HOME=\"${ORACLE_HOME}\"/" env-base.sh
 perl -pi -e "s/^INSTANCE_NAME=.*/INSTANCE_NAME=\"${INSTANCE_NAME}\"/" env-base.sh
 perl -pi -e "s/^INSTALL_SCRIPT_DIR=.*/INSTALL_SCRIPT_DIR=\"${INSTALL_SCRIPT_DIR}\"/" env-base.sh
-perl -pi -e "s/^ORACLE_HOME=.*/ORACLE_HOME=${ORACLE_HOME}/" ${INSTALL_SCRIPT_DIR}/ohs/response-sw-only-11g.rsp
