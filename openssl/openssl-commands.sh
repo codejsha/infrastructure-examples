17a3cb7fe919e87d07acf46cbb1693ba1c048eec
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-12-08 18:38:22 +0900

    update openssl commands

--- openssl/openssl-commands.sh
+++ openssl/openssl-commands.sh
@@ -1,3 +1,19 @@
 ######################################################################
 
 openssl s_client -connect test.example.com:443 -tls1_2
+
+######################################################################
+
+### create pfx file
+openssl pkcs12 -export -in /svc/certs/tls.crt -inkey /svc/certs/tls.key -out tls.pfx
+openssl pkcs12 -info -in tls.pfx -passin file:tls.password.txt
+
+### extract certificate
+openssl pkcs12 -in tls.pfx -clcerts -nokeys -out tls.crt.pem
+openssl pkcs12 -in tls.pfx -clcerts -nokeys -out tls.crt.pem -password pass:PASSWORD
+openssl pkcs12 -in tls.pfx -clcerts -nokeys -out tls.crt.pem -passin file:tls.password.txt
+
+### extract private key
+openssl pkcs12 -in tls.pfx -nocerts -nodes -out tls.key.pem
+openssl pkcs12 -in tls.pfx -nocerts -nodes -out tls.key.pem -password pass:PASSWORD
+openssl pkcs12 -in tls.pfx -nocerts -nodes -out tls.key.pem -passin file:tls.password.txt
