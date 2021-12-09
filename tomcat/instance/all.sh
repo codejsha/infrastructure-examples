e8f6116604334f5160cc43f97e637329e5cd45ac
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2021-12-09 18:52:44 +0900

    update all.sh

--- tomcat/instance/all.sh
+++ tomcat/instance/all.sh
@@ -7,32 +7,50 @@ set -o errtrace
 
 ### create instances
 
-bash ./create-instance.sh --instance=inst1 --http-port=8080 --https-port=8443 --shutdown-port=8005
-bash ./create-instance.sh --instance=inst2 --http-port=8081 --https-port=8444 --shutdown-port=8006
+# bash ./create-instance.sh --instance=inst1 --address=test1.example.com --http-port=8080 --https-port=8443 --shutdown-port=8005
+# bash ./create-instance.sh --instance=inst2 --address=test2.example.com --http-port=8081 --https-port=8444 --shutdown-port=8006
 
 ######################################################################
 
 ### create instances - cluster (multicast)
 
-bash ./create-instance-cluster.sh --instance=inst1 --http-port=8080 --https-port=8443 --shutdown-port=8005 --ajp-port=8009 --jmx-port=8050
-bash ./create-instance-cluster.sh --instance=inst2 --http-port=8081 --https-port=8444 --shutdown-port=8006 --ajp-port=8010 --jmx-port=8051
+bash ./create-instance-cluster.sh --instance=inst1 --address=test1.example.com --http-port=8080 --https-port=8443 --shutdown-port=8005 --ajp-port=8009 --jmx-port=8050
+bash ./create-instance-cluster.sh --instance=inst2 --address=test2.example.com --http-port=8080 --https-port=8443 --shutdown-port=8005 --ajp-port=8009 --jmx-port=8050
 
-# bash ./create-instance-cluster-full.sh --instance=inst1 --http-port=8080 --https-port=8443 --shutdown-port=8005 --ajp-port=8009 --jmx-port=8050
-# bash ./create-instance-cluster-full.sh --instance=inst2 --http-port=8081 --https-port=8444 --shutdown-port=8006 --ajp-port=8010 --jmx-port=8051
+# bash ./create-instance-cluster.sh --instance=inst1 --address=test.example.com --http-port=8080 --https-port=8443 --shutdown-port=8005 --ajp-port=8009 --jmx-port=8050
+# bash ./create-instance-cluster.sh --instance=inst2 --address=test.example.com --http-port=8081 --https-port=8444 --shutdown-port=8006 --ajp-port=8010 --jmx-port=8051
+
+# bash ./create-instance-cluster-full.sh --instance=inst1 --address=test1.example.com --http-port=8080 --https-port=8443 --shutdown-port=8005 --ajp-port=8009 --jmx-port=8050
+# bash ./create-instance-cluster-full.sh --instance=inst2 --address=test2.example.com --http-port=8080 --https-port=8443 --shutdown-port=8005 --ajp-port=8009 --jmx-port=8050
+
+# bash ./create-instance-cluster-full.sh --instance=inst1 --address=test.example.com --http-port=8080 --https-port=8443 --shutdown-port=8005 --ajp-port=8009 --jmx-port=8050
+# bash ./create-instance-cluster-full.sh --instance=inst2 --address=test.example.com --http-port=8081 --https-port=8444 --shutdown-port=8006 --ajp-port=8010 --jmx-port=8051
 
 ######################################################################
 
 ### create instances - cluster (tcp)
 
-# bash ./create-instance-cluster-static.sh --instance=inst1 --http-port=8080 --https-port=8443 --shutdown-port=8005 --ajp-port=8009 --jmx-port=8050 \
-#     --cluster-address1=test1.example.com --cluster-port1=4000 --cluster-address2=test2.example.com --cluster-port2=4000 --receiver-port=4000
-# bash ./create-instance-cluster-static.sh --instance=inst2 --http-port=8081 --https-port=8444 --shutdown-port=8006 --ajp-port=8010 --jmx-port=8051 \
-#     --cluster-address1=test1.example.com --cluster-port1=4000 --cluster-address2=test2.example.com --cluster-port2=4000 --receiver-port=4000
+# bash ./create-instance-cluster-static.sh --instance=inst1 --address=test1.example.com \
+#     --http-port=8080 --https-port=8443 --shutdown-port=8005 --ajp-port=8009 --jmx-port=8050 \
+#     --cluster-address1=test1.example.com --cluster-port1=4000 \
+#     --cluster-address2=test2.example.com --cluster-port2=4000 \
+#     --receiver-port=4000
+# bash ./create-instance-cluster-static.sh --instance=inst2 --address=test2.example.com \
+#     --http-port=8080 --https-port=8443 --shutdown-port=8005 --ajp-port=8009 --jmx-port=8050 \
+#     --cluster-address1=test1.example.com --cluster-port1=4000 \
+#     --cluster-address2=test2.example.com --cluster-port2=4000 \
+#     --receiver-port=4000
 
-# bash ./create-instance-cluster-static.sh --instance=inst1 --http-port=8080 --https-port=8443 --shutdown-port=8005 --ajp-port=8009 --jmx-port=8050 \
-#     --cluster-address1=test.example.com --cluster-port1=4001 --cluster-address2=test.example.com --cluster-port2=4002 --receiver-port=4001
-# bash ./create-instance-cluster-static.sh --instance=inst2 --http-port=8081 --https-port=8444 --shutdown-port=8006 --ajp-port=8010 --jmx-port=8051 \
-#     --cluster-address1=test.example.com --cluster-port1=4001 --cluster-address2=test.example.com --cluster-port2=4002 --receiver-port=4002
+# bash ./create-instance-cluster-static.sh --instance=inst1 --address=test.example.com \
+#     --http-port=8080 --https-port=8443 --shutdown-port=8005 --ajp-port=8009 --jmx-port=8050 \
+#     --cluster-address1=test.example.com --cluster-port1=4001 \
+#     --cluster-address2=test.example.com --cluster-port2=4002 \
+#     --receiver-port=4001
+# bash ./create-instance-cluster-static.sh --instance=inst2 --address=test.example.com \
+#     --http-port=8081 --https-port=8444 --shutdown-port=8006 --ajp-port=8010 --jmx-port=8051 \
+#     --cluster-address1=test.example.com --cluster-port1=4001 \
+#     --cluster-address2=test.example.com --cluster-port2=4002 \
+#     --receiver-port=4002
 
 ######################################################################
 
