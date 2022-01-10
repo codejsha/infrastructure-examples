6e9e6d140a4f1932421e46d7fe603cea5d0dd536
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-01-10 23:04:14 +0900

    update grafana

--- grafana/start-grafana.sh
+++ grafana/start-grafana.sh
@@ -3,6 +3,18 @@
 GRAFANA_HOME="/usr/local/grafana"
 GRAFANA_CONFIG_FILE="${GRAFANA_HOME}/conf/defaults.ini"
 
+### create log directory
+if [ ! -d "${LOG_DIR}/backup" ]; then
+    mkdir -p ${LOG_DIR}/backup
+fi
+
+### backup stdout log
+DATETIME="$(date +'%Y%m%d_%H%M%S')"
+if [ -f "${LOG_DIR}/nohup.grafana.out" ]; then
+    mv ${LOG_DIR}/nohup.grafana.out ${LOG_DIR}/backup/nohup.grafana.${DATETIME}.out
+fi
+
+### start
 nohup ${GRAFANA_HOME}/bin/grafana-server \
     --config ${GRAFANA_CONFIG_FILE} \
     > ${GRAFANA_HOME}/nohup.grafana.out 2>&1 &
