1762611464cb8a074f80005672eb80931dd09822
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-01-19 02:22:24 +0900

    update kvm

--- linux/kvm/virsh-commands.sh
+++ linux/kvm/virsh-commands.sh
@@ -6,3 +6,10 @@ virsh list --all
 virsh start testmachine
 virsh shutdown testmachine
 virsh destroy testmachine
+virsh undefine testmachine
+
+######################################################################
+
+virsh nodeinfo
+virsh dominfo testmachine
+virsh net-info default
