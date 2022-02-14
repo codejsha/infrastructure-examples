f1e042a1f45ede9b383423a2b06c433898c67e49
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-02-14 12:41:25 +0900

    update orapki

--- ohs/orapki-commands.sh
+++ ohs/orapki-commands.sh
@@ -1,17 +1,57 @@
 ######################################################################
 
-# ${ORACLE_HOME}/oracle_common/bin
+# ORACLE_HOME=/usr/local/ohs
 
-./orapki wallet create -wallet ./default -auto_login
+######################################################################
+
+### create wallet
+${ORACLE_HOME}/oracle_common/bin/orapki wallet create -wallet ./default -auto_login
+${ORACLE_HOME}/oracle_common/bin/orapki wallet create -wallet ./default -auto_login_only
 
 ### display wallet
-./orapki wallet display -wallet ./default
-./orapki wallet display -complete -wallet ./default
+${ORACLE_HOME}/oracle_common/bin/orapki wallet display -wallet ./default
+${ORACLE_HOME}/oracle_common/bin/orapki wallet display -wallet ./default -complete
+
+######################################################################
+
+### create wallet and import pkcs#12
+${ORACLE_HOME}/oracle_common/bin/orapki wallet create \
+    -wallet ./default -pwd welcome1 -auto_login
+${ORACLE_HOME}/oracle_common/bin/orapki wallet import_pkcs12 \
+    -wallet ./default -pwd welcome1 -pkcs12file tls.p12 -pkcs12pwd welcome1
+
+######################################################################
+
+### import trusted certificate
+${ORACLE_HOME}/oracle_common/bin/orapki wallet add \
+    -wallet ./default -trusted_cert -cert trusted-cert.crt
+
+### import user certificate
+${ORACLE_HOME}/oracle_common/bin/orapki wallet add \
+    -wallet ./default -user_cert -cert user-cert.crt
+
+######################################################################
+
+### export certificate from wallet
+${ORACLE_HOME}/oracle_common/bin/orapki wallet display \
+    -wallet ./default -complete
+${ORACLE_HOME}/oracle_common/bin/orapki wallet export \
+    -wallet ./default -dn "<certificate dn>" -cert tls.crt
+
+### export csr from wallet
+${ORACLE_HOME}/oracle_common/bin/orapki wallet display \
+    -wallet ./default -complete
+${ORACLE_HOME}/oracle_common/bin/orapki wallet export \
+    -wallet ./default -dn "<certificate dn>" -request tls.csr
 
 ######################################################################
 
 ### self signed certificate
-./orapki wallet create -wallet default -auto_login_only
-./orapki wallet add -wallet default -dn "CN=test.example.com,ST=Seoul,C=KR" -keysize 2048 -self_signed -validity 1095 -auto_login_only
-./orapki wallet add -wallet default -dn "CN=localhost,ST=Seoul,C=KR" -keysize 2048 -self_signed -validity 1095 -auto_login_only
-./orapki wallet display -wallet default/
+${ORACLE_HOME}/oracle_common/bin/orapki wallet \
+    create -wallet ./default -auto_login_only
+${ORACLE_HOME}/oracle_common/bin/orapki wallet \
+    add -wallet ./default -dn "CN=localhost,ST=Seoul,C=KR" -keysize 2048 -self_signed -validity 1095 -auto_login_only
+${ORACLE_HOME}/oracle_common/bin/orapki wallet \
+    add -wallet ./default -dn "CN=test.example.com,ST=Seoul,C=KR" -keysize 2048 -self_signed -validity 1095 -auto_login_only
+${ORACLE_HOME}/oracle_common/bin/orapki wallet \
+    display -wallet ./default
