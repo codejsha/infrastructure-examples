622f6eaf5ed46eeefd66b8b5ad793bec86c63d1c
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-02-18 02:28:48 +0900

    update install

--- confluent/ansible/install-confluent-kafka-ansible.sh
+++ confluent/ansible/install-confluent-kafka-ansible.sh
@@ -4,11 +4,26 @@ set -o errexit
 set -o errtrace
 
 ansible-galaxy collection install \
+    --collections-path /root/ansible-infra-stack \
     git+https://github.com/confluentinc/cp-ansible.git
+# ansible-galaxy collection list -p /root/ansible-infra-stack
 
-ansible -i hosts.yml all -m ping
+cd /root/ansible-infra-stack
 
-ansible-playbook -i hosts.yml confluent.platform.all
+# ansible -i inventory/confluent-hosts-plaintext.yml all -m ping
+# ansible-playbook -i inventory/confluent-hosts-plaintext.yml all confluent.platform.all
+
+# ansible -i inventory/confluent-hosts-scram-plaintext.yml all -m ping
+# ansible-playbook -i inventory/confluent-hosts-scram-plaintext.yml all confluent.platform.all
+
+# ansible -i inventory/confluent-hosts-scram-rbac-ssl.yml all -m ping
+# ansible-playbook -i inventory/confluent-hosts-scram-rbac-ssl.yml all confluent.platform.all
+
+ansible -i inventory/confluent-hosts-kerberos-rbac-ssl.yml all -m ping
+ansible-playbook -i inventory/confluent-hosts-kerberos-rbac-ssl.yml all confluent.platform.all
+
+# ansible -i inventory/confluent-hosts-kerberos-rbac-mtls.yml all -m ping
+# ansible-playbook -i inventory/confluent-hosts-kerberos-rbac-mtls.yml all confluent.platform.all
 
 # ansible-playbook -i hosts.yml confluent.platform.all --tags=zookeeper
 # ansible-playbook -i hosts.yml confluent.platform.all --tags=kafka_broker
