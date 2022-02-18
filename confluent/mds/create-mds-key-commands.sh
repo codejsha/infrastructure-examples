ff1e2fc2d66d8a165b1c37aefad5d81deaeacb1a
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-02-18 14:22:34 +0900

    update keypair

--- confluent/mds/create-mds-key-commands.sh
+++
@@ -1,4 +0,0 @@
-######################################################################
-
-openssl genrsa -out tokenKeypair.pem 2048
-openssl rsa -in tokenKeypair.pem -outform PEM -pubout -out public.pem
