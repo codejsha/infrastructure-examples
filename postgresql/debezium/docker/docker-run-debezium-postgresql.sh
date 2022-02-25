8a054844551a599eced32b0562c651c42f064b59
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-02-25 12:27:01 +0900

    update postgresql container

--- postgresql/debezium/docker/docker-run-debezium-postgresql.sh
+++ postgresql/debezium/docker/docker-run-debezium-postgresql.sh
@@ -3,12 +3,36 @@ trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func $
 set -o errexit
 set -o errtrace
 
-PASSWORD="postgres"
-
 ######################################################################
 
-function docker_run_debezium_postgresql13 {
+function docker_run_debezium_postgresql14 {
+    local PASSWORD="postgres"
     local POSTGRESQL_VOLUME_DIR="/mnt/volume/postgresql"
+    # sudo rm -rf ${POSTGRESQL_VOLUME_DIR}
+    sudo mkdir -p ${POSTGRESQL_VOLUME_DIR}/data
+    # docker container run --rm -i debezium/postgres:14 cat /usr/share/postgresql/postgresql.conf.sample > debezium-postgresql.conf
+    sudo /bin/cp -f debezium-postgresql.conf ${POSTGRESQL_VOLUME_DIR}/postgresql.conf
+
+    docker container run \
+        --detach \
+        --name postgresql14 \
+        --publish 5432:5432 \
+        --env POSTGRES_DB="postgres" \
+        --env POSTGRES_USER="postgres" \
+        --env POSTGRES_PASSWORD="${PASSWORD}" \
+        --env PGDATA="/var/lib/postgresql/data/pgdata" \
+        --mount type="bind",src="${POSTGRESQL_VOLUME_DIR}/data",dst="/var/lib/postgresql/data" \
+        --mount type="bind",src="/mnt/share",dst="/mnt/share",readonly \
+        --mount type="bind",src="/mnt/storage",dst="/mnt/storage" \
+        --mount type="bind",src="${POSTGRESQL_VOLUME_DIR}/postgresql.conf",dst="/etc/postgresql/postgresql.conf" \
+        debezium/postgres:14 \
+        -c "config_file=/etc/postgresql/postgresql.conf"
+}
+
+function docker_run_debezium_postgresql13 {
+    local PASSWORD="postgres"
+    local POSTGRESQL_VOLUME_DIR="/mnt/volume/postgresql"
+    # sudo rm -rf ${POSTGRESQL_VOLUME_DIR}
     sudo mkdir -p ${POSTGRESQL_VOLUME_DIR}/data
     # docker container run --rm -i debezium/postgres:13 cat /usr/share/postgresql/postgresql.conf.sample > debezium-postgresql.conf
     sudo /bin/cp -f debezium-postgresql.conf ${POSTGRESQL_VOLUME_DIR}/postgresql.conf
@@ -27,11 +51,12 @@ function docker_run_debezium_postgresql13 {
         --mount type="bind",src="${POSTGRESQL_VOLUME_DIR}/postgresql.conf",dst="/etc/postgresql/postgresql.conf" \
         debezium/postgres:13 \
         -c "config_file=/etc/postgresql/postgresql.conf"
-
 }
 
 function docker_run_debezium_postgresql12 {
+    local PASSWORD="postgres"
     local POSTGRESQL_VOLUME_DIR="/mnt/volume/postgresql"
+    # sudo rm -rf ${POSTGRESQL_VOLUME_DIR}
     sudo mkdir -p ${POSTGRESQL_VOLUME_DIR}/data
     # docker container run --rm -i debezium/postgres:12 cat /usr/share/postgresql/postgresql.conf.sample > debezium-postgresql.conf
     sudo /bin/cp -f debezium-postgresql.conf ${POSTGRESQL_VOLUME_DIR}/postgresql.conf
@@ -53,7 +78,9 @@ function docker_run_debezium_postgresql12 {
 }
 
 function docker_run_debezium_postgresql11 {
+    local PASSWORD="postgres"
     local POSTGRESQL_VOLUME_DIR="/mnt/volume/postgresql"
+    # sudo rm -rf ${POSTGRESQL_VOLUME_DIR}
     sudo mkdir -p ${POSTGRESQL_VOLUME_DIR}/data
     # docker container run --rm -i debezium/postgres:11 cat /usr/share/postgresql/postgresql.conf.sample > debezium-postgresql.conf
     sudo /bin/cp -f debezium-postgresql.conf ${POSTGRESQL_VOLUME_DIR}/postgresql.conf
@@ -76,6 +103,7 @@ function docker_run_debezium_postgresql11 {
 
 ######################################################################
 
-docker_run_debezium_postgresql13
+docker_run_debezium_postgresql14
+# docker_run_debezium_postgresql13
 # docker_run_debezium_postgresql12
 # docker_run_debezium_postgresql11
--- postgresql/docker/docker-run-postgresql.sh
+++ postgresql/docker/docker-run-postgresql.sh
@@ -3,14 +3,32 @@ trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func $
 set -o errexit
 set -o errtrace
 
-PASSWORD="${PASSWORD}"
-
-POSTGRESQL_VOLUME_DIR="/mnt/volume/postgresql"
-sudo mkdir -p ${POSTGRESQL_VOLUME_DIR}/data
-
 ######################################################################
 
+function docker_run_postgresql14 {
+    local PASSWORD="postgres"
+    local POSTGRESQL_VOLUME_DIR="/mnt/volume/postgresql"
+    # sudo rm -rf ${POSTGRESQL_VOLUME_DIR}
+    sudo mkdir -p ${POSTGRESQL_VOLUME_DIR}/data
+
+    docker container run \
+        --detach \
+        --name postgresql14 \
+        --publish 5432:5432 \
+        --env POSTGRES_DB="postgres" \
+        --env POSTGRES_USER="postgres" \
+        --env POSTGRES_PASSWORD="${PASSWORD}" \
+        --env PGDATA="/var/lib/postgresql/data/pgdata" \
+        --mount type="bind",src="${POSTGRESQL_VOLUME_DIR}/data",dst="/var/lib/postgresql/data" \
+        postgres:14
+}
+
 function docker_run_postgresql13 {
+    local PASSWORD="postgres"
+    local POSTGRESQL_VOLUME_DIR="/mnt/volume/postgresql"
+    # sudo rm -rf ${POSTGRESQL_VOLUME_DIR}
+    sudo mkdir -p ${POSTGRESQL_VOLUME_DIR}/data
+
     docker container run \
         --detach \
         --name postgresql13 \
@@ -24,6 +42,11 @@ function docker_run_postgresql13 {
 }
 
 function docker_run_postgresql12 {
+    local PASSWORD="postgres"
+    local POSTGRESQL_VOLUME_DIR="/mnt/volume/postgresql"
+    # sudo rm -rf ${POSTGRESQL_VOLUME_DIR}
+    sudo mkdir -p ${POSTGRESQL_VOLUME_DIR}/data
+
     docker container run \
         --detach \
         --name postgresql12 \
@@ -37,6 +60,11 @@ function docker_run_postgresql12 {
 }
 
 function docker_run_postgresql11 {
+    local PASSWORD="postgres"
+    local POSTGRESQL_VOLUME_DIR="/mnt/volume/postgresql"
+    # sudo rm -rf ${POSTGRESQL_VOLUME_DIR}
+    sudo mkdir -p ${POSTGRESQL_VOLUME_DIR}/data
+
     docker container run \
         --detach \
         --name postgresql11 \
@@ -51,6 +79,7 @@ function docker_run_postgresql11 {
 
 ######################################################################
 
-docker_run_postgresql13
+docker_run_postgresql14
+# docker_run_postgresql13
 # docker_run_postgresql12
 # docker_run_postgresql11
