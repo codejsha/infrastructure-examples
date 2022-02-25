2cead9ffa09b5f0c3738b76221735653a2d84d7e
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-02-25 13:36:24 +0900

    update commands

--- confluent/command/confluent-kafka-mds-commands.sh
+++ confluent/command/confluent-kafka-mds-commands.sh
@@ -3,10 +3,27 @@
 ######################################################################
 
 ### login mds cluster
-confluent login --ca-cert-path /var/ssl/private/ca.crt --url https://kafka1:8090
+confluent login --ca-cert-path /opt/confluent/ssl/ca.crt --url https://kafka1.example.com:8090
 
 ### context
 confluent context list
+
+curl --silent \
+    --request GET \
+    --user mds:mds-secret \
+    --cacert /opt/confluent/ssl/ca.crt \
+    --cert /opt/confluent/ssl/kafka_broker.crt \
+    --key /opt/confluent/ssl/kafka_broker.key \
+    https://kafka1.example.com:8090/security/1.0/authenticate \
+    | jq
+curl --silent \
+    --request GET \
+    --user control-center:control-center-secret \
+    --cacert /opt/confluent/ssl/ca.crt \
+    --cert /opt/confluent/ssl/control_center.crt \
+    --key /opt/confluent/ssl/control_center.key
+    https://kafka1.example.com:8090/security/1.0/authenticate \
+    | jq
 
 ######################################################################
 
@@ -14,12 +31,14 @@ confluent context list
 
 ### list
 confluent cluster list
+KAFKA_CLUSTER_ID="${KAFKA_CLUSTER_ID}"
 
 ######################################################################
 
 ### role binding list
 
 confluent iam rbac role-binding list --kafka-cluster-id ${KAFKA_CLUSTER_ID} --principal User:mds
+confluent iam rbac role-binding list --kafka-cluster-id ${KAFKA_CLUSTER_ID} --principal User:kafkaadmin
 confluent iam rbac role-binding list --kafka-cluster-id ${KAFKA_CLUSTER_ID} --principal User:kafka
 confluent iam rbac role-binding list --kafka-cluster-id ${KAFKA_CLUSTER_ID} --principal User:schema-registry
 confluent iam rbac role-binding list --kafka-cluster-id ${KAFKA_CLUSTER_ID} --principal User:kafka-connect
