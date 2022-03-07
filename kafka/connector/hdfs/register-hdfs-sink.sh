fe74034dee039c848438beae015c2ceb1baf654b
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-03-07 22:15:23 +0900

    update connectors

--- kafka/connector/hdfs/register-hdfs-sink.sh
+++ kafka/connector/hdfs/register-hdfs-sink.sh
@@ -4,6 +4,8 @@ KAFKA_CONNECT_URL="http://localhost:8083"
 CONNECTOR_NAME="hdfs-sink"
 CONNECTOR_CONFIG_FILE="hdfs-sink.json"
 
+######################################################################
+
 ### delete connector
 curl \
     --request DELETE \
@@ -21,3 +23,31 @@ curl \
     ${KAFKA_CONNECT_URL}/connectors
 
 echo
+
+######################################################################
+
+# ### delete connector
+# curl \
+#     --user kafka-connect:kafka-connect-secret \
+#     --request DELETE \
+#     --header "Accept:application/json" \
+#     --cacert /opt/confluent/ssl/ca.crt \
+#     --cert /opt/confluent/ssl/kafka_connect.crt \
+#     --key /opt/confluent/ssl/kafka_connect.key \
+#     ${KAFKA_CONNECT_URL}/connectors/${CONNECTOR_NAME}
+#
+# echo
+#
+# ### register connector
+# curl \
+#     --user kafka-connect:kafka-connect-secret \
+#     --request POST \
+#     --header "Accept:application/json" \
+#     --header "Content-Type:application/json" \
+#     --cacert /opt/confluent/ssl/ca.crt \
+#     --cert /opt/confluent/ssl/kafka_connect.crt \
+#     --key /opt/confluent/ssl/kafka_connect.key \
+#     --data @${CONNECTOR_CONFIG_FILE} \
+#     ${KAFKA_CONNECT_URL}/connectors
+#
+# echo
