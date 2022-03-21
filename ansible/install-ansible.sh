356a2a07b7a30f068d76f4771b7e460a3bdfb283
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-03-22 04:03:42 +0900

    add ansible

---
+++ ansible/install-ansible.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+set -o errexit
+set -o errtrace
+
+pip3 install -y ansible
+
+# pip3 install --requirement requirements.txt
