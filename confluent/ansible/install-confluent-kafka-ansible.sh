4a829c659412fd6b0e9cb3961da182a4ff2b3cce
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-03-22 04:57:58 +0900

    update ansible

--- confluent/ansible/install-confluent-kafka-ansible.sh
+++ confluent/ansible/install-confluent-kafka-ansible.sh
@@ -3,28 +3,6 @@ trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func $
 set -o errexit
 set -o errtrace
 
-ansible-galaxy collection install \
-    --collections-path /root/ansible-infra-stack \
-    git+https://github.com/confluentinc/cp-ansible.git
-# ansible-galaxy collection list -p /root/ansible-infra-stack
-
-cd /root/ansible-infra-stack
-
-# ansible -i inventory/confluent-hosts-plaintext.yml all -m ping
-# ansible-playbook -i inventory/confluent-hosts-plaintext.yml confluent.platform.all
-
-# ansible -i inventory/confluent-hosts-scram-plaintext.yml all -m ping
-# ansible-playbook -i inventory/confluent-hosts-scram-plaintext.yml confluent.platform.all
-
-# ansible -i inventory/confluent-hosts-scram-rbac-ssl.yml all -m ping
-# ansible-playbook -i inventory/confluent-hosts-scram-rbac-ssl.yml confluent.platform.all
-
-ansible -i inventory/confluent-hosts-kerberos-rbac-ssl.yml all -m ping
-ansible-playbook -i inventory/confluent-hosts-kerberos-rbac-ssl.yml confluent.platform.all
-
-# ansible -i inventory/confluent-hosts-kerberos-rbac-mtls.yml all -m ping
-# ansible-playbook -i inventory/confluent-hosts-kerberos-rbac-mtls.yml confluent.platform.all
-
 # ansible -i hosts.yml all -m ping
 # ansible-playbook -i hosts.yml confluent.platform.all --tags=zookeeper
 # ansible-playbook -i hosts.yml confluent.platform.all --tags=kafka_broker
@@ -33,3 +11,15 @@ ansible-playbook -i inventory/confluent-hosts-kerberos-rbac-ssl.yml confluent.pl
 # ansible-playbook -i hosts.yml confluent.platform.all --tags=ksql
 # ansible-playbook -i hosts.yml confluent.platform.all --tags=kafka_rest
 # ansible-playbook -i hosts.yml confluent.platform.all --tags=control_center
+
+# ansible -i inventory/hosts-plaintext.yml all -m ping
+# ansible-playbook -i inventory/hosts-plaintext.yml confluent.platform.all
+
+# ansible -i inventory/hosts-scram-plaintext.yml all -m ping
+# ansible-playbook -i inventory/hosts-scram-plaintext.yml confluent.platform.all
+
+ansible -i inventory/hosts-scram-rbac-ssl.yml all -m ping
+ansible-playbook -i inventory/hosts-scram-rbac-ssl.yml confluent.platform.all
+
+# ansible -i inventory/hosts-kerberos-rbac-ssl.yml all -m ping
+# ansible-playbook -i inventory/hosts-kerberos-rbac-ssl.yml confluent.platform.all
