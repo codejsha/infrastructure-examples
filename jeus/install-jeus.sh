b9dcb5f01614a17802423a4e4f1dab4c163a4d59
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-03-31 17:08:08 +0900

    update syntax

--- jeus/install-jeus.sh
+++ jeus/install-jeus.sh
@@ -2,6 +2,6 @@
 
 INSTALL_FILE_DIR="/mnt/share/tmax-jeus"
 INSTALL_FILE="jeus7_unix_generic_ko.bin"
-INSTALL_SCRIPT_DIR="/svc/infrastructure/others/jeus"
+INSTALL_SCRIPT_DIR="/svc/infrastructure/jeus"
 
 ${INSTALL_FILE_DIR}/${INSTALL_FILE} -f ${INSTALL_SCRIPT_DIR}/jeus_install_linux.properties
