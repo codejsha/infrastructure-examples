cbbcae5609e35117d335b3a5ccfcb3193513114c
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-06-13 14:28:38 +0900

    add java install

---
+++ java/choco-install-microsoft-jdk.ps1
@@ -0,0 +1,7 @@
+# choco install --confirm microsoft-openjdk
+# choco install --confirm microsoft-openjdk17
+choco install --confirm microsoft-openjdk11
+
+# choco uninstall --confirm microsoft-openjdk
+# choco uninstall --confirm microsoft-openjdk17
+# choco uninstall --confirm microsoft-openjdk11
