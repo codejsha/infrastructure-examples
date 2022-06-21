bbee256b71c499c1a0dcebd2e8e3bb7f2958ee50
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-06-21 18:14:07 +0900

    update commands

--- argo-cd/repository/add-repository.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
-set -o errexit
-set -o errtrace
-
-argocd repo add https://git.example.com/developer/my-app-cd.git --type git --username developer --password developer --insecure-skip-server-verification --insecure --server argocd.example.com
