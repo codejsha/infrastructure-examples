9b728e36a75327abb763e0a83e02a9bff5f45540
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-06-30 04:09:21 +0900

    delete files

--- descheduler/docker-run-descheduler-cli-commands.sh
+++
@@ -1,9 +0,0 @@
-######################################################################
-
-docker container run --rm -it k8s.gcr.io/descheduler/descheduler:v0.19.0 descheduler version
-docker container run --rm -it k8s.gcr.io/descheduler/descheduler:v0.19.0 descheduler --help
-
-######################################################################
-
-function descheduler() { echo "+ descheduler ${@}">&2; command descheduler ${@}; }
-alias descheduler="docker container run --rm -it k8s.gcr.io/descheduler/descheduler:v0.19.0 descheduler"
