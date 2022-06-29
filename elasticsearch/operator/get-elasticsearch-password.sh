e37684839cf9c33cc949e75bd9a341c451d55e14
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-06-30 07:09:47 +0900

    delete files

--- elasticsearch/operator/get-elasticsearch-password.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
-set -o errexit
-set -o errtrace
-
-### username: elastic
-
-### get password
-kubectl get secret --namespace elastic elasticstack-es-elastic-user -o=jsonpath='{.data.elastic}' | base64 --decode; echo
--- elasticsearch/stop-elasticsearch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pkill -9 -ecf "org\.elasticsearch\.bootstrap\.Elasticsearch"
-# pkill -15 -ecf "org\.elasticsearch\.bootstrap\.Elasticsearch"
