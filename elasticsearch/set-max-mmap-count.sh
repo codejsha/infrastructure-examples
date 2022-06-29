2dd6e7f66807c831687f79695d0700bfa09caed0
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-06-30 07:09:47 +0900

    delete files

--- elasticsearch/set-max-mmap-count.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
-set -o errexit
-set -o errtrace
-
-sudo sysctl -w vm.max_map_count=262144
-echo "vm.max_map_count=262144" | sudo tee -a /etc/sysctl.conf
