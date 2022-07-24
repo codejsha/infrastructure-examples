7e4cf742fa4f87cecc43a7cc25ac6680f5133802
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-07-25 07:22:08 +0900

    delete files

--- keycloak/operator/install-keycloak-operator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
-# set -o errexit
-# set -o errtrace
-
-curl -sL https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.18.3/install.sh | bash -s v0.18.3
-kubectl apply -f https://operatorhub.io/install/keycloak-operator.yaml
