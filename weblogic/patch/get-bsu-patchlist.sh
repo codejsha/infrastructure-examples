3f7aafe0984de16eb69c665efa543bd40754ee07
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2022-11-18 20:29:57 +0900

    fix error

--- weblogic/patch/get-bsu-patchlist.sh
+++ weblogic/patch/get-bsu-patchlist.sh
@@ -17,7 +17,7 @@ function bsu_view_applied {
     #     -prod_dir=${MW_HOME}/wlserver_10.3 \
     #     -verbose \
     #     -log_priority=debug \
-    #     -log=${MW_HOME}/utils/bsu/bsu-install-${PATCH_LIST}.log
+    #     -log=${MW_HOME}/utils/bsu/bsu-view.log
     # ${MW_HOME}/utils/bsu/bsu.sh \
     #     -view \
     #     -status=applied \
@@ -25,7 +25,7 @@ function bsu_view_applied {
     #     -patch_download_dir=${MW_HOME}/utils/bsu/cache_dir \
     #     -verbose \
     #     -log_priority=debug \
-    #     -log=${MW_HOME}/utils/bsu/bsu-install-${PATCH_LIST}.log
+    #     -log=${MW_HOME}/utils/bsu/bsu-view.log
 }
 
 ######################################################################
