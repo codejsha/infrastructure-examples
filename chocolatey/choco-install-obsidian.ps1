fe034cdc64ef62fd3f9a1d7d419f989cee3e8a18
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-02-16 22:11:37 +0900

    update chocolatey

---
+++ chocolatey/choco-install-obsidian.ps1
@@ -0,0 +1,3 @@
+choco install --confirm obsidian
+
+# choco uninstall --confirm obsidian
--- chocolatey/choco-install-redistributable-package.ps1
+++ chocolatey/choco-install-redistributable-package.ps1
@@ -1,13 +1,19 @@
-choco install --confirm vcredist140   # 2015-2019
+# choco install --confirm vcredist-all
+choco install --confirm vcredist140   # 2015-2022
 choco install --confirm vcredist2017
 choco install --confirm vcredist2015
 choco install --confirm vcredist2013
 choco install --confirm vcredist2012
 choco install --confirm vcredist2010
+# choco install --confirm vcredist2008
+# choco install --confirm vcredist2005
 
-# choco uninstall --confirm vcredist140   # 2015-2019
+# choco uninstall --confirm vcredist-all
+# choco uninstall --confirm vcredist140   # 2015-2022
 # choco uninstall --confirm vcredist2017
 # choco uninstall --confirm vcredist2015
 # choco uninstall --confirm vcredist2013
 # choco uninstall --confirm vcredist2012
 # choco uninstall --confirm vcredist2010
+# choco uninstall --confirm vcredist2008
+# choco uninstall --confirm vcredist2005
