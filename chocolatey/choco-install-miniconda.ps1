6a15c11ed6f0d7619d0b8e0e914c42dd20f631c7
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-02-16 22:11:37 +0900

    update chocolatey

---
+++ chocolatey/choco-install-miniconda.ps1
@@ -0,0 +1,5 @@
+choco install --confirm miniconda3 --params '"/InstallationType:JustMe"'
+# choco install --confirm miniconda3 --params '"/InstallationType:JustMe /AddToPath:1"'
+# choco install --confirm miniconda3 --params '"/InstallationType:JustMe /AddToPath:1 /RegisterPython:1"'
+
+# choco uninstall miniconda3
---
+++ chocolatey/choco-install-texlive.ps1
@@ -0,0 +1,4 @@
+# choco install --confirm texlive
+choco install --confirm texlive --params "'/InstallationPath:$env:USERPROFILE\tools\texlive'"
+
+# choco uninstall --confirm texlive
