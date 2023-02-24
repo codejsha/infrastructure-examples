9eb5b4359b7f91cd97069d40de4409e9a322a4f6
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-02-24 21:36:49 +0900

    update docker

--- docker/docker-desktop/export-docker-desktop-data.ps1
+++ docker/docker-desktop/export-docker-desktop-data.ps1
@@ -1,4 +1,11 @@
-wsl --export docker-desktop-data "E:\Docker\wsl\data\docker-desktop-data.tar"
+### wsl --list --all
+###     Windows Subsystem for Linux Distributions:
+###     Ubuntu-20.04 (Default)
+###     docker-desktop
+###     docker-desktop-data
+
+$DataLocation = "E:\Docker\wsl\data"
+wsl --export docker-desktop-data "$DataLocation\docker-desktop-data.tar"
 wsl --unregister docker-desktop-data
-wsl --import docker-desktop-data "E:\Docker\wsl\data" "E:\Docker\wsl\data\docker-desktop-data.tar" --version 2
-Remove-Item -Force E:\Docker\wsl\data\docker-desktop-data.tar
+wsl --import docker-desktop-data "$DataLocation" "$DataLocation\docker-desktop-data.tar" --version 2
+Remove-Item -Force $DataLocation\docker-desktop-data.tar
