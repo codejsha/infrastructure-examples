964b6948307e21fa1e4e2f255efa508739083a38
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-02-26 22:56:58 +0900

    update linux

--- linux/file-system/cifs/install-samba-client-linux.sh
+++ linux/file-system/cifs/install-samba-client-linux.sh
@@ -20,12 +20,23 @@ sudo yum install -y \
 # sudo mount -t cifs -o user=shareuser,password=${PASSWORD} //192.168.140.1/share /mnt/share
 # sudo mount -t cifs -o user=shareuser,password=${PASSWORD} //192.168.140.1/storage /mnt/storage
 
-### auto
-echo "//192.168.137.1/share   /mnt/share              cifs    username=shareuser,password=${PASSWORD},file_mode=0755,dir_mode=0755 0 0" | sudo tee -a /etc/fstab
-# echo "//192.168.137.1/storage /mnt/storage            cifs    username=shareuser,password=${PASSWORD},uid=prouser,gid=progroup 0 0" | sudo tee -a /etc/fstab
-# echo "//192.168.140.1/share   /mnt/share              cifs    username=shareuser,password=${PASSWORD},file_mode=0755,dir_mode=0755 0 0" | sudo tee -a /etc/fstab
-# echo "//192.168.140.1/storage /mnt/storage            cifs    username=shareuser,password=${PASSWORD},uid=prouser,gid=progroup 0 0" | sudo tee -a /etc/fstab
-
 ### umount
 # sudo umount /mnt/share
 # sudo umount /mnt/storage
+
+### auto
+echo "//192.168.137.1/share   /mnt/share              cifs    username=shareuser,password=${PASSWORD},file_mode=0644,dir_mode=0755 0 0" | sudo tee -a /etc/fstab
+# echo "//192.168.137.1/storage /mnt/storage            cifs    username=shareuser,password=${PASSWORD},uid=prouser,gid=progroup 0 0" | sudo tee -a /etc/fstab
+# echo "//192.168.138.1/share   /mnt/share              cifs    username=shareuser,password=${PASSWORD},file_mode=0644,dir_mode=0755 0 0" | sudo tee -a /etc/fstab
+# echo "//192.168.138.1/code-examples   /mnt/repos/code-examples   cifs   username=shareuser,password=${PASSWORD},noauto,user 0 0" | sudo tee -a /etc/fstab
+# echo "//192.168.140.1/share   /mnt/share              cifs    username=shareuser,password=${PASSWORD},file_mode=0644,dir_mode=0755 0 0" | sudo tee -a /etc/fstab
+# echo "//192.168.140.1/storage /mnt/storage            cifs    username=shareuser,password=${PASSWORD},uid=prouser,gid=progroup 0 0" | sudo tee -a /etc/fstab
+
+### credentials
+# cat <<EOF > ~/.smbcredentials
+# username=shareuser
+# password=${PASSWORD}
+# EOF
+# sudo chown root .smbcredentials
+# sudo chmod 600 .smbcredentials
+# echo "//192.168.138.1/code-examples   /mnt/repos/code-examples   cifs   credentials=/home/prouser/.smbcredentials 0 0" | sudo tee -a /etc/fstab
