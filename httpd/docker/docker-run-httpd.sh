bb240c6881d653cbd1a080e17d5d286153687cbc
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-02-26 22:43:13 +0900

    update httpd

--- httpd/docker/docker-run-httpd.sh
+++ httpd/docker/docker-run-httpd.sh
@@ -3,23 +3,41 @@ trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func $
 set -o errexit
 set -o errtrace
 
-HTTPD_VOLUME_DIR="/mnt/volume/httpd"
-sudo mkdir -p ${HTTPD_VOLUME_DIR}/{conf,htdocs}
-# sudo /bin/cp -f docker-httpd.conf ${HTTPD_VOLUME_DIR}/conf/httpd.conf
-
 ######################################################################
 
 function docker_run_httpd {
+    HTTPD_VOLUME_DIR="/mnt/volume/httpd"
+    sudo mkdir -p ${HTTPD_VOLUME_DIR}/{conf,htdocs,logs}
+    # sudo /bin/cp -f docker-httpd.conf ${HTTPD_VOLUME_DIR}/conf/httpd.conf
+
     docker container run \
         --detach \
         --name httpd \
         --publish 8080:80 \
         --mount type="bind",source="${HTTPD_VOLUME_DIR}/conf",target="/usr/local/apache2/conf" \
         --mount type="bind",source="${HTTPD_VOLUME_DIR}/htdocs",target="/usr/local/apache2/htdocs" \
+        --mount type="bind",source="${HTTPD_VOLUME_DIR}/logs",target="/usr/local/apache2/logs" \
         --mount type="bind",src="/mnt/share",dst="/mnt/share",readonly \
+        httpd:2.4
+}
+
+function docker_run_httpd_specific_network {
+    HTTPD_VOLUME_DIR="/mnt/volume/httpd"
+    sudo mkdir -p ${HTTPD_VOLUME_DIR}/{conf,htdocs,logs}
+
+    docker container run \
+        --detach \
+        --name httpd \
+        --network minikube \
+        --publish 80:80 \
+        --publish 443:443 \
+        --mount type="bind",source="${HTTPD_VOLUME_DIR}/conf",target="/usr/local/apache2/conf" \
+        --mount type="bind",source="${HTTPD_VOLUME_DIR}/htdocs",target="/usr/local/apache2/htdocs" \
+        --mount type="bind",source="${HTTPD_VOLUME_DIR}/logs",target="/usr/local/apache2/logs" \
         httpd:2.4
 }
 
 ######################################################################
 
 docker_run_httpd
+# docker_run_httpd_specific_network
