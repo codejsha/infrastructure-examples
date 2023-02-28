b1b234ac7a0e2185efbfbae794ee7be33decfcd3
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-02-28 18:57:54 +0900

    update openssl

---
+++ openssl/kube-run-openssl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+set -o errexit
+set -o errtrace
+
+kubectl run --rm -it --restart=Never --image=frapsoft/openssl:latest openssl
