b3518225a6f8e50670464d1d7ccf2d6791ceb258
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-02-28 18:46:28 +0900

    update commands

--- kubernetes/kubectl-commands.sh
+++ kubernetes/kubectl-commands.sh
@@ -1,14 +1,28 @@
 ######################################################################
 
+source <(kubectl completion bash)
+
+function kubectl() { echo "+ kubectl ${@}">&2; command kubectl ${@}; }
+
+[ -f ~/.aliases/kubectl_aliases ] && source ~/.aliases/kubectl_aliases
+
+######################################################################
+
 kubectl api-versions
 
-kubectl top nodes
-kubectl top pods
+### dashboard password
+kubectl describe secrets kubernetes-dashboard-token --namespace kube-system | grep token: | awk '{print $2}'
 
-kubectl create --recursive --filename resource.yaml
 
-cat <<EOF | kubectl apply --filename -
+cat <<EOF | kubectl apply -f -
+EOF
+cat <<EOF | kubectl create -f -
+EOF
+cat <<EOF | kubectl delete --recursive -f
 EOF
 
-cat <<EOF | kubectl create --filename -
-EOF
+kubectl delete jobs --grace-period=0 --force
+kubectl delete jobs --all
+kubectl delete jobs --all --grace-period=0 --force
+kubectl delete pods --grace-period=0 --force
+kubectl delete pods --all --grace-period=0 --force
