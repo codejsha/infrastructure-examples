c08315df81be72015be01b85afa22ed64d950656
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-03-03 00:25:01 +0900

    update mysql docker

--- mysql/docker/docker-run-mysql.sh
+++ mysql/docker/docker-run-mysql.sh
@@ -7,6 +7,24 @@ set -o errtrace
 
 function docker_run_mysql8 {
     local PASSWORD="${PASSWORD}"
+    local MYSQL_DATA_VOLUME="mysql_data"
+    local MYSQL_CONFIG_VOLUME="mysql_config"
+    docker volume create ${MYSQL_DATA_VOLUME}
+    docker volume create ${MYSQL_CONFIG_VOLUME}
+
+    docker container run \
+        --detach \
+        --name mysql8 \
+        --publish 3306:3306 \
+        --publish 33060:33060 \
+        --env MYSQL_ROOT_PASSWORD="${PASSWORD}" \
+        --mount type="volume",src="${MYSQL_DATA_VOLUME}",dst="/var/lib/mysql" \
+        --mount type="volume",src="${MYSQL_CONFIG_VOLUME}",dst="/etc/mysql/conf.d" \
+        mysql:8
+}
+
+function docker_run_mysql8_bind {
+    local PASSWORD="${PASSWORD}"
     local MYSQL_VOLUME_DIR="/mnt/volume/mysql"
     mkdir -p ${MYSQL_VOLUME_DIR}/data
 
@@ -22,7 +40,7 @@ function docker_run_mysql8 {
         # --mount type="bind",src="${MYSQL_VOLUME_DIR}/config-file.cnf",dst="/etc/mysql/conf.d/config-file.cnf" \
 }
 
-function docker_run_mysql5 {
+function docker_run_mysql5_bind {
     local PASSWORD="${PASSWORD}"
     local MYSQL_VOLUME_DIR="/mnt/volume/mysql"
     mkdir -p ${MYSQL_VOLUME_DIR}/data
@@ -42,4 +60,5 @@ function docker_run_mysql5 {
 ######################################################################
 
 docker_run_mysql8
-# docker_run_mysql5
+# docker_run_mysql8_bind
+# docker_run_mysql5_bind
