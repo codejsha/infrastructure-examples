14956dd7b67a43e1b1876a3caba4f0908c11aba7
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-03-15 05:46:16 +0900

    update argocd

--- argo-cd/get-initial-password.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
-set -o errexit
-set -o errtrace
-
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
-
-echo
