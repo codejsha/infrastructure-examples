a446cdb1108eede843b431c7c98a3bc03efa5dab
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-09-17 04:25:19 +0900

    feat: add kafka-ui container

---
+++ kafka/kafka-ui/docker-run-kafka-ui.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+set -o errexit
+set -o errtrace
+
+HOSTNAME="$(hostname --fqdn)"
+HOSTIP="$(ip -4 addr show scope global dev eth0 | grep inet | awk '{print $2}' | cut -d / -f 1 | sed -n 1p)"
+
+docker container run \
+	--rm \
+    --detach \
+	--name kafka-ui \
+	--publish 9080:8080 \
+	--env KAFKA_CLUSTERS_0_NAME="clusterlocal" \
+	--env KAFKA_CLUSTERS_0_BOOTSTRAPSERVERS="${HOSTNAME}:9092" \
+	--env KAFKA_CLUSTERS_0_SCHEMAREGISTRY="http://${HOSTNAME}:8081" \
+	--env KAFKA_CLUSTERS_0_KAFKACONNECT_0_NAME="connect-cluster" \
+	--env KAFKA_CLUSTERS_0_KAFKACONNECT_0_ADDRESS="http://${HOSTNAME}:8083" \
+	--add-host="${HOSTNAME}:${HOSTIP}" \
+	provectuslabs/kafka-ui:latest
