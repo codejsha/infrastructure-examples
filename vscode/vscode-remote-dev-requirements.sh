33b9150b70e2cc5e7178ebf214d06703b4001531
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-09-17 04:25:41 +0900

    update

---
+++ vscode/vscode-remote-dev-requirements.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+set -o errexit
+set -o errtrace
+
+### https://code.visualstudio.com/docs/remote/linux
+### https://code.visualstudio.com/docs/remote/troubleshooting
+
+sudo yum install -y glibc libgcc libstdc++ python3 ca-certificates tar
+
+# export USER_AT_HOST="your-user-name-on-host@hostname"
+# export PUBKEYPATH="${HOME}/.ssh/id_rsa.pub"
+# ssh-copy-id -i "${PUBKEYPATH}" "${USER_AT_HOST}"
+# chmod 600 ${HOME}/.ssh/authorized_keys
