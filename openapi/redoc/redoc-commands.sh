859bd4ee9abb2b9e950da80355808e268f3a7445
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-09-17 04:25:29 +0900

    feat: add redoc

---
+++ openapi/redoc/redoc-commands.sh
@@ -0,0 +1,5 @@
+######################################################################
+
+npm install -g redoc-cli
+redoc-cli bundle path/to/your/openapi3.yaml -o openapi.html
+redoc-cli bundle openapi3.yaml -o openapi.html
