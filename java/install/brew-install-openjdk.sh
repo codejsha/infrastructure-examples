d7e1614a2f71b8f934490d183220b68ed3124b85
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-09-24 08:16:12 +0900

    feat: add install scripts

---
+++ java/install/brew-install-openjdk.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+brew install openjdk
+# brew uninstall openjdk
---
+++ java/install/brew-install-temurin-jdk.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+brew install --cask temurin
+brew install --cask temurin17
+brew install --cask temurin11
+brew install --cask temurin8
+
+# brew uninstall --cask temurin
+# brew uninstall --cask temurin17
+# brew uninstall --cask temurin11
+# brew uninstall --cask temurin8
