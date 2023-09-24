aea053ad011bb6a5dbcbb30a0e980a302d92d433
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2023-09-24 10:17:45 +0900

    feat: update gnupg commands

--- git/gnupg/gnupg-commands.sh
+++ git/gnupg/gnupg-commands.sh
@@ -1,3 +1,27 @@
 ######################################################################
 
+gpg --gen-key
+gpg --list-key
+gpg --armor --export "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+######################################################################
+
+git config --global user.name "USER_NAME"
+git config --global user.email "USER_EMAIL"
+git config --global user.signingkey "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+git config --global commit.gpgsign true
+git config --global gpg.program gpg
+# git config --global gpg.program $(which gpg)
+
+######################################################################
+
+git config user.name "USER_NAME"
+git config user.email "USER_EMAIL"
+git config user.signingkey "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+git config commit.gpgsign true
+git config gpg.program gpg
+# git config gpg.program $(which gpg)
+
+######################################################################
+
 gpg --symmetric --output filename.encrypted filename
