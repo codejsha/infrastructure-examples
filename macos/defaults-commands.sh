68fd44f916ac2ab3a51e277192611d393f1af9f5
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2024-01-15 20:35:37 +0900

    feat: update defaults commands

--- macos/defaults-commands.sh
+++ macos/defaults-commands.sh
@@ -1,3 +1,4 @@
 ######################################################################
 
 defaults write com.apple.finder AppleShowAllFiles -boolean true; killall Finder;
+defaults read com.apple.dock persistent-apps | grep bundle-identifier
