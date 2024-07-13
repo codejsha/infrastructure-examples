c68d6d389ff8a44b7ec46fd8cceef0b8ffd76758
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2024-07-13 22:28:13 +0900

    feat: add iplanet

---
+++ iplanet/server/create-links.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+set -o errexit
+set -o errtrace
+
+IPLANET_HOME="/usr/local/iplanet"
+IPLANET_SERVER="${1:-"admin-server"}"
+
+if [ "${IPLANET_SERVER}" == "admin-server" ]; then
+    ln -snf ${IPLANET_HOME}/admin-server/bin/startserv ${IPLANET_HOME}/start-admin.sh
+    ln -snf ${IPLANET_HOME}/admin-server/bin/stopserv ${IPLANET_HOME}/stop-admin.sh
+    chmod 750 ${IPLANET_HOME}/start-admin.sh
+    chmod 750 ${IPLANET_HOME}/stop-admin.sh
+
+else
+    ln -snf ${IPLANET_HOME}/https-${IPLANET_SERVER}/bin/startserv ${IPLANET_HOME}/start-${IPLANET_SERVER}.sh
+    ln -snf ${IPLANET_HOME}/https-${IPLANET_SERVER}/bin/stopserv ${IPLANET_HOME}/stop-${IPLANET_SERVER}.sh
+    chmod 750 ${IPLANET_HOME}/start-${IPLANET_SERVER}.sh
+    chmod 750 ${IPLANET_HOME}/stop-${IPLANET_SERVER}.sh
+fi
