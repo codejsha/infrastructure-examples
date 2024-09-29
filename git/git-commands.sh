fac66ec56864d8c4d1927ab92130d76500701f3a
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2024-09-29 20:04:44 +0900

    feat: update git commands

--- git/git-commands.sh
+++ git/git-commands.sh
@@ -9,6 +9,24 @@ function gclo() { REPO_URL="${1}"; DIR_NAME="${2}"; if [ -z "${DIR_NAME}" ]; the
 
 ######################################################################
 
+### create a new repository on the command line
+
+echo "# hello-world" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/codejsha/hello-world.git
+git push -u origin main
+
+### push an existing repository from the command line
+
+git remote add origin https://github.com/codejsha/hello-world.git
+git branch -M main
+git push -u origin main
+
+######################################################################
+
 ### repo
 git init
 
