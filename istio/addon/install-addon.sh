7910c849ab1da3cd58cd93e805c146eecd59bad8
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2024-10-06 16:41:43 +0900

    refactor: update istio addon

--- istio/addon/install-addon.sh
+++ istio/addon/install-addon.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-# trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
-# set -o errexit
-# set -o errtrace
+trap 'echo "${BASH_SOURCE[0]}: line ${LINENO}: status ${?}: user ${USER}: func ${FUNCNAME[0]}"' ERR
+set -o errexit
+set -o errtrace
 
-cd ../istio-*/
-kubectl apply --namespace istio-system --filename samples/addons
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/refs/heads/master/samples/addons/grafana.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/refs/heads/master/samples/addons/jaeger.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/refs/heads/master/samples/addons/kiali.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/refs/heads/master/samples/addons/loki.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/refs/heads/master/samples/addons/prometheus.yaml
+
+######################################################################
+
+### install
+# cd ../istio-*/
+# kubectl apply -f samples/addons
 
 ### delete
 # cd ../istio-*/
-# kubectl delete --filename samples/addons
+# kubectl delete -f samples/addons
