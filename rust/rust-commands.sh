13f8c638545ddabf78f574bd973df86986e7b045
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2024-12-07 08:12:35 +0900

    feat: add rust commands

---
+++ rust/rust-commands.sh
@@ -0,0 +1,16 @@
+######################################################################
+
+rustc --version
+
+rustup --version
+rustup update
+rustup self uninstall
+
+rustfmt --version
+
+cargo --version
+cargo new hello_cargo
+cargo build
+cargo build --release
+cargo run
+cargo check
