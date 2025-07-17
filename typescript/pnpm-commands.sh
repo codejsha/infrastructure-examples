64fff7bdb8544fad0407c7ecc8a1edf59589df6d
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2025-07-17 11:11:44 +0900

    refactor: update commands

---
+++ typescript/pnpm-commands.sh
@@ -0,0 +1,5 @@
+######################################################################
+
+pnpm create vite my-app --template react-ts
+pnpm add -D @vitejs/plugin-react-swc
+pnpm add -D tailwindcss postcss autoprefixer
