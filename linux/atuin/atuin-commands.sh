c3d3156ebe89af1c733540fb65dc5dfc69b791ef
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2025-09-04 12:32:37 +0900

    refactor: update atuin commands

--- linux/atuin/atuin-commands.sh
+++ linux/atuin/atuin-commands.sh
@@ -1,8 +1,26 @@
 ######################################################################
 
+atuin --help
+atuin --version
+
+atuin info
+
+######################################################################
+
+### statistics
+atuin stats
+
+### history
+atuin search curl
+
+######################################################################
+
 ### delete all history
 atuin search --delete-it-all
-atuin search --delete-it-all --exclude-exit 0
+atuin search --delete-it-all --exclude-exit=0
 
 ### delete history with filter
-atuin search --delete --exclude-exit 0 docker
+atuin search --delete --exclude-exit=0 docker
+atuin search --delete --exclude-exit=0 curl
+atuin search --delete hostctl
+
