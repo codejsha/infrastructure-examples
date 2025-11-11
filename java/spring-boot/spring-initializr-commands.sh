6c65dbec56540c71eaff00d710e99d18cdba7558
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2025-11-11 12:22:48 +0900

    feat: add spring boot initializr

---
+++ java/spring-boot/spring-initializr-commands.sh
@@ -0,0 +1,10 @@
+######################################################################
+
+curl https://start.spring.io/starter.zip \
+    -d dependencies=web \
+    -d name=demo-web \
+    -d artifactId=demo-web \
+    -d groupId=com.example \
+    -d packageName=com.example.demo \
+    -d javaVersion=21 \
+    -o demo-web.zip
