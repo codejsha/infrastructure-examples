a26bad1e259bd86c665620c6558f77d522b19cb9
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2025-11-20 06:32:40 +0900

    refactor: update spring boot commands

--- java/spring-boot/spring-initializr-commands.sh
+++ java/spring-boot/spring-initializr-commands.sh
@@ -2,9 +2,24 @@
 
 curl https://start.spring.io/starter.zip \
     -d dependencies=web \
-    -d name=demo-web \
-    -d artifactId=demo-web \
+    -d name=demo \
+    -d artifactId=demo \
     -d groupId=com.example \
     -d packageName=com.example.demo \
     -d javaVersion=21 \
-    -o demo-web.zip
+    -o demo.zip
+
+curl https://start.spring.io/starter.zip \
+    -d type=maven-project \
+    -d language=java \
+    -d platformVersion=3.5.7 \
+    -d packaging=jar \
+    -d configurationFileFormat=properties \
+    -d jvmVersion=21 \
+    -d groupId=com.example \
+    -d artifactId=demo \
+    -d name=demo \
+    -d description="Demo project for Spring Boot" \
+    -d packageName=com.example.demo \
+    -d dependencies=web \
+    -o demo.zip
