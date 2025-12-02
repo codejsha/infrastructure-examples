21839b2acc702a6ef18c2da169511643c19f8283
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2025-12-02 23:24:16 +0900

    feat: add trust

--- git/gnupg/gnupg-commands.sh
+++ git/gnupg/gnupg-commands.sh
@@ -71,4 +71,28 @@ gpg --edit-key "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 ######################################################################
 
+### change trust level
+
+gpg --edit-key "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# gpg> trust
+# ...
+# Please decide how far you trust this user to correctly verify other users' keys
+# (by looking at passports, checking fingerprints from different sources, etc.)
+#
+#   1 = I don't know or won't say
+#   2 = I do NOT trust
+#   3 = I trust marginally
+#   4 = I trust fully
+#   5 = I trust ultimately
+#   m = back to the main menu
+#
+# Your decision?
+# ...
+
+# gpg> save
+
+######################################################################
+
+### encrypt file
+
 gpg --symmetric --output filename.encrypted filename
