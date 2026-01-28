827d7e7020c447daf35df0d284fb05e7ebe65d20
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2026-01-28 11:09:33 +0900

    refactor: update brew commands

--- linux/package/package-brew-commands.sh
+++ linux/package/package-brew-commands.sh
@@ -11,6 +11,15 @@ brew install gcc
 
 ######################################################################
 
+brew install <package_name>
+brew upgrade <package_name>
+brew uninstall <package_name>
+
+brew update && brew outdated
+brew upgrade && brew cleanup
+
+######################################################################
+
 brew list
 brew list --versions protoc-gen-go
 brew list --versions protoc-gen-go-grpc
@@ -18,12 +27,6 @@ brew list --versions protoc-gen-grpc-java
 
 brew search <package_name>
 brew info <package_name>
-
-######################################################################
-
-brew install <package_name>
-brew upgrade <package_name>
-brew uninstall <package_name>
 
 ######################################################################
 
