ec413e1d7927261cf60e0ef609c63c7723454c1d
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2026-04-30 00:33:49 +0900

    refactor: update commands

--- docker/docker-compose/docker-compose-commands.sh
+++ docker/docker-compose/docker-compose-commands.sh
@@ -13,3 +13,60 @@ brew install docker-compose
 
 ### chocolatey
 choco install -y docker-compose
+
+######################################################################
+
+docker compose version
+docker compose config
+docker compose logs
+docker compose logs --follow
+
+######################################################################
+
+### up/start
+
+docker compose up
+docker compose up --detach
+docker compose up -d
+docker compose up -d --force-recreate proxy
+docker compose -f docker-compose.local.yaml up -d --build
+
+docker compose -f docker-compose.local.yaml down
+docker compose -f docker-compose.local.yaml down --volumes
+
+docker compose start
+
+docker compose restart
+docker compose -f docker-compose.local.yaml restart grafana
+
+######################################################################
+
+### stop/down
+
+docker compose stop
+
+docker compose down
+docker compose down --remove-orphans --volumes
+
+######################################################################
+
+docker compose top
+
+docker compose ps
+docker compose ps --services
+docker compose ps --filter "status=running"
+docker compose ps --filter "status=dead"
+docker compose ps --filter "status=exited"
+
+######################################################################
+
+### exec
+
+docker compose exec httpd /bin/bash
+docker compose exec --detach httpd /bin/bash -c "ls -al /var/www/html"
+docker compose exec --detach httpd /bin/sh -c "ls -al /var/www/html"
+
+######################################################################
+
+docker compose kill
+docker compose rm
