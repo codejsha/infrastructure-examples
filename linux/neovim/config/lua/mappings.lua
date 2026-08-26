42915b6de8997c39a8202c00864a01b6963e27dd
Jinseong Ha <50877637+codejsha@users.noreply.github.com>
2026-08-26 18:25:21 +0900

    feat(neovim): update config files

--- linux/neovim/config/lua/mappings.lua
+++ linux/neovim/config/lua/mappings.lua
@@ -1,12 +1,9 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
 
 map({ "n", "v" }, "d", '"_d')
 map({ "n", "v" }, "c", '"_c')
